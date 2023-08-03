5442bc83d2c62ec0542deec070369fc93ca04872
krypek <krypek@tuta.io>
2023-08-04 00:09:50 +0200

    Add ig.ENTITY.EnemySpawner.Settings

--- modules/game.feature.combat.entities.enemy-spawner.d.ts
+++ modules/game.feature.combat.entities.enemy-spawner.d.ts
@@ -1,1 +1,21 @@
 // requires impact.base.actor-entity
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace EnemySpawner {
+        interface Entry {
+          info: sc.EnemyInfo;
+          count: number;
+        }
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          size?: Vec2;
+          enemyTypes?: ig.ENTITY.EnemySpawner.Entry[];
+          onActiveClear?: boolean;
+        }
+      }
+    }
+  }
+}
