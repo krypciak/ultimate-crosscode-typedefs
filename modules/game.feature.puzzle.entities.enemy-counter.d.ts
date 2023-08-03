566d00f5b7ff559756f2f7d3e3a752ef70050667
krypek <krypek@tuta.io>
2023-08-03 19:56:16 +0200

    Add ig.ENTITY.EnemyCounter.Settings

--- modules/game.feature.puzzle.entities.enemy-counter.d.ts
+++ modules/game.feature.puzzle.entities.enemy-counter.d.ts
@@ -1,2 +1,19 @@
 // requires game.feature.combat.combat
 // requires impact.base.entity
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace EnemyCounter {
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        enemyGroup: string;
+        enemyCount: number;
+        preVariable?: string;
+        postVariable?: string;
+        countVariable?: string;
+        shuffleCondition?: string;
+      }
+    }
+  }
+}
