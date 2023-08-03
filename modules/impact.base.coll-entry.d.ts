10f1a020ea453a8974a2ec75f95bdc59b98b0848
krypek <krypek@tuta.io>
2023-08-03 19:10:19 +0200

    Add ig.ENTITY.HiddenBlock.Settings

--- modules/impact.base.coll-entry.d.ts
+++ modules/impact.base.coll-entry.d.ts
@@ -2,6 +2,32 @@ export {};
 
 declare global {
   namespace ig {
+    enum COLLTYPE {
+      NONE = 0,
+      IGNORE = 1,
+      PROJECTILE = 2,
+      VIRTUAL = 3,
+      PBLOCK = 4,
+      NPBLOCK = 5,
+      BLOCK = 6,
+      TRIGGER = 7,
+      PASSIVE = 8,
+      SEMI_IGNORE = 9,
+      FENCE = 10,
+      NPFENCE = 11
+    }
+    enum COLL_HEIGHT_SHAPE {
+      NONE = 0,
+      NORTH_UP = 1,
+      EAST_UP = 2,
+      WEST_UP = 3,
+      SOUTH_UP = 4
+    }
+    enum COLL_SHADOW_TYPE {
+      DEFAULT = 0,
+      STATIC_SIZE = 1,
+      RECTANGULAR = 2, 
+    }
     interface CollEntry extends ig.Class {
       pos: Vec3;
       size: Vec3;
