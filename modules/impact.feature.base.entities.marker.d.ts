325e104ee75389313734e87adb9fc7d22100b6c8
krypek <krypek@tuta.io>
2023-08-04 00:11:56 +0200

    Add ig.ENTITY.Marker.Settings

--- modules/impact.feature.base.entities.marker.d.ts
+++ modules/impact.feature.base.entities.marker.d.ts
@@ -1,1 +1,15 @@
 // requires impact.base.actor-entity
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace Marker {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          dir: keyof typeof ig.ActorEntity.FACE8;
+        }
+      }
+    }
+  }
+}
