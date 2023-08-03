e67d441da4e263bb44272794f10c46203cfb6d54
krypek <krypek@tuta.io>
2023-08-04 00:35:49 +0200

    Add ig.ENTITY.ElementPole.Settings

--- modules/game.feature.puzzle.entities.thermo-pole.d.ts
+++ modules/game.feature.puzzle.entities.thermo-pole.d.ts
@@ -1,3 +1,25 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
 // requires game.feature.combat.combat-poi
+
+export {};
+
+declare global {
+  namespace sc {
+    enum TERMO_POLE_TYPE {
+      LONG,
+      LONG64,
+      SHORT,
+    }
+  }
+
+  namespace ig.ENTITY {
+    namespace ElementPole {
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        group: string;
+        spawnCondition?: string;
+        poleType?: keyof typeof sc.TERMO_POLE_TYPE;
+      }
+    }
+  }
+}
