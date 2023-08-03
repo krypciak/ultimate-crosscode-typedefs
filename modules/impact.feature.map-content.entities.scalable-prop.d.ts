6e5af3d0dac564e322860f9c7e425408b1689e62
krypek <krypek@tuta.io>
2023-08-03 18:32:23 +0200

    Add ig.ENTITY.ScalableProp.Settings

--- modules/impact.feature.map-content.entities.scalable-prop.d.ts
+++ modules/impact.feature.map-content.entities.scalable-prop.d.ts
@@ -1,1 +1,26 @@
 // requires impact.base.entity
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace ScalableProp {
+      namespace Settings {
+        interface PropConfig {
+          name: string;
+          sheet: string;
+          ends: { west?: string; east?: string; north?: string; south?: string } | null;
+        }
+      }
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        size?: Vec2;
+        patternOffset?: Vec2;
+        timeOffset?: number;
+        blockNavMap: boolean;
+        hideCondition?: string;
+        propConfig?: ig.ENTITY.ScalableProp.Settings.PropConfig;
+      }
+    }
+  }
+}
+
