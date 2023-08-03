6dfeb31104a6043a10d2d2b7caa22ac7bd9c9180
krypek <krypek@tuta.io>
2023-08-03 22:07:30 +0200

    Add ig.ENTITY.BallChanger.Settings

--- modules/impact.base.actor-entity.d.ts
+++ modules/impact.base.actor-entity.d.ts
@@ -5,6 +5,47 @@ export {};
 
 declare global {
   namespace ig {
+    enum ActorEntity$FACE4 {
+      NORTH = 0,
+      EAST = 1,
+      SOUTH = 2,
+      WEST = 3,
+    }
+    enum ActorEntity$FACE8 {
+      NORTH = 0,
+      EAST = 1,
+      SOUTH = 2,
+      WEST = 3,
+      NORTH_EAST = 4,
+      SOUTH_EAST = 5,
+      SOUTH_WEST = 6,
+      NORTH_WEST = 7,
+    }
+
+    namespace ActorEntity {
+      interface WalkAnims {
+        idle: string;
+        preMove?: string;
+        move?: string;
+        moveRev?: string;
+        moveLeft?: string;
+        run?: string;
+        runRev?: string;
+        runLeft?: string;
+        brake?: string;
+        preIdle?: string;
+        jump?: string;
+        fall?: string;
+        hover?: string;
+        preHoverMove?: string;
+        hoverMove?: string;
+        hoverMoveRev?: string;
+        land?: string;
+      }
+      type FACE4 = ActorEntity$FACE4
+      type FACE8 = ActorEntity$FACE8
+    }
+
     interface ActorEntity extends ig.AnimatedEntity {
       face: Vec2;
       animationFixed: boolean;
@@ -19,7 +60,10 @@ declare global {
       setAttribute(this: this, key: string, value: unknown): void;
       getAttribute(this: this, key: string): unknown;
     }
-    interface ActorEntityConstructor extends ImpactClass<ActorEntity> {}
+    interface ActorEntityConstructor extends ImpactClass<ActorEntity> {
+      FACE4: typeof ActorEntity$FACE4
+      FACE8: typeof ActorEntity$FACE8
+    }
     var ActorEntity: ActorEntityConstructor;
   }
 }
