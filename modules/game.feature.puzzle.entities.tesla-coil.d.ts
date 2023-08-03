f4746523a3a1674988b170a9f320aa53346561db
krypek <krypek@tuta.io>
2023-08-04 00:00:00 +0200

    Add ig.ENTITY.TeslaCoil.Settings

--- modules/game.feature.puzzle.entities.tesla-coil.d.ts
+++ modules/game.feature.puzzle.entities.tesla-coil.d.ts
@@ -1,2 +1,34 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace sc {
+    enum TESLA_COIL_TYPE {
+      SOURCE,
+      SOURCE_FAST,
+      SOURCE_LOOSE,
+      SOURCE_LOOST_FLIP,
+      EXTENDER,
+      EXTENDER_LOOSE,
+      EXTENDER_LOOSE_FLIP,
+      EXTENDER_IGNORE,
+      GROUND_DISCHARGE,
+      WHALE_DISCHARGE,
+      GROUND_DISCHARGE_FISH,
+    }
+  }
+  namespace ig {
+    namespace ENTITY {
+      namespace TeslaCoil {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          coilType?: keyof typeof sc.TESLA_COIL_TYPE;
+          varOnCharge?: string;
+          varOnDischarge?: string;
+          align?: string;
+        }
+      }
+    }
+  }
+}
