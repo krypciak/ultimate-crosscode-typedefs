be9a36cab2b0f29eea3b4eaae7f3e8993995e1f5
krypek <krypek@tuta.io>
2023-08-03 18:32:15 +0200

    Add ig.ENTITY.GlowingLine.Settings

--- modules/game.feature.puzzle.entities.glowing-line.d.ts
+++ modules/game.feature.puzzle.entities.glowing-line.d.ts
@@ -1,1 +1,14 @@
 // requires impact.base.entity
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace GlowingLine {
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        size: Vec2;
+        condition: string;
+      }
+    }
+  }
+}
