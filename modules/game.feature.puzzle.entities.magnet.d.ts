415e2293acf3270e894fdd6bdacbd0dfc1fb906d
krypek <krypek@tuta.io>
2023-08-03 23:47:45 +0200

    Add ig.ENTITY.Magnet.Settings

--- modules/game.feature.puzzle.entities.magnet.d.ts
+++ modules/game.feature.puzzle.entities.magnet.d.ts
@@ -2,3 +2,18 @@
 // requires impact.feature.effect.effect-sheet
 // requires game.feature.combat.combat-target-event
 // requires impact.base.actor-entity
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace Magnet {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          dir: keyof typeof ig.ActorEntity.FACE4
+          altDirs?: string[]; // array of conditions
+        }
+      }
+    }
+  }
+}
