005babc4192cbf4e791a22f302f3d27acb9cedcb
krypek <krypek@tuta.io>
2023-08-04 00:27:40 +0200

    Add ig.ENTITY.EventTrigger.Settings

--- modules/game.feature.msg.entities.event-trigger.d.ts
+++ modules/game.feature.msg.entities.event-trigger.d.ts
@@ -3,27 +3,38 @@
 export {};
 
 declare global {
-    namespace sc {
-        //note: not an impact class!
-        interface Cutscene {
-            startCutscene(this: this, event: ig.Event, input: unknown, callEntity?: ig.Entity): ig.EventCall;
-        }
-        let Cutscene: Cutscene;
+  namespace sc {
+    //note: not an impact class!
+    interface Cutscene {
+      startCutscene(this: this, event: ig.Event, input: unknown, callEntity?: ig.Entity): ig.EventCall;
+    }
+    let Cutscene: Cutscene;
+  }
+
+  namespace ig {
+    enum EVENT_TYPE {
+      PARALLEL = 1,
+      CUTSCENE = 2,
+      INTERRUPTABLE = 3,
+      AUTO_CONTROL = 4,
+      COMBAT_CUTSCENE = 5
     }
 
-    namespace ig {
-        enum EVENT_TYPE {
-            PARALLEL = 1,
-            CUTSCENE = 2,
-            INTERRUPTABLE = 3,
-            AUTO_CONTROL = 4,
-            COMBAT_CUTSCENE = 5
-        }
-
-        enum EVENT_TRIGGER_TYPE {
-            ALWAYS = 0,
-            ONCE_PER_ENTRY = 1,
-            ONCE = 2
-        }
+    enum EVENT_TRIGGER_TYPE {
+      ALWAYS = 0,
+      ONCE_PER_ENTRY = 1,
+      ONCE = 2
     }
+    namespace ENTITY {
+      namespace EventTrigger {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          eventType?: keyof typeof ig.EVENT_TYPE;
+          startCondition?: string;
+          triggerType?: keyof typeof ig.EVENT_TRIGGER_TYPE;
+          loadCondition?: string;
+          event?: ig.Event.Settings;
+        }
+      }
+    }
+  }
 }
