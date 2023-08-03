44baa45b8d64fcaeb250804410fa527e5d6ac658
krypek <krypek@tuta.io>
2023-08-03 14:40:24 +0200

    Add ig.ENTITY.Door.Settings

--- modules/game.feature.map-content.sc-doors.d.ts
+++ modules/game.feature.map-content.sc-doors.d.ts
@@ -1,1 +1,17 @@
 // requires impact.feature.map-content.entities.door
+
+export {};
+
+declare global {
+  namespace ig {
+    enum DOOR_TYPE {
+      DEFAULT,
+      COLD_DUNGEON,
+      HEAT_DUNGEON_ENTRANCE,
+      HEAT_DUNGEON_MASTER_DOOR,
+      SHOCKWAVE_DNG_DOOR,
+      FINAL_DNG_DOOR,
+      FINAL_DNG_DOOR_SAPPHIRE,
+    }
+  }
+}
--- modules/impact.feature.map-content.entities.door.d.ts
+++ modules/impact.feature.map-content.entities.door.d.ts
@@ -3,3 +3,26 @@
 // requires impact.feature.map-content.entities.hidden-block
 // requires impact.feature.base.entities.object-layer-view
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace Door {
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        doorType?: keyof typeof ig.DOOR_TYPE;
+        transitionType?: "REGULAR" | "INTER_AREA";
+        narrow?: boolean;
+        map: string;
+        marker: string;
+        dir?: keyof typeof ig.ActorEntity.FACE4;
+        variation?: string;
+        condition?: string;
+        blockEvent?: unknown; // steps: e.blockEvent
+        blockEventCondition?: string;
+        hideCondition?: string;
+      }
+    }
+  }
+}
+
