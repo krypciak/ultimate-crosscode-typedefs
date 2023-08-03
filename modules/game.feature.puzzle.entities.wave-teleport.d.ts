cea87f08f49bd81b66da63cc75fa8254bc844105
krypek <krypek@tuta.io>
2023-08-03 20:47:54 +0200

    Add ig.ENTITY.WaveTeleport.Settings

--- modules/game.feature.puzzle.entities.wave-teleport.d.ts
+++ modules/game.feature.puzzle.entities.wave-teleport.d.ts
@@ -1,3 +1,15 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
 // requires game.feature.combat.combat-target-event
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace WaveTeleport {
+        interface Settings extends sc.MapModel.MapEntity.Settings {}
+      }
+    }
+  }
+}
