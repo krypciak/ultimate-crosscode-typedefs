5fd1a51006064becb69bc8407da63e663a852aa6
krypek <krypek@tuta.io>
2023-08-03 20:44:16 +0200

    Add ig.ENTITY.WaterBubblePanel.Settings

--- modules/game.feature.puzzle.entities.water-bubble.d.ts
+++ modules/game.feature.puzzle.entities.water-bubble.d.ts
@@ -1,2 +1,16 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace WaterBubblePanel {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          coalCoolTime?: number;
+        }
+      }
+    }
+  }
+}
