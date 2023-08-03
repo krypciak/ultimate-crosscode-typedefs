dfdc0452cbf78145961256ac156686e36a1eee06
krypek <krypek@tuta.io>
2023-08-04 00:35:49 +0200

    Add ig.ENTITY.ElementPole.Settings

--- modules/game.feature.puzzle.entities.thermo-pole.d.ts
+++ modules/game.feature.puzzle.entities.thermo-pole.d.ts
@@ -5,36 +5,21 @@
 export {};
 
 declare global {
-    namespace sc {
-        namespace ElementPoleGroups {
-            interface Group {
-                currentBall: ig.ENTITY.Ball | null;
-                hitCount: number;
-                poles: ig.ENTITY.ElementPole[];
-            }
-        }
-
-        interface ElementPoleGroups {
-            getGroup(group: string): ElementPoleGroups.Group;
-            onPoleHit(pole: ig.ENTITY.ElementPole, ball: ig.ENTITY.Ball, alreadyHit: boolean): boolean;
-            onCancelCheck(pole: ig.ENTITY.ElementPole): boolean;
-        }
-        let ElementPoleGroups: ElementPoleGroups;
+  namespace sc {
+    enum TERMO_POLE_TYPE {
+      LONG,
+      LONG64,
+      SHORT,
     }
+  }
 
-    namespace ig.ENTITY {
-        namespace ElementPole {
-            interface Charge {
-                element: sc.ELEMENT;
-            }
-        }
-        interface ElementPole extends ig.AnimatedEntity {
-            group: string;
-            charge: ElementPole.Charge;
-
-            ballHit(this: this, ball: ig.ENTITY.Ball): boolean;
-        }
-        interface ElementPoleConstructor extends ImpactClass<ElementPole> {}
-        let ElementPole: ElementPoleConstructor
+  namespace ig.ENTITY {
+    namespace ElementPole {
+      interface Settings extends sc.MapModel.MapEntity.Settings {
+        group: string;
+        spawnCondition?: string;
+        poleType?: keyof typeof sc.TERMO_POLE_TYPE;
+      }
     }
+  }
 }
