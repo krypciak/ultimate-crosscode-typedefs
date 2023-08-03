e2b0ffededc86ff023320592aae43ce618045d14
krypek <krypek@tuta.io>
2023-08-03 22:10:28 +0200

    Add ig.ENTITY.Compressor.Settings

--- modules/game.feature.puzzle.entities.compressor.d.ts
+++ modules/game.feature.puzzle.entities.compressor.d.ts
@@ -1,3 +1,19 @@
 // requires impact.base.entity
 // requires impact.base.actor-entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace Compressor {
+        interface Settings extends sc.MapModel.MapEntity.Settings {
+          followCamera: boolean;
+          basllSpeed?: number;
+          fastMode?: boolean;
+        }
+      }
+    }
+  }
+}
