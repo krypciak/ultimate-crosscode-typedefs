d935280e6544eb2dcd1a0de6e9ae89261778a989
krypek <krypek@tuta.io>
2023-09-29 19:11:42 +0200

    Add ig.ENTITY.Chest

--- modules/game.feature.puzzle.entities.chest.d.ts
+++ modules/game.feature.puzzle.entities.chest.d.ts
@@ -3,3 +3,37 @@
 // requires game.feature.interact.map-interact
 // requires game.feature.inventory.inventory
 // requires game.feature.menu.menu-model
+
+export {};
+
+declare global {
+    namespace sc {
+        enum CHEST_TYPE {
+            Default,
+            Bronze,
+            Silver,
+            Gold,
+            Key,
+            MasterKey,
+        }
+        enum CHEST_KEY {
+            BRONZE = 154,
+            SILVER = 155,
+            GOLD = 156,
+        }
+    }
+    namespace ig.ENTITY {
+        namespace Chest {
+            interface Settings extends ig.Entity.Settings {
+                trigger?: string
+                noTrack?: boolean
+                chestType?: keyof typeof sc.CHEST_TYPE
+                variable?: string
+                item: number
+                amount?: number
+                hideCondition?: string
+                detectCondition?: string
+            }
+        }
+    }
+}
