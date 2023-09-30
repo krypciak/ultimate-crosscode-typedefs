93d0e3553415238c669c6df51f4c0a60f5239de0
krypek <krypek@tuta.io>
2023-09-30 18:53:36 +0200

    Add ig.ENTITY.Destructible.Settings

--- modules/game.feature.puzzle.entities.destructible.d.ts
+++ modules/game.feature.puzzle.entities.destructible.d.ts
@@ -1,2 +1,43 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+    namespace sc {
+        enum DESTRUCTIBLE_TYPE {
+            autumnWall,
+            autumnWall2,
+            autumnWall3,
+            bombBlock,
+            bombWallEast,
+            bombWallNorth,
+            bombWallWest,
+            boxLarge,
+            boxMedEast,
+            boxMedNorth,
+            boxMedSouth,
+            boxMedWest,
+            boxMedium,
+            iceBlock,
+            keyPillar,
+            keyPillarAR,
+            keyWallNorth,
+            masterKeyWallColdDungeon,
+            masterKeyWallHeatDungeon,
+            masterKeyWallTreeDungeon,
+        }
+    }
+    namespace ig.ENTITY {
+        namespace Destructible {
+            interface Settings extends ig.Entity.Settings {
+                permaDestruct?: boolean
+                onDestructIncrease?: string
+                onPreDestructIncrease?: string
+                desType: keyof typeof sc.DESTRUCTIBLE_TYPE
+                enemyInfo?: sc.EnemyInfo
+                blockNavMap?: boolean
+            }
+        }
+    }
+}
