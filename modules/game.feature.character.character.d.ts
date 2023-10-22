fd039d2de7f4bc5df9e5503362e2b1c1215b4bfc
krypek <krypek@tuta.io>
2023-10-22 19:26:55 +0200

    Update sc.CharacterExpression

--- modules/game.feature.character.character.d.ts
+++ modules/game.feature.character.character.d.ts
@@ -8,8 +8,15 @@ declare global {
     interface CharacterConstructor extends ImpactClass<Character> {}
     let Character: Character;
 
-    interface CharacterExpression extends ig.Cacheable {}
-    interface CharacterExpressionConstructor extends ImpactClass<CharacterExpression> {}
+    interface CharacterExpression extends ig.Cacheable {
+      cacheType: 'CHAR_EXPRESSION'
+      character: sc.Character
+      expression: string
+      expressionImages: Record<string, ig.Image>
+    }
+    interface CharacterExpressionConstructor extends ImpactClass<CharacterExpression> {
+      new (character: string, expression: string): CharacterExpression
+    }
     var CharacterExpression: CharacterExpressionConstructor;
   }
 }
