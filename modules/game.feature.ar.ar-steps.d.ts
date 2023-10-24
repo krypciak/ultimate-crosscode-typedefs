0ed2c205302a558ebceaa58dbdd1ed00862a3411
krypek <krypek@tuta.io>
2023-10-24 23:27:48 +0200

    Add ig.EVENT_STEP.SHOW_AR_MSG

--- modules/game.feature.ar.ar-steps.d.ts
+++ modules/game.feature.ar.ar-steps.d.ts
@@ -1,3 +1,39 @@
 // requires impact.base.action
 // requires impact.base.event
 // requires game.feature.ar.gui.ar-box
+
+export {}
+
+declare global {
+    namespace ig {
+        namespace EVENT_STEP {
+            namespace SHOW_AR_MSG {
+                interface Settings {
+                    entity: { player?: boolean }
+                    text: ig.LangLabel
+                    time: number
+                    mode: sc.AR_BOX_MODE
+                    color: keyof sc.AR_COLOR
+                    hideOutsideOfScreen: boolean
+                    partName?: string
+                    varFill?: string
+                    varFillMax?: number
+                }
+            }
+            interface SHOW_AR_MSG extends ig.EventStepBase {
+                entity: ig.EVENT_STEP.SHOW_AR_MSG.Settings['entity']
+                text: ig.LangLabel
+                time: number
+                mode: boolean
+                color: string
+                partName?: string
+                varFill?: string
+                varFillMax?: number
+            }
+            interface SHOW_AR_MSGConstructor extends ImpactClass<SHOW_AR_MSG> {
+                new (settings: ig.EVENT_STEP.SHOW_AR_MSG.Settings): SHOW_AR_MSG
+            }
+            var SHOW_AR_MSG: SHOW_AR_MSGConstructor
+        }
+    }
+}
--- modules/game.feature.ar.gui.ar-box.d.ts
+++ modules/game.feature.ar.gui.ar-box.d.ts
@@ -1,1 +1,17 @@
 // requires impact.feature.gui.gui
+
+export {}
+
+declare global {
+    namespace sc {
+        enum AR_BOX_MODE {
+            NO_LINE = 0,
+            LINE_FILL = 1,
+            LINE_EMPTY = 2
+        }
+        interface AR_COLOR {
+            GREEN: { rgb: string, yOff: number }
+            RED: { rgb: string, yOff: number }
+        }
+    }
+}
