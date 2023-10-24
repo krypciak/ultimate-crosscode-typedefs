1fb6ad16c96a20bb5dfcb08241cb1a206291c170
krypek <115574014+krypciak@users.noreply.github.com>
2023-10-24 09:05:52 +0200

    Voice acting and CharacterExpression typedefs (#11)
    * Add sc.VoiceActing & sc.VA_CONFIG
    
    * Update sc.CharacterExpression
    
    * Update sc.Character
    
    * Fix ordering
    
    ---------
    
    Co-authored-by: Alyxia Sother <alyxia@riseup.net>

--- modules/game.feature.character.character.d.ts
+++ modules/game.feature.character.character.d.ts
@@ -4,12 +4,24 @@ export {};
 
 declare global {
   namespace sc {
-    interface Character extends ig.JsonLoadable {}
+    interface Character extends ig.JsonLoadable {
+      cacheType: 'Character'
+      data: unknown
+      name: string
+      faceImage: ig.Image
+    }
     interface CharacterConstructor extends ImpactClass<Character> {}
     let Character: Character;
 
-    interface CharacterExpression extends ig.Cacheable {}
-    interface CharacterExpressionConstructor extends ImpactClass<CharacterExpression> {}
+    interface CharacterExpression extends ig.Cacheable {
+      cacheType: 'CHAR_EXPRESSION'
+      character: sc.Character
+      expression: string
+      expressionImages: Record<string, ig.Image>
+    }
+    interface CharacterExpressionConstructor extends ImpactClass<CharacterExpression> {
+      new (character: string, expression: string): CharacterExpression
+    }
     var CharacterExpression: CharacterExpressionConstructor;
   }
 }
--- modules/game.feature.voice-acting.voice-acting.d.ts
+++ modules/game.feature.voice-acting.voice-acting.d.ts
@@ -1,1 +1,22 @@
 // requires impact.base.game
+
+export {}
+
+declare global {
+    namespace sc {
+        interface VoiceActing extends ig.GameAddon {
+            active: boolean
+            loaded: boolean
+            voices: Record<string, sc.VA_CONFIG.Entry[]>
+
+            toggle(this: this): void
+            load(this: this): void
+            loadConfigSounds(this: this, vaConfigEntry: sc.VA_CONFIG.Entry): unknown
+            play(this: this, expression: sc.CharacterExpression, label: ig.LangLabel): void
+        }
+        interface VoiceActingConstructor extends ImpactClass<VoiceActing> {
+            new (): VoiceActing
+        }
+        var VoiceActing: VoiceActingConstructor
+    }
+}
