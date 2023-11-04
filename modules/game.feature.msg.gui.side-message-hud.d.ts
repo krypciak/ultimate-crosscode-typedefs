549d090606ae90a07d7fd32c325a60150451af2a
krypek <krypek@tuta.io>
2023-11-04 23:55:13 +0100

    Add sc.getMessageTime & sc.SideMessageHudGui

--- modules/game.feature.msg.gui.side-message-hud.d.ts
+++ modules/game.feature.msg.gui.side-message-hud.d.ts
@@ -3,3 +3,19 @@
 // requires game.feature.gui.base.text
 // requires game.feature.gui.base.slick-box
 // requires game.feature.msg.gui.msg-skip-hud
+
+export {}
+
+declare global {
+    namespace sc {
+        function getMessageTime(textLike: sc.TextLike): number
+
+        interface SideMessageHudGui extends ig.GuiElementBase {
+            showNextSideMessage(this: this): void
+        }
+        interface SideMessageHudGuiConstructor extends ImpactClass<SideMessageHudGui> {
+            new (): SideMessageHudGui
+        }
+        var SideMessageHudGui: SideMessageHudGuiConstructor
+    }
+}
