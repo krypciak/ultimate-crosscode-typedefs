2e36dcbb33a32ed1f4ed7f548c420cd5003dada8
krypek <krypek@tuta.io>
2023-11-04 17:10:31 +0100

    Update sc.AutoControl

--- modules/game.feature.auto-control.auto-control.d.ts
+++ modules/game.feature.auto-control.auto-control.d.ts
@@ -4,7 +4,11 @@ export {};
 
 declare global {
   namespace sc {
-    interface AutoControl extends ig.GameAddon {}
+    interface AutoControl extends ig.GameAddon {
+      get(this: this, type: string): boolean;
+      isActive(this: this): boolean
+      setActive(this: this, value: boolean): void
+    }
     interface AutoControlConstructor extends ImpactClass<AutoControl> {
       new (): AutoControl;
     }
