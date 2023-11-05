5796c2c6e101f9267ec12b69ac842ebbe9a94b6d
krypek <krypek@tuta.io>
2023-11-05 13:20:51 +0100

    Add ig.Physics.TraceResult & ig.Physics.CollEntry

--- modules/impact.base.physics.d.ts
+++ modules/impact.base.physics.d.ts
@@ -5,6 +5,26 @@ export {};
 
 declare global {
   namespace ig {
+    namespace Physics {
+      interface TraceResult {
+        dir: Vec2;
+        dist: number;
+        levelUp: boolean;
+        forcePushEntries: unknown;
+        forcePushDirs: unknown;
+      }
+      interface CollEntry {
+        _inCollisionMap: boolean;
+        type: ig.COLLTYPE;
+        pos: Vec2;
+        size: Vec2;
+        padding: Vec2;
+        parentColl: CollEntry;
+        ignoreCollision: boolean;
+        weight: number;
+        entity: ig.Entity;
+      }
+    }
     interface Physics extends ig.Class {
       cellSize: number;
       _updateCount: number;
