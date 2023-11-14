a4999006927e25ff8e9aca2397a843856a4f39bd
krypek <krypek@tuta.io>
2023-11-14 22:44:17 +0100

    Add sc.QuickMenuAnalysis

--- modules/game.feature.quick-menu.gui.quick-screen.d.ts
+++ modules/game.feature.quick-menu.gui.quick-screen.d.ts
@@ -3,3 +3,19 @@
 // requires game.feature.quick-menu.gui.quick-screen-types
 // requires game.feature.menu.gui.menu-misc
 // requires game.feature.quick-menu.gui.quick-misc
+
+export { }
+
+declare global {
+    namespace sc {
+        interface QuickMenuAnalysis extends ig.GuiElementBase {
+            entities: sc.QuickMenuTypesBase[]
+
+            show(this: this): void
+        }
+        interface QuickMenuAnalysisConstructor extends ImpactClass<QuickMenuAnalysis> {
+            new(): QuickMenuAnalysis
+        }
+        var QuickMenuAnalysis: QuickMenuAnalysisConstructor
+    }
+}
