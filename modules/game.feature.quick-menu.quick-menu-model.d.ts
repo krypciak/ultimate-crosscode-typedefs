45009d853656caf395a4ae43820efa63bed9a870
krypek <krypek@tuta.io>
2023-11-14 19:04:52 +0100

    Add sc.QUICK_MENU_STATE & sc.QuickMenuModel

--- modules/game.feature.quick-menu.quick-menu-model.d.ts
+++ modules/game.feature.quick-menu.quick-menu-model.d.ts
@@ -2,3 +2,30 @@
 // requires impact.feature.interact.button-interact
 // requires impact.feature.database.database
 // requires game.feature.model.base-model
+
+export {}
+
+declare global {
+    namespace sc {
+        enum QUICK_MENU_STATE {
+            NONE = 0,
+            ITEMS = 1,
+            CHECK = 2,
+            PARTY = 3,
+            MAP = 4,
+        }
+        interface QuickMenuModel extends ig.GameAddon {
+            currentState: sc.QUICK_MENU_STATE
+            previousState: sc.QUICK_MENU_STATE
+            cursorMoved: boolean
+            cursor: Vec2
+
+            enterQuickMenu(this: this): void
+            exitQuickMenu(this: this): void
+        }
+
+        interface QuickMenuModelConstructor extends ImpactClass<QuickMenuModel> { new (): QuickMenuModel }
+        var QuickMenuModel: QuickMenuModelConstructor
+        var quickmodel: QuickMenuModel
+    }
+}
