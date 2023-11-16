74c139bbd0e31aa3bbbb34474e5edf2e7d60559c
krypek <krypek@tuta.io>
2023-11-16 18:44:56 +0100

    Add ig.ENTITY.Door

--- modules/impact.feature.map-content.entities.door.d.ts
+++ modules/impact.feature.map-content.entities.door.d.ts
@@ -4,25 +4,37 @@
 // requires impact.feature.base.entities.object-layer-view
 // requires impact.feature.effect.effect-sheet
 
-export {};
+export { };
 
 declare global {
-  namespace ig.ENTITY {
-    namespace Door {
-      interface Settings extends ig.Entity.Settings {
-        doorType?: keyof typeof ig.DOOR_TYPE;
-        transitionType?: "REGULAR" | "INTER_AREA";
-        narrow?: boolean;
-        map: string;
-        marker: string;
-        dir?: keyof typeof ig.ActorEntity.FACE4;
-        variation?: string;
-        condition?: string;
-        blockEvent?: unknown; // steps: e.blockEvent
-        blockEventCondition?: string;
-        hideCondition?: string;
-      }
+    namespace ig.ENTITY {
+        namespace Door {
+            interface Settings extends ig.Entity.Settings {
+                doorType?: keyof typeof ig.DOOR_TYPE;
+                transitionType?: "REGULAR" | "INTER_AREA";
+                narrow?: boolean;
+                map: string;
+                marker: string;
+                dir?: keyof typeof ig.ActorEntity.FACE4;
+                variation?: string;
+                condition?: string;
+                blockEvent?: unknown; // steps: e.blockEvent
+                blockEventCondition?: string;
+                hideCondition?: string;
+            }
+        }
+        interface Door extends ig.AnimatedEntity {
+            doorType: keyof typeof ig.DOOR_TYPE
+            map: string
+            marker: string
+            dir: keyof typeof ig.ActorEntity.FACE4
+            active: boolean
+            blockEventCondition: string
+        }
+        interface DoorConstructor extends ImpactClass<Door> {
+            new(x: number, y: number, z: number, settings: ig.ENTITY.Door.Settings): Door
+        }
+        var Door: DoorConstructor
     }
-  }
 }
 
