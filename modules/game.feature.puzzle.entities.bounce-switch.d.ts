0b0e4b1db1a44f40337ff3b6dedfcda5fc471e13
krypek <krypek@tuta.io>
2023-11-16 15:51:53 +0100

    Add ig.ENTITY.BounceBlock

--- modules/game.feature.puzzle.entities.bounce-switch.d.ts
+++ modules/game.feature.puzzle.entities.bounce-switch.d.ts
@@ -1,3 +1,22 @@
 // requires impact.base.entity
 // requires impact.base.game
 // requires impact.feature.effect.effect-sheet
+
+export { }
+
+declare global {
+    namespace ig {
+        namespace ENTITY {
+            namespace BounceBlock {
+                interface Settings {}
+            }
+            interface BounceBlock extends ig.AnimatedEntity {
+                blockState: number
+            }
+            interface BounceBlockConstructor extends ImpactClass<BounceBlock> {
+                new(x: number, y: number, z: number, settings: ig.ENTITY.BounceBlock.Settings): BounceBlock
+            }
+            var BounceBlock: BounceBlockConstructor
+        }
+    }
+}
