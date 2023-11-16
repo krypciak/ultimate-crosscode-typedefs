209960bf6d01059fa5b8b2ca7f88c59796b182f7
krypek <krypek@tuta.io>
2023-11-16 16:25:02 +0100

    Add sc.BounceSwitchGroups

--- modules/game.feature.puzzle.entities.bounce-switch.d.ts
+++ modules/game.feature.puzzle.entities.bounce-switch.d.ts
@@ -5,6 +5,31 @@
 export { }
 
 declare global {
+    namespace sc {
+        namespace BounceSwitchGroups {
+            interface Group {
+                endSwitch: null,
+                variable: string,
+                blocks: unknown[],
+                blockHitCount: number,
+                finalHit: boolean,
+                currentBall: null,
+                cameraHandle: null,
+                overrideHandle: null
+            }
+        }
+        interface BounceSwitchGroups extends ig.GameAddon {
+            groups: Record<string, sc.BounceSwitchGroups.Group>
+
+            isGroupBallConflict(this: this, groupName: string, entity: ig.Entity): boolean
+            getGroup(this: this, groupName: string): sc.BounceSwitchGroups.Group
+        }
+        interface BounceSwitchGroupsConstructor extends ImpactClass<BounceSwitchGroups> {
+            new (): BounceSwitchGroups
+        }
+        var BounceSwitchGroups: BounceSwitchGroupsConstructor
+        var bounceSwitchGroups: BounceSwitchGroups
+    }
     namespace ig {
         namespace ENTITY {
             namespace BounceBlock {
