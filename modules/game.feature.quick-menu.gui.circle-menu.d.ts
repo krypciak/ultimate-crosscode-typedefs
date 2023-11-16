2768fd79f7e3584066cf04efc11371091d00b8f2
krypek <krypek@tuta.io>
2023-11-16 19:35:22 +0100

    Add sc.RingMenuButton

--- modules/game.feature.quick-menu.gui.circle-menu.d.ts
+++ modules/game.feature.quick-menu.gui.circle-menu.d.ts
@@ -3,3 +3,18 @@
 // requires impact.feature.gui.base.basic-gui
 // requires impact.feature.interact.gui.focus-gui
 // requires game.feature.interact.button-group
+
+export { }
+
+declare global {
+    namespace sc {
+        interface RingMenuButton extends ig.FocusGui {
+            state: sc.QUICK_MENU_STATE
+            data: string
+        }
+        interface RingMenuButtonConstructor extends ImpactClass<RingMenuButton> {
+            new(state: sc.QUICK_MENU_STATE, endPosX: number, endPosY: number): RingMenuButton
+        }
+        var RingMenuButton: RingMenuButtonConstructor
+    }
+}
