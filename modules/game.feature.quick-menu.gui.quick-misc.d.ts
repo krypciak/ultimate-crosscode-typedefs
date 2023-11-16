ee718b8f2a1566d15843beca2555dc597f1cdc51
krypek <krypek@tuta.io>
2023-11-16 20:01:05 +0100

    Add sc.QuickArrowBox

--- modules/game.feature.quick-menu.gui.quick-misc.d.ts
+++ modules/game.feature.quick-menu.gui.quick-misc.d.ts
@@ -6,36 +6,45 @@
 // requires game.feature.menu.gui.enemies.enemy-pages
 // requires game.feature.quick-menu.gui.quick-item-menu
 
-export {};
+export { };
 
 declare global {
-  namespace sc {
-    interface QuickFocusScreen extends ig.GuiElementBase {
-      addSubGui(this: this, gui: ig.GuiElementBase): void
-    }
-    interface QuickFocusScreenConstructor extends ImpactClass<QuickFocusScreen> { new (): QuickFocusScreen }
-    var QuickFocusScreen: QuickFocusScreenConstructor
+    namespace sc {
+        interface QuickFocusScreen extends ig.GuiElementBase {
+            addSubGui(this: this, gui: ig.GuiElementBase): void
+        }
+        interface QuickFocusScreenConstructor extends ImpactClass<QuickFocusScreen> { new(): QuickFocusScreen }
+        var QuickFocusScreen: QuickFocusScreenConstructor
 
-    interface QuickBorderArrowLevelBox extends ig.GuiElementBase {
-      ninepatch: ig.NinePatch;
-      levelNumber: sc.NumberGui;
-      displayColor: number;
-      sizeTransition: ig.GuiHook.SizeTransition;
-      nameVisible: boolean;
+        interface QuickArrowBox extends ig.GuiElementBase {
+            name: sc.TextGui
+            arrowOff: Vec2
+        }
+        interface QuickArrowBoxConstructor extends ImpactClass<QuickArrowBox> {
+            new(text: string, notFixedHeight: boolean, maxWidth: number): QuickArrowBox
+        }
+        var QuickArrowBox: QuickArrowBoxConstructor
 
-      doSizeTransition(
-        this: this,
-        width: Optional<number>,
-        height: Optional<number>,
-        time: number,
-        timeFunction: KeySpline,
-        delay: Optional<number>,
-      ): void;
-      getLevelColor(enemyLevel: number): sc.GUI_NUMBER_COLOR;
+        interface QuickBorderArrowLevelBox extends ig.GuiElementBase {
+            ninepatch: ig.NinePatch;
+            levelNumber: sc.NumberGui;
+            displayColor: number;
+            sizeTransition: ig.GuiHook.SizeTransition;
+            nameVisible: boolean;
+
+            doSizeTransition(
+                this: this,
+                width: Optional<number>,
+                height: Optional<number>,
+                time: number,
+                timeFunction: KeySpline,
+                delay: Optional<number>,
+            ): void;
+            getLevelColor(enemyLevel: number): sc.GUI_NUMBER_COLOR;
+        }
+        interface QuickBorderArrowLevelBoxConstructor extends ImpactClass<QuickBorderArrowLevelBox> {
+            new(enemyName: string, entity: ig.ENTITY.Enemy): QuickBorderArrowLevelBox;
+        }
+        var QuickBorderArrowLevelBox: QuickBorderArrowLevelBoxConstructor;
     }
-    interface QuickBorderArrowLevelBoxConstructor extends ImpactClass<QuickBorderArrowLevelBox> {
-      new (enemyName: string, entity: ig.ENTITY.Enemy): QuickBorderArrowLevelBox;
-    }
-    var QuickBorderArrowLevelBox: QuickBorderArrowLevelBoxConstructor;
-  }
 }
