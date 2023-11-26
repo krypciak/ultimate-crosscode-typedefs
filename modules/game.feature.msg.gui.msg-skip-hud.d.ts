efed20ecb74baddac6421f3bdf4f2f0a52067449
krypek <krypek@tuta.io>
2023-11-26 12:28:02 +0100

    Add sc.MsgSkipGui

--- modules/game.feature.msg.gui.msg-skip-hud.d.ts
+++ modules/game.feature.msg.gui.msg-skip-hud.d.ts
@@ -1,3 +1,23 @@
 // requires impact.feature.gui.gui
 // requires game.feature.gui.base.boxes
 // requires game.feature.gui.base.text
+
+export { }
+
+declare global {
+    namespace sc {
+        interface MsgSkipGui extends ig.GuiElementBase {
+            text: sc.TextGui
+            box: sc.SmallBlackBoxGui
+            personEntry: unknown
+            timer: number
+
+            hide(this: this): void
+            show(this: this): void
+        }
+        interface MsgSkipGuiConstructor extends ImpactClass<MsgSkipGui> {
+            new(): MsgSkipGui
+        }
+        var MsgSkipGui: MsgSkipGuiConstructor
+    }
+}
