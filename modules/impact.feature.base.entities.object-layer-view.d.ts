bb3c1ce3c1d94e019c49d3b3f600a85d0de39ab0
krypek <krypek@tuta.io>
2023-11-26 12:29:44 +0100

    Add ig.EntityHideManager

--- modules/impact.feature.base.entities.object-layer-view.d.ts
+++ modules/impact.feature.base.entities.object-layer-view.d.ts
@@ -1,1 +1,20 @@
 // requires impact.base.actor-entity
+
+export { }
+
+declare global {
+    namespace ig {
+        interface EntityHideManager extends ig.Class {
+            hideCondition: ig.VarCondition
+            hideTimer: number
+            hidden: boolean
+            efficentMode: boolean
+
+            isBusy(this: this): number
+        }
+        interface EntityHideManagerConstructor extends ImpactClass<EntityHideManager> {
+            new(condition: string): EntityHideManager
+        }
+        var EntityHideManager: EntityHideManagerConstructor
+    }
+}
