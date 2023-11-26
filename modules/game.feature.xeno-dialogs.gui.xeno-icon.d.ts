06e225b7cd8a70a2de67cff00c3ad5a2c58a6bb2
krypek <krypek@tuta.io>
2023-11-26 12:29:35 +0100

    Add sc.XenoDialogIcon

--- modules/game.feature.xeno-dialogs.gui.xeno-icon.d.ts
+++ modules/game.feature.xeno-dialogs.gui.xeno-icon.d.ts
@@ -3,3 +3,22 @@
 // requires impact.feature.gui.base.basic-gui
 // requires game.feature.interact.map-interact
 // requires game.feature.msg.gui.msg-skip-hud
+
+export { }
+
+declare global {
+    namespace sc {
+        interface XenoDialogIcon extends ig.BoxGui {
+            ninepatch: ig.NinePatch
+            textGui: sc.TextGui
+            skipGui: sc.MsgSkipGui
+            xenoDialog: ig.ENTITY.XenoDialog
+            skipEntry: sc.SkipInteractEntry
+        }
+        interface XenoDialogIconConstructor extends ImpactClass<XenoDialogIcon> {
+            new(): XenoDialogIcon
+        }
+        var XenoDialogIcon: XenoDialogIconConstructor
+
+    }
+}
