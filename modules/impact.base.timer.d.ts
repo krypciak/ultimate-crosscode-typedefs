8432d2d9c573afea276964e3710430b8a1bbc9d2
krypek <krypek@tuta.io>
2023-11-27 20:14:01 +0100

    Add ig.Timer

--- modules/impact.base.timer.d.ts
+++ modules/impact.base.timer.d.ts
@@ -3,6 +3,31 @@ export { }
 
 declare global {
     namespace ig {
+        interface Timer extends ig.Class {
+            target: number
+            base: number
+            last: number
+            stopped: boolean
+            
+            set(this: this, seconds: number, keepRelativeDelta: boolean): void //todo
+            reverseRelativeDelta(this: this): void
+            stop(this: this): void
+            resume(this: this): void
+            reset(this: this): void
+            tick(this: this): number
+            weight(this: this): number
+            delta(this: this): number
+        }
+        interface TimerConstructor extends ImpactClass<Timer> {
+            _last: number
+            time: number
+            timeScale: number
+            maxStep: number
+
+            new(target: number): Timer
+        }
+        var Timer: TimerConstructor
+
         enum TIMER_MODE { ONCE = 0, REPEAT = 1, BLINK = 2, SINUS = 3, SINUS_RND = 4 }
         interface WeightTimer extends ig.Class {
             duration: number
