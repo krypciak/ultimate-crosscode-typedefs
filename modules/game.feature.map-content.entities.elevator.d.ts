9b13bbae53db0ca256b64b4e7640f18769784771
krypek <krypek@tuta.io>
2023-11-28 21:28:07 +0100

    Add ig.ENTITY.Elevator

--- modules/game.feature.map-content.entities.elevator.d.ts
+++ modules/game.feature.map-content.entities.elevator.d.ts
@@ -1,3 +1,20 @@
 // requires impact.base.entity
 // requires game.feature.interact.map-interact
 // requires impact.base.actor-entity
+
+export { }
+
+declare global {
+    namespace ig {
+        namespace ENTITY {
+            namespace Elevator {
+                interface Settings { }
+            }
+            interface Elevator extends ig.Entity { }
+            interface ElevatorConstructor extends ImpactClass<Elevator> {
+                new(x: number, y: number, z: number, settings: ig.ENTITY.Elevator.Settings): Elevator
+            }
+            var Elevator: ElevatorConstructor
+        }
+    }
+}
