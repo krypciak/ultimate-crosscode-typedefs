054559bf0be068623a0f7cfd0f35412f89b68cb3
krypek <krypek@tuta.io>
2023-11-28 21:28:24 +0100

    Add ig.Terrain

--- modules/impact.feature.terrain.terrain.d.ts
+++ modules/impact.feature.terrain.terrain.d.ts
@@ -1,2 +1,18 @@
 // requires impact.base.loader
 // requires game.config
+
+export { }
+
+declare global {
+    namespace ig {
+        interface Terrain extends ig.SingleLoadable {
+            isFallTerrain(this: this, terrain: ig.TERRAIN): boolean
+            getTerrain(this: this, coll: ig.CollEntry, precise?: boolean, andBelow?: boolean): ig.TERRAIN
+        }
+        interface TerrainConstructor extends ImpactClass<Terrain> {
+            new(): Terrain
+        }
+        var Terrain: TerrainConstructor
+        var terrain: Terrain
+    }
+}
