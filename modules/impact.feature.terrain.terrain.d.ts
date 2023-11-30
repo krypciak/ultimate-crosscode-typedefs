4fb6fc541713fa675c91dbfe14d72fc3f2ef7e87
krypek <krypek@tuta.io>
2023-11-30 20:52:49 +0100

    Add getMapTerrain to ig.Terrain

--- modules/impact.feature.terrain.terrain.d.ts
+++ modules/impact.feature.terrain.terrain.d.ts
@@ -8,6 +8,7 @@ declare global {
         interface Terrain extends ig.SingleLoadable {
             isFallTerrain(this: this, terrain: ig.TERRAIN): boolean
             getTerrain(this: this, coll: ig.CollEntry, precise?: boolean, andBelow?: boolean): ig.TERRAIN
+            getMapTerrain(this: this, x: number, y: number, level: number, sizeX: number, sizeY: number): ig.TERRAIN
         }
         interface TerrainConstructor extends ImpactClass<Terrain> {
             new(): Terrain
