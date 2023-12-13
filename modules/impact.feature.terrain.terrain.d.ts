19fffd0f32baddb7a817c572179fdfe6506ffa7e
krypek <krypek@tuta.io>
2023-12-13 19:00:23 +0100

    Add tilesetIds to ig.Terrain

--- modules/impact.feature.terrain.terrain.d.ts
+++ modules/impact.feature.terrain.terrain.d.ts
@@ -6,6 +6,8 @@ export { }
 declare global {
     namespace ig {
         interface Terrain extends ig.SingleLoadable {
+            tilesetIds: Record<string, (number | string)[]>
+
             isFallTerrain(this: this, terrain: ig.TERRAIN): boolean
             getTerrain(this: this, coll: ig.CollEntry, precise?: boolean, andBelow?: boolean): ig.TERRAIN
             getMapTerrain(this: this, x: number, y: number, level: number, sizeX: number, sizeY: number): ig.TERRAIN
