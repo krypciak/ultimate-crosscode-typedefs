f7a8b53a4a94ad4ac5c4746fb3a2dda65d486e09
krypek <krypek@tuta.io>
2023-12-14 19:18:30 +0100

    Add ig.ENTITY.Elevator & sc.ElevatorSwitchEntity

--- modules/game.feature.map-content.entities.elevator.d.ts
+++ modules/game.feature.map-content.entities.elevator.d.ts
@@ -10,11 +10,31 @@ declare global {
             namespace Elevator {
                 interface Settings { }
             }
-            interface Elevator extends ig.Entity { }
+            interface Elevator extends ig.Entity {
+                condition: ig.VarCondition
+                switchEntity: sc.ElevatorSwitchEntity
+            }
             interface ElevatorConstructor extends ImpactClass<Elevator> {
                 new(x: number, y: number, z: number, settings: ig.ENTITY.Elevator.Settings): Elevator
             }
             var Elevator: ElevatorConstructor
         }
+    }
+    namespace sc {
+        namespace ElevatorSwitchEntity {
+            interface Settings {
+                ground: ig.ENTITY.Elevator
+                // elevatorData: sc.ELEVATOR_TYPE
+            }
+        }
+        interface ElevatorSwitchEntity extends ig.AnimatedEntity {
+            groundEntity: ig.ENTITY.Elevator
+            interactEntry: sc.MapInteractEntry
+        }
+        interface ElevatorSwitchEntityConstructor extends ImpactClass<ElevatorSwitchEntity> {
+            new(x: number, y: number, z: number, settings: sc.ElevatorSwitchEntity.Settings): ElevatorSwitchEntity
+        }
+        var ElevatorSwitchEntity: ElevatorSwitchEntityConstructor
+
     }
 }
