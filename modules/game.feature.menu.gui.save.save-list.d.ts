e395e858d661152d74b05a8808cac8f6ee379634
krypek <krypek@tuta.io>
2023-12-20 16:28:43 +0100

    Add sc.SaveAutoSaveSlot

--- modules/game.feature.menu.gui.save.save-list.d.ts
+++ modules/game.feature.menu.gui.save.save-list.d.ts
@@ -5,3 +5,18 @@
 // requires game.feature.menu.gui.menu-misc
 // requires game.feature.menu.gui.list-boxes
 // requires game.feature.menu.gui.save.save-misc
+
+export { }
+
+declare global {
+    namespace sc {
+        interface SaveAutoSaveSlot extends ig.BoxGui {
+            button: null /* i swear this is never set */
+        }
+        interface SaveAutoSaveSlotConstructor extends ImpactClass<SaveAutoSaveSlot> {
+            new(): SaveAutoSaveSlot
+        }
+        var SaveAutoSaveSlot: SaveAutoSaveSlotConstructor
+
+    }
+}
