07c1b13c75fbfec4b2fbc4e9cab13a5588c6a0c6
krypek <krypek@tuta.io>
2023-12-20 16:23:20 +0100

    Add sc.LoreModel

--- modules/game.feature.menu.lore-model.d.ts
+++ modules/game.feature.menu.lore-model.d.ts
@@ -1,3 +1,19 @@
 // requires impact.base.game
 // requires game.feature.model.base-model
 // requires impact.feature.storage.storage
+
+export { }
+
+declare global {
+    namespace sc {
+        interface LoreModel extends ig.GameAddon {
+            observers: unknown[]
+            lories: ig.Database.Data['lore']
+            unlockedLories: Record<keyof ig.Database.Data['lore'], Record<string, boolean>>
+        }
+        interface LoreModelConstructor extends ImpactClass<LoreModel> {
+            new(): LoreModel
+        }
+        var LoreModel: LoreModelConstructor
+    }
+}
