e2bdf07dc7dfbeb07388670d0ca116b3c8f761b3
krypek <krypek@tuta.io>
2023-12-23 16:52:52 +0100

    Add scramble to sc.NumberGui

--- modules/game.feature.gui.base.numbers.d.ts
+++ modules/game.feature.gui.base.numbers.d.ts
@@ -44,9 +44,10 @@ declare global {
       }
     }
     interface NumberGui extends ig.GuiElementBase {
-      showPlus: boolean;
       maxNumber: number;
+      showPlus: boolean;
       targetNumber: number; 
+      scramble: boolean
 
       setNumber(this: this, number: number, skipTransition?: Optional<boolean>): void;
       setMaxNumber(this: this, number: number): void;
