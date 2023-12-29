08aabd80978b24c978d8fc8a00fa5d61fc129621
krypek <krypek@tuta.io>
2023-12-29 11:29:31 +0100

    Add ig.ENTITY.EnemyCounter

--- modules/game.feature.puzzle.entities.enemy-counter.d.ts
+++ modules/game.feature.puzzle.entities.enemy-counter.d.ts
@@ -15,5 +15,30 @@ declare global {
         shuffleCondition?: string;
       }
     }
+    interface EnemyCounter extends ig.AnimatedEntity {
+      enemyGroup: string;
+      enemyCount: number;
+      preCount: number;
+      postCount: number;
+      preVariable: string;
+      postVariable: string;
+      done: boolean;
+      shuffleCondition: ig.VarCondition;
+      timer: number;
+      MAX_FLASH_TIME: number;
+      sounds: { count: ig.Sound; done: ig.Sound };
+      effects: { sheet: ig.EffectSheet; hideHandle: ig.ENTITY.Effect };
+
+      decreaseCount(this: this): void;
+    }
+    interface EnemyCounterConstructor extends ImpactClass<EnemyCounter> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.EnemyCounter.Settings,
+      ): EnemyCounter;
+    }
+    var EnemyCounter: EnemyCounterConstructor;
   }
 }
