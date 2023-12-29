10d26e95d4bdfbfac1b832006291bac297bcbd1e
krypek <krypek@tuta.io>
2023-12-29 19:30:51 +0100

    Add sc.AssaultTools

--- modules/game.feature.combat.combat-assault.d.ts
+++ modules/game.feature.combat.combat-assault.d.ts
@@ -1,2 +1,18 @@
 // requires game.feature.combat.model.combat-params
 // requires game.feature.combat.entities.ball
+
+export {};
+
+declare global {
+  namespace sc {
+    var ASSAULT_PROJECTILES: Record<sc.ELEMENT, sc.BallInfo>;
+
+    namespace AssaultTools {
+      function spawn(
+        entity: ig.ENTITY.Combatant,
+        projectileType: keyof typeof sc.ASSAULT_PROJECTILES,
+        multi: number,
+      ): void;
+    }
+  }
+}
