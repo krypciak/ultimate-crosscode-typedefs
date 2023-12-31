5d6ab35644d77099e804d27b5bde8e302368c9b2
krypek <krypek@tuta.io>
2023-12-31 17:42:16 +0100

    Add a constructor to sc.SavePreset

--- modules/game.feature.save-preset.save-preset.d.ts
+++ modules/game.feature.save-preset.save-preset.d.ts
@@ -11,16 +11,20 @@ declare global {
       sub: any;
       saveSlot: string;
 
-      onCacheCleared(): void;
-      getJsonPath(): string;
-      onload(preset: SavePresetData): void
+      onCacheCleared(this: this): void;
+      getJsonPath(this: this): string;
+      onload(this: this, preset: SavePresetData): void;
     }
 
     interface SavePreset extends ig.GameAddon {
       slots: SavePresetData[];
 
-      load(id: number): void;
+      load(this: this, id: number): void;
     }
+    interface SavePresetConstructor extends ImpactClass<SavePreset> {
+      new (): SavePreset;
+    }
+    var SavePreset: SavePresetConstructor;
 
     var savePreset: SavePreset;
   }
