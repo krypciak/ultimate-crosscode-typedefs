417187e8a70a5d8c20b2fd01417233e3cca8a8e3
krypek <krypek@tuta.io>
2024-01-07 19:46:20 +0100

    Add sc.PropInteract

--- modules/game.feature.map-content.prop-interact.d.ts
+++ modules/game.feature.map-content.prop-interact.d.ts
@@ -1,2 +1,40 @@
 // requires impact.feature.map-content.entities.prop
 // requires game.feature.map-content.gui.icon-hover-text
+
+export {};
+
+declare global {
+  namespace sc {
+    interface PROP_INTERACT_ICONS_BASE {
+      noEvent: sc.MapInteractIcon;
+      withEvent: sc.MapInteractIcon;
+    }
+    var PROP_INTERACT_ICONS: {
+      INFO: sc.PROP_INTERACT_ICONS_BASE;
+      GRAB: sc.PROP_INTERACT_ICONS_BASE;
+    };
+
+    interface PropInteract extends ig.Class {
+      prop: ig.ENTITY.Prop;
+      icon: sc.PROP_INTERACT_ICONS_BASE;
+      interactEntry: sc.MapInteractEntry;
+      permaEffect?: ig.EffectHandle;
+      event?: ig.Event;
+      combatOkay: boolean;
+      cutsceneType?: ig.EVENT_TYPE;
+
+      onShow(this: this): true | undefined;
+      onPermaUpdate(this: this): void;
+      onHide(this: this): void;
+      onKill(this: this): void;
+      onInteraction(this: this): boolean;
+    }
+    interface PropInteractConstructor extends ImpactClass<PropInteract> {
+      new (prop: ig.ENTITY.Prop): PropInteract;
+    }
+    var PropInteract: PropInteractConstructor;
+  }
+  namespace ig {
+    type PROP_INTERACT_CLASS = sc.PropInteractConstructor;
+  }
+}
