91de02f701c1fadae7ac71973721bd91f62ebd38
krypek <krypek@tuta.io>
2024-01-24 19:37:16 +0100

    Update sc.TabbedPane & sc.ListTabbedPane

--- modules/game.feature.menu.gui.tab-box.d.ts
+++ modules/game.feature.menu.gui.tab-box.d.ts
@@ -8,21 +8,67 @@ export {};
 
 declare global {
   namespace sc {
+    namespace TabbedPane {
+      interface Settings {
+        /* this may be incomplemte */
+        type: number;
+        skipSounds?: boolean;
+      }
+      interface Content {
+        buttongroup: sc.ButtonGroup;
+        list: sc.ButtonListBox;
+        sort: number;
+      }
+    }
     interface TabbedPane extends ig.GuiElementBase {
       keys: string[];
-      tabs: { [key: string]: sc.ItemTabbedBox.TabButton };
+      tabs: Record<string, sc.ItemTabbedBox.TabButton>;
       tabArray: sc.ItemTabbedBox.TabButton[];
+      tabContent: sc.TabbedPane.Content[];
+      currentTabIndex: number;
+      currentContent: sc.TabbedPane.Content;
+      cacheContent: boolean;
+      _prevIndex: number;
 
       setPanelSize(this: this, width: number, height: number): void;
-      addTab(this: this, key: string, index: number, settings: unknown): void;
+      addTab(this: this, key: string, index: number, settings: sc.TabbedPane.Settings): void;
+      setTab(
+        this: this,
+        index: number,
+        ignorePrev?: boolean,
+        settings?: sc.TabbedPane.Settings,
+      ): void;
+      show(this: this, tabIndex?: number): void;
+      hide(this: this): void;
       rearrangeTabs(this: this): void;
-
       onTabButtonCreation(
         this: this,
         key: string,
         index: number,
         settings: unknown,
       ): sc.ItemTabbedBox.TabButton;
+      getCurrentTabButton(this: this): null | sc.ItemTabbedBox.TabButton;
+      getCurrentTabKey(this: this): string;
+      getButtonIndex(this: this, tab: sc.ItemTabbedBox.TabButton): number;
+
+      onTabSelected(this: this, button: sc.ItemTabbedBox.TabButton): void;
+      onTabPressed(this: this, button: sc.ItemTabbedBox.TabButton, wasSame: boolean): void;
+      onTabButtonCreation(
+        this: this,
+        key: string,
+        index: number,
+        settings: sc.TabbedPane.Settings,
+      ): void;
+      onTabMouseFocusLost(this: this): void;
+      onTabChanged(this: this, newIndex: number, prevIndex: number): void;
+      onClearPrevContent(this: this, prevContent: unknown): void;
+      onContentCreation(this: this, index: number, settings: sc.TabbedPane.Settings): void;
+      onSetCacheContent(
+        this: this,
+        content: sc.TabbedPane.Content,
+        index: number,
+        settings: sc.TabbedPane.Settings,
+      ): void;
     }
     interface TabbedPaneConstructor extends ImpactClass<TabbedPane> {
       new (cacheContent?: boolean): TabbedPane;
@@ -30,6 +76,8 @@ declare global {
     var TabbedPane: TabbedPaneConstructor;
 
     interface ListTabbedPane extends sc.TabbedPane {
+      currentList: sc.ButtonListBox;
+      currentGroup: sc.ButtonGroup;
       bg: sc.MenuScanLines;
 
       onCreateListEntries(
@@ -40,7 +88,9 @@ declare global {
         sort: number,
       ): void;
     }
-    interface ListTabbedPaneConstructor extends ImpactClass<ListTabbedPane> {}
+    interface ListTabbedPaneConstructor extends ImpactClass<ListTabbedPane> {
+      new (cacheContent?: boolean): ListTabbedPane;
+    }
     var ListTabbedPane: ListTabbedPaneConstructor;
   }
 }
