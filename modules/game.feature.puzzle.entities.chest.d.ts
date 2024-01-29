6d58960240d2f981d4cc4147825ca79bea263c24
krypek <krypek@tuta.io>
2024-01-29 14:35:46 +0100

    Add ig.ENTITY.Chest

--- modules/game.feature.puzzle.entities.chest.d.ts
+++ modules/game.feature.puzzle.entities.chest.d.ts
@@ -7,33 +7,67 @@
 export {};
 
 declare global {
-    namespace sc {
-        enum CHEST_TYPE {
-            Default,
-            Bronze,
-            Silver,
-            Gold,
-            Key,
-            MasterKey,
-        }
-        enum CHEST_KEY {
-            BRONZE = 154,
-            SILVER = 155,
-            GOLD = 156,
-        }
+  namespace sc {
+    enum CHEST_TYPE {
+      Default,
+      Bronze,
+      Silver,
+      Gold,
+      Key,
+      MasterKey,
     }
-    namespace ig.ENTITY {
-        namespace Chest {
-            interface Settings extends ig.Entity.Settings {
-                trigger?: string
-                noTrack?: boolean
-                chestType?: keyof typeof sc.CHEST_TYPE
-                variable?: string
-                item: number
-                amount?: number
-                hideCondition?: string
-                detectCondition?: string
-            }
-        }
+    enum CHEST_KEY {
+      BRONZE = 154,
+      SILVER = 155,
+      GOLD = 156,
     }
+  }
+  namespace ig.ENTITY {
+    namespace Chest {
+      interface Settings extends ig.Entity.Settings {
+        trigger?: string;
+        noTrack?: boolean;
+        chestType?: keyof typeof sc.CHEST_TYPE;
+        variable?: string;
+        item: number;
+        amount?: number;
+        hideCondition?: string;
+        detectCondition?: string;
+      }
+    }
+    interface Chest extends ig.AnimatedEntity {
+      mapIcons: sc.MapInteractIcon;
+      mapIconsKey: sc.MapInteractIcon;
+      effects: { sheet: ig.EffectSheet; handle?: ig.ENTITY.Effect };
+      interactEntry: sc.MapInteractEntry;
+      hideHandle?: ig.ENTITY.Effect;
+      isOpen: boolean;
+      chestVariable: boolean;
+      item: sc.ItemID;
+      amount: number;
+      chestType: sc.CHEST_TYPE;
+      lockedIcon: boolean;
+      _itemName: string;
+      _trigger?: string;
+      _initialized: boolean;
+      _noTrack: boolean;
+
+      _initGfx(this: this): void;
+      onEffectEvent(this: this, handle: { isDone: () => boolean }): void;
+      show(this: this, noEffects?: boolean): void;
+      _updateIcon(this: this): void;
+      onHideRequest(this: this): void;
+      update(this: this): void;
+      onInteraction(this: this): boolean | undefined;
+      openUp(this: this): void;
+      _reallyOpenUp(this: this): void;
+      isOpened(this: this): boolean;
+      animationEnded(this: this, animName?: string): void;
+      varsChanged(this: this): void;
+    }
+    interface ChestConstructor extends ImpactClass<Chest> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.Chest.Settings): Chest;
+    }
+    var Chest: ChestConstructor;
+  }
 }
