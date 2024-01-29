bb34740ef8c3dccc27433ffaa86c0f256c61fa73
krypek <krypek@tuta.io>
2024-01-29 15:24:51 +0100

    Update sc.MultiPageBoxGui

--- modules/game.feature.menu.gui.help-boxes.d.ts
+++ modules/game.feature.menu.gui.help-boxes.d.ts
@@ -6,10 +6,56 @@ export {};
 
 declare global {
   namespace sc {
-    interface MultiPageBoxGui extends ig.GuiElementBase {
-      header: sc.TextGui;
+    interface MultiPagePageCounter extends ig.GuiElementBase {
+      count: sc.NumberGui;
+      max: sc.NumberGui;
 
+      setCount(this: this, count: number): void;
+      setMax(this: this, max: number): void;
+    }
+    interface MultiPagePageCounterConstructor extends ImpactClass<MultiPagePageCounter> {
+      new (width: number): MultiPagePageCounter;
+    }
+    var MultiPagePageCounter: MultiPagePageCounterConstructor;
+
+    namespace MultiPageBoxGui {
+      interface Page {
+        header?: Optional<string>;
+        title?: Optional<string>;
+        content: string[]
+        isList?: boolean;
+      }
+      interface ConditionalPage extends Page {
+        condition?: string;
+      }
+    }
+    interface MultiPageBoxGui extends ig.GuiElementBase {
+      msgBox: sc.CenterBoxGui;
+      content: ig.GuiElementBase;
+      header: sc.TextGui;
+      buttonInteract: ig.ButtonInteractEntry;
+      buttonGroup: sc.ButtonGroup;
+      defaultHeaderText: string;
+      linePadding: number;
+      listPadding: number;
+      turnLeft: sc.ButtonGui;
+      turnRight: sc.ButtonGui;
+      pageCounter: sc.MultiPagePageCounter;
+      pages: sc.MultiPageBoxGui.Page[];
+      curPage: number;
+      backSound: ig.Sound;
+      _width: number;
+
+      openMenu(this: this): void;
+      closeMenu(this: this): void;
+      addPage(this: this, header: string, content: string, isList?: boolean): void;
+      addPages(this: this, pages: sc.MultiPageBoxGui.ConditionalPage[]): void;
+      setDefaultHeaderText(this: this, header: string): void;
+      _setPage(this: this, index: number): void;
+      addListEntry(this: this, langLabel: string, parent: ig.GuiElementBase, y: number): number;
       _createInitContent(this: this, width: number): void;
+      onTurnLeftCheck(this: this): boolean;
+      onTurnRightCheck(this: this): boolean;
     }
     interface MultiPageBoxGuiConstructor extends ImpactClass<MultiPageBoxGui> {}
     var MultiPageBoxGui: MultiPageBoxGuiConstructor;
