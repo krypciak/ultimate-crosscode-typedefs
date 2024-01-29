3e6fa795a82782b27c1aa832182b9cea6b3bd515
krypek <krypek@tuta.io>
2024-01-29 14:33:55 +0100

    Add ig.NavBlocker

--- modules/impact.feature.navigation.navigation.d.ts
+++ modules/impact.feature.navigation.navigation.d.ts
@@ -1,42 +1,64 @@
 // requires impact.base.game
 
-export { }
+export {};
 
 declare global {
-    namespace ig {
-        interface NavPath extends ig.Class {
-            mapVersion: number
-            searcher: unknown
-            startRelativeVel: number
-            targetEntity: ig.Entity
-            targetPos: Vec3
-            retargetPos: Vec3
-            retargetNode: unknown
-            maxDistance: number
-            precise: boolean
-            options: unknown
-            path: unknown
-            pathIdx: number
-            pathLength: number
-            nextNodeData: { startPos: Vec2, endPos: Vec2, jump: boolean, height: number },
-            overNodePass: boolean
-            pathComplete: boolean
-            triedNodePassChange: boolean
-            triedRandom: number
-            doBackUp: boolean
-            triedBackUp: number
-            avoidTarget: Vec2
-            avoidTarget2: Vec2
-            firstMovement: boolean
-            targetDir: Vec2
-            targetDist: number
-            failCount: number
-            wrongNodeTimer: number
-            lastSideWayDir: Vec2
-        }
-        interface NavPathConstructor extends ImpactClass<NavPath> {
-            new(searcher: unknown): NavPath
-        }
-        var NavPath: NavPathConstructor
+  namespace ig {
+    interface NavPath extends ig.Class {
+      mapVersion: number;
+      searcher: unknown;
+      startRelativeVel: number;
+      targetEntity: ig.Entity;
+      targetPos: Vec3;
+      retargetPos: Vec3;
+      retargetNode: unknown;
+      maxDistance: number;
+      precise: boolean;
+      options: unknown;
+      path: unknown;
+      pathIdx: number;
+      pathLength: number;
+      nextNodeData: { startPos: Vec2; endPos: Vec2; jump: boolean; height: number };
+      overNodePass: boolean;
+      pathComplete: boolean;
+      triedNodePassChange: boolean;
+      triedRandom: number;
+      doBackUp: boolean;
+      triedBackUp: number;
+      avoidTarget: Vec2;
+      avoidTarget2: Vec2;
+      firstMovement: boolean;
+      targetDir: Vec2;
+      targetDist: number;
+      failCount: number;
+      wrongNodeTimer: number;
+      lastSideWayDir: Vec2;
     }
+    interface NavPathConstructor extends ImpactClass<NavPath> {
+      new (searcher: unknown): NavPath;
+    }
+    var NavPath: NavPathConstructor;
+
+    enum NAV_BLOCKER_TYPE {
+      REGULAR = 0,
+      NO_BLOCK = 1,
+      NO_TOP = 2,
+    }
+
+    interface NavBlocker extends ig.Class {
+      entity: ig.Entity;
+      pos: Vec3;
+      size: Vec3;
+      blockType: ig.NAV_BLOCKER_TYPE;
+
+      update(this: this, blockType?: ig.NAV_BLOCKER_TYPE): void;
+      embedInNavMap(this: this): void;
+      removeFromNavMap(this: this): void;
+      remove(this: this): void;
+    }
+    interface NavBlockerConstructor extends ImpactClass<NavBlocker> {
+      new (entity: ig.Entity, blockType?: ig.NAV_BLOCKER_TYPE): NavBlocker;
+    }
+    var NavBlocker: NavBlockerConstructor;
+  }
 }
