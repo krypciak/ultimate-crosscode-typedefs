bd7e7178b108cabe1fb566aaa36b3f909d6c4b3a
krypek <krypek@tuta.io>
2024-01-29 14:35:17 +0100

    Add ig.ENTITY.Analyzable

--- modules/game.feature.quick-menu.entities.analyzable.d.ts
+++ modules/game.feature.quick-menu.entities.analyzable.d.ts
@@ -1,2 +1,30 @@
 // requires impact.base.entity
 // requires game.feature.quick-menu.gui.quick-screen-types
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace Analyzable {
+      interface Settings {
+        analType?: 'DEFAULT' | 'QUEST' | 'SPECIAL';
+        visible?: boolean;
+        text: ig.LangLabel.Data;
+        distance?: number;
+      }
+    }
+    interface Analyzable extends ig.Entity {
+      color: sc.ANALYSIS_COLORS;
+      text: ig.LangLabel.Data;
+      visible: boolean;
+      distance?: number;
+
+      isQuickMenuVisible(this: this): boolean;
+      getQuickMenuSettings(this: this): Omit<sc.QuickMenuTypesBaseSettings, 'entity'>;
+    }
+    interface AnalyzableConstructor extends ImpactClass<Analyzable> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.Analyzable.Settings): Analyzable;
+    }
+    var Analyzable: AnalyzableConstructor;
+  }
+}
