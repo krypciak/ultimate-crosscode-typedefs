21777f7935ee7ff9f08e55c4112f7761011a9e15
krypek <krypek@tuta.io>
2024-02-01 18:55:42 +0100

    Add ig.ENTITY.Enemy related enmus

---
+++ modules/game.feature.combat.model.enemy-annotation.d.ts
@@ -0,0 +1,45 @@
+export {};
+
+declare global {
+  namespace sc {
+    enum ENEMY_AGGRESSION {
+      THREAT = 0,
+      TEMP_THREAT = 1,
+      PEACEFUL = 2,
+    }
+    enum ENEMY_ANNO_ACTIVE {
+      NONE = 0,
+      PRE_ATTACK = 1,
+      ATTACK = 2,
+      WIDE_RANGE_ATTACK = 3,
+      POST_ATTACK = 4,
+    }
+    enum ENEMY_ANNO_PASSIVE {
+      NONE = 0,
+      VULNERABLE = 1,
+      WEAK = 2,
+      IMMUNE = 3,
+    }
+    enum ENEMY_ANNO_WEAPON {
+      ANY = 0,
+      MELEE = 1,
+      RANGED = 2,
+    }
+    enum ENEMY_ANNO_EXTRA {
+      ATTACK_BACK = 1,
+      ATTACK_FRONT = 2,
+      LOOK_AWAY = 4,
+      BE_PASSIVE = 8,
+      BE_PASSIVE_IF_TARGETED = 16,
+      VIRUS_SPECIAL_RULE = 32,
+    }
+    enum ENEMY_ANNO_ELEMENT {
+      ANY = 0,
+      NEUTRAL = 1,
+      HEAT = 2,
+      COLD = 3,
+      SHOCK = 4,
+      WAVE = 5,
+    }
+  }
+}
