28a36fbd62427c68caad3651df60305626eee3e8
krypek <krypek@tuta.io>
2024-02-04 14:53:50 +0100

    Update sc.SideBoxGui

--- modules/game.feature.gui.base.boxes.d.ts
+++ modules/game.feature.gui.base.boxes.d.ts
@@ -5,18 +5,24 @@ export {};
 
 declare global {
   namespace sc {
-    enum ArrowBoxGui$POINTER { NONE = 0, TOP_LEFT = 1, BOTTOM_LEFT = 2, TOP_RIGHT = 3, BOTTOM_RIGHT = 4 }
+    enum ArrowBoxGui$POINTER {
+      NONE = 0,
+      TOP_LEFT = 1,
+      BOTTOM_LEFT = 2,
+      TOP_RIGHT = 3,
+      BOTTOM_RIGHT = 4,
+    }
     namespace ArrowBoxGui {
-      type POINTER = ArrowBoxGui$POINTER
+      type POINTER = ArrowBoxGui$POINTER;
     }
-    interface ArrowBoxGui extends ig.BoxGui { }
+    interface ArrowBoxGui extends ig.BoxGui {}
     interface ArrowBoxGuiConstructor extends ImpactClass<ArrowBoxGui> {
-      new(sizeX: number, sizeY: number, pointerType: sc.ArrowBoxGui.POINTER): ArrowBoxGui
+      new (sizeX: number, sizeY: number, pointerType: sc.ArrowBoxGui.POINTER): ArrowBoxGui;
     }
-    var ArrowBoxGui: ArrowBoxGuiConstructor
+    var ArrowBoxGui: ArrowBoxGuiConstructor;
 
-    interface BlackWhiteBox extends ig.BoxGui { }
-    interface BlackWhiteBoxConstructor extends ImpactClass<BlackWhiteBox> { }
+    interface BlackWhiteBox extends ig.BoxGui {}
+    interface BlackWhiteBoxConstructor extends ImpactClass<BlackWhiteBox> {}
     var BlackWhiteBox: BlackWhiteBoxConstructor;
 
     interface CenterBoxGui extends ig.GuiElementBase {
@@ -33,14 +39,34 @@ declare global {
       ninepatch: ig.NinePatch;
     }
     interface LineGuiConstructor extends ImpactClass<LineGui> {
-      new (width: number): sc.LineGui
+      new (width: number): sc.LineGui;
     }
     var LineGui: LineGuiConstructor;
 
     interface SideBoxGui extends ig.GuiElementBase {
+      titleGui: sc.SlickTitleGui;
+      contentEntries: sc.SlickBoxGui[];
+      right: boolean;
+
+      pushContent(
+        this: this,
+        subGui: ig.GuiElementBase,
+        showSubGui?: boolean,
+        paddingX?: number,
+        paddingY?: number,
+      ): void;
+      replaceContent(this: this, index: number, subGui: ig.GuiElementBase): void;
+      removeContent(this: this, index: number): sc.SlickBoxGui;
+      clearContent(this: this): void;
+      popContent(this: this): void;
+      rearrangeContent(this: this, slickBoxGui?: sc.SlickBoxGui): void;
+      hide(this: this, skipTransition?: boolean, initDelay?: number): void;
+      show(this: this, skipTransition?: boolean, initDelay?: number): void;
       remove(this: this): void;
     }
-    interface SideBoxGuiConstructor extends ImpactClass<SideBoxGui> {}
+    interface SideBoxGuiConstructor extends ImpactClass<SideBoxGui> {
+      new (right: boolean, text: sc.TextLike): SideBoxGui;
+    }
     var SideBoxGui: SideBoxGuiConstructor;
 
     type SmallBoxAlign = (dest: Vec2, coll: ig.CollEntry) => void;
@@ -48,16 +74,22 @@ declare global {
 
     interface SmallEntityBox extends ig.GuiElementBase {}
     interface SmallEntityBoxConstructor extends ImpactClass<SmallEntityBox> {
-      new (entity: ig.Entity, text: string, time: number, align?: SmallBoxAlign, offY?: number): SmallEntityBox;
+      new (
+        entity: ig.Entity,
+        text: string,
+        time: number,
+        align?: SmallBoxAlign,
+        offY?: number,
+      ): SmallEntityBox;
     }
-    let  SmallEntityBox: SmallEntityBoxConstructor;
+    let SmallEntityBox: SmallEntityBoxConstructor;
 
     interface SmallBlackBoxGui extends ig.BoxGui {
-      text: unknown
+      text: unknown;
     }
     interface SmallBlackBoxGuiConstructor extends ImpactClass<SmallBlackBoxGui> {
-      new(width: number): SmallBlackBoxGui
+      new (width: number): SmallBlackBoxGui;
     }
-    var SmallBlackBoxGui: SmallBlackBoxGuiConstructor
+    var SmallBlackBoxGui: SmallBlackBoxGuiConstructor;
   }
 }
