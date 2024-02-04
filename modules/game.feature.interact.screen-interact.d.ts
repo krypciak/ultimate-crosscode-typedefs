08b883e1780ad31e7621782378acb763199da3b3
krypek <krypek@tuta.io>
2024-02-04 19:52:04 +0100

    Add sc.ScreenInteractEntry

--- modules/game.feature.interact.screen-interact.d.ts
+++ modules/game.feature.interact.screen-interact.d.ts
@@ -1,1 +1,20 @@
 // requires impact.feature.interact.interact
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ScreenInteractEntry extends ig.InteractEntry {
+      callbackObject: { onInteraction: () => void };
+      withEscape: boolean;
+      autoCtrlIgnore: boolean;
+    }
+    interface ScreenInteractEntryConstructor extends ImpactClass<ScreenInteractEntry> {
+      new (
+        callbackObject: sc.ScreenInteractEntry['callbackObject'],
+        withEscape?: boolean,
+      ): ScreenInteractEntry;
+    }
+    var ScreenInteractEntry: ScreenInteractEntryConstructor;
+  }
+}
