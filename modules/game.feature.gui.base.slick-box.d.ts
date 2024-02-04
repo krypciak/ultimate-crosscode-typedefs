9c00281a9f6c75fc7abea219019253d1ae3df7df
krypek <krypek@tuta.io>
2024-02-04 14:53:21 +0100

    Add sc.SlickBoxGui

--- modules/game.feature.gui.base.slick-box.d.ts
+++ modules/game.feature.gui.base.slick-box.d.ts
@@ -4,9 +4,36 @@
 export {};
 
 declare global {
-    namespace sc {
-        interface SlickBoxRawGui extends ig.BoxGui {}
-        interface SlickBoxRawGuiConstructor extends ImpactClass<SlickBoxRawGui> {}
-        let SlickBoxRawGui: SlickBoxRawGuiConstructor;
+  namespace sc {
+    interface SlickTitleGui extends ig.BoxGui {
+      text: null /* unset */;
     }
+    interface SlickTitleGuiConstructor extends ImpactClass<SlickTitleGui> {
+      new (text: sc.TextLike, right?: boolean, flipped?: Nullable<boolean>): SlickTitleGui;
+    }
+    var SlickTitleGui: SlickTitleGuiConstructor;
+
+    interface SlickBoxRawGui extends ig.BoxGui {}
+    interface SlickBoxRawGuiConstructor extends ImpactClass<SlickBoxRawGui> {}
+    var SlickBoxRawGui: SlickBoxRawGuiConstructor;
+
+    interface SlickBoxGui extends sc.SlickBoxRawGui {
+      paddingX: number;
+      paddingY: number;
+      minWidth: number;
+      subGui: unknown;
+
+      setContent(this: this, subGui: ig.GuiElementBase): void;
+    }
+    interface SlickBoxGuiConstructor extends ImpactClass<SlickBoxGui> {
+      new (
+        subGui: ig.GuiElementBase,
+        flipped?: boolean,
+        paddingX?: number,
+        paddingY?: number,
+        minWidth?: number,
+      ): SlickBoxGui;
+    }
+    var SlickBoxGui: SlickBoxGuiConstructor;
+  }
 }
