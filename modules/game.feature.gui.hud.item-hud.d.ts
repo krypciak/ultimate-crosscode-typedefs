49611b82e787597ec2051fb42dc6210d4d5eedce
krypek <krypek@tuta.io>
2024-02-04 14:02:56 +0100

    Add sc.ItemContent

--- modules/game.feature.gui.hud.item-hud.d.ts
+++ modules/game.feature.gui.hud.item-hud.d.ts
@@ -1,2 +1,24 @@
 // requires game.feature.gui.hud.right-hud
 // requires game.feature.model.options-model
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ItemContent extends ig.GuiElementBase {
+      timer: number;
+      id: ItemID;
+      amount: number;
+      textGui: sc.TextGui;
+      amountGui: sc.NumberGui;
+
+      updateOption(this: this, bigFont?: boolean): void;
+      updateTimer(this: this): void;
+      increaseNumber(this: this, amount?: number, skipTransition?: Nullable<boolean>): void;
+    }
+    interface ItemContentConstructor extends ImpactClass<ItemContent> {
+      new (id: ItemID, amount?: number): ItemContent;
+    }
+    var ItemContent: ItemContentConstructor;
+  }
+}
