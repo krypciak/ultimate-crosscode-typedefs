ed03081d048250690ca1c5cb0fd06c3de52eae25
krypek <krypek@tuta.io>
2024-02-04 14:54:15 +0100

    Add sc.RightHudBoxGui & sc.RightHudGui

--- modules/game.feature.gui.hud.right-hud.d.ts
+++ modules/game.feature.gui.hud.right-hud.d.ts
@@ -6,9 +6,40 @@
 export {};
 
 declare global {
-    namespace sc {
-        interface RightHudBoxGui extends sc.SideBoxGui {}
-        interface RightHudBoxGuiConstructor extends ImpactClass<RightHudBoxGui> {}
-        let RightHudBoxGui: RightHudBoxGuiConstructor;
+  namespace sc {
+    interface RightHudGui extends ig.GuiElementBase, sc.Model.Observer {
+      taskTitle: unknown;
+      maxCount: number;
+      currentCount: number;
+      variable: unknown;
+      boxes: unknown;
+      doReorder: boolean;
+
+      addHudBox(this: this, rightHudBox: sc.RightHudGui, insertIndex?: number): void;
+      addHudBoxBefore(this: this, rightHudBox: sc.RightHudGui, index?: number): void;
+      removeHudBox(this: this, rightHudBox: sc.RightHudGui): void;
+      reorder(this: this): void;
+      updateSize(this: this): void;
     }
+    interface RightHudGuiConstructor extends ImpactClass<RightHudGui> {
+      new (): RightHudGui;
+    }
+    var RightHudGui: RightHudGuiConstructor;
+
+    /* game.feature.gui.hud.right-hud */
+    interface RightHudBoxGui extends sc.SideBoxGui {
+      hidden: boolean;
+      justAdded: boolean;
+      parentPanel: sc.RightHudGui;
+
+      show(this: this, skipTransition?: boolean, initDelay?: number): void;
+      hide(this: this, skipTransition?: boolean, initDelay?: number): void;
+      remove(this: this): void;
+    }
+    interface RightHudBoxGuiConstructor extends ImpactClass<RightHudBoxGui> {
+      new (text: sc.TextLike): RightHudBoxGui;
+    }
+    var RightHudBoxGui: RightHudBoxGuiConstructor;
+  }
 }
+
