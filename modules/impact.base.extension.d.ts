387de0de2b35e16c438c79a99dd4ed8d10b25712
krypek <krypek@tuta.io>
2024-02-06 21:31:56 +0100

    Update ig.ExtensionManager

--- modules/impact.base.extension.d.ts
+++ modules/impact.base.extension.d.ts
@@ -4,8 +4,22 @@ declare global {
   namespace ig {
     type ExtensionName = string;
 
+    interface ExtensionListener {
+      onExtensionLoaded(data: any): void;
+    }
+
     interface ExtensionManager extends ig.Class {
       enabled: Record<ExtensionName, boolean>;
+      list: ig.ExtensionList;
+      listeners: Record<ExtensionName, unknown>[];
+
+      addListener(this: this, clazz: ig.ExtensionListener, type: string): void;
+      load(this: this): void;
+      getExtensionList(this: this): ExtensionName[];
+      getExtension(this: this, name: ExtensionName): Nullable<Extension>;
+      hasExtension(this: this, name: ExtensionName): boolean;
+      setExtensionEnabled(this: this, name: ExtensionName, value: boolean): void;
+      onExtensionLoaded(this: this, a: unknown): void;
     }
     interface ExtensionManagerConstructor extends ImpactClass<ExtensionManager> {
       new (): ExtensionManager;
@@ -13,7 +27,10 @@ declare global {
     var ExtensionManager: ExtensionManagerConstructor;
     var extensions: ExtensionManager;
 
-    interface ExtensionList extends ig.SingleLoadable {}
+    type Extension = unknown;
+    interface ExtensionList extends ig.SingleLoadable {
+      extensions: Record<ExtensionName, Extension>;
+    }
     interface ExtensionListConstructor extends ImpactClass<ExtensionList> {
       new (): ExtensionList;
     }
