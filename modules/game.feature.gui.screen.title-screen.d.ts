097c5700a489e947a3c17db410a55c9a73bd9b9c
krypek <krypek@tuta.io>
2024-02-06 21:35:40 +0100

    Update sc.TitleScreenButtonGui

--- modules/game.feature.gui.screen.title-screen.d.ts
+++ modules/game.feature.gui.screen.title-screen.d.ts
@@ -26,15 +26,42 @@ declare global {
     interface TitleScreenButtonGui extends ig.GuiElementBase {
       buttonInteract: ig.ButtonInteractEntry;
       buttonGroup: sc.ButtonGroup;
-      buttons: sc.ButtonGui[];
+      gamepadGui: null /* unset */;
+      setOptions: null /* unset */;
+      namedButtons: Record<string, sc.ButtonGui>;
+      presetMenu: sc.TitlePresetMenu;
+      story: null /* unset */;
+      gameplay: null /* unset */;
+      puzzle: null /* unset */;
+      closeButton: null /* unset */;
       changelogButton: sc.ButtonGui;
+      perfWarnButton: sc.ButtonGui;
+      changelogGui: sc.ChangelogGui;
       gameCodeButton: sc.ButtonGui;
-
+      background: ig.ColorGui;
+      perfWarning: boolean;
+      buttons: sc.ButtonGui[];
+      _rearrangeAnchor: number;
+      _newGamePlus: boolean;
       followButton: sc.ButtonGui;
+      dlcButton: sc.ButtonGui;
 
+      postInit(this: this): void;
       show(this: this): void;
-      hide(this: this, skipTransition: boolean): void;
-      _createButton(this: this, name: string, y: number, buttonGroup: number, onButtonPress: () => void): number
+      hide(this: this, skipTransition?: boolean): void;
+      checkPerformanceWarning(this: this): void;
+      showPerformanceWarning(this: this, text: sc.TextLike): void;
+      checkClearSaveFiles(this: this): boolean;
+      _createButton(
+        this: this,
+        name: string,
+        y: number,
+        buttonGroup: number,
+        onButtonPress: () => void,
+        namedButtonsName?: string,
+      ): number;
+      _enterLoadMenu(this: this): void;
+      _enterOptionsMenu(this: this): void;
     }
     interface TitleScreenButtonGuiConstructor extends ImpactClass<TitleScreenButtonGui> {
       new (): TitleScreenButtonGui;
