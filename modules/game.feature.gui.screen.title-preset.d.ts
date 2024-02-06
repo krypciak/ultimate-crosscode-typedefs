7a1825a86f911e5d7cde1911f52cab82b84fee7a
krypek <krypek@tuta.io>
2024-02-06 21:35:51 +0100

    Add sc.TitlePresetMenu

--- modules/game.feature.gui.screen.title-preset.d.ts
+++ modules/game.feature.gui.screen.title-preset.d.ts
@@ -1,3 +1,49 @@
 // requires impact.feature.gui.gui
 // requires impact.feature.gui.base.basic-gui
 // requires game.feature.menu.gui.menu-misc
+
+export {};
+
+declare global {
+  namespace sc {
+    namespace TitlePresetMenu {
+      interface SaLoButton extends ig.FocusGui {
+        ninepatch: ig.NinePatch;
+        level: sc.NumberGui;
+        location: sc.TextGui;
+        mainText: sc.TextGui;
+        position: null /* unset */;
+        slot: number;
+
+        focusGained(this: this): void;
+        updateDrawables(this: this, b: unknown): void;
+      }
+      interface SaLoButtonConstructor extends ImpactClass<SaLoButton> {
+        new (slot: number, data: sc.SavePresetData): SaLoButton;
+      }
+    }
+    interface TitlePresetMenu extends sc.HeaderMenuPanel {
+      buttonInteract: ig.ButtonInteractEntry;
+      itemList: sc.ItemListBox;
+      backButton: sc.ButtonGui;
+      submitSound: ig.Sound;
+      slots: sc.TitlePresetMenu.SaLoButton[];
+      _loadCallback: (loadSlot: number) => void;
+      _removeCallback: () => void;
+      _doLoad: boolean;
+      _loadSlot: number;
+      _firstTime: boolean;
+
+      update(this: this): void;
+      setPos(this: this, b: unknown, a: unknown): void;
+      activate(this: this): void;
+      deactivate(this: this): void;
+      createList(this: this): void;
+    }
+    interface TitlePresetMenuConstructor extends ImpactClass<TitlePresetMenu> {
+      new (loadCallback: (loadSlot: number) => void, removeCallback: () => void): TitlePresetMenu;
+      SaLoButton: sc.TitlePresetMenu.SaLoButtonConstructor;
+    }
+    var TitlePresetMenu: TitlePresetMenuConstructor;
+  }
+}
