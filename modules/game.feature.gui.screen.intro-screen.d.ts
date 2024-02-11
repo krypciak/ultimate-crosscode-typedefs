433b99925c6dafde2cc3e3871f370ee597c1e850
krypek <krypek@tuta.io>
2024-02-11 11:27:30 +0100

    Add ig.GUI.IntroScreen

--- modules/game.feature.gui.screen.intro-screen.d.ts
+++ modules/game.feature.gui.screen.intro-screen.d.ts
@@ -2,3 +2,17 @@
 // requires impact.feature.gui.base.basic-gui
 // requires game.feature.gui.base.button
 // requires impact.base.image
+
+export {};
+
+declare global {
+  namespace ig.GUI {
+    interface IntroScreen extends ig.SequenceGui {
+      screenInteract: sc.ScreenInteractEntry;
+    }
+    interface IntroScreenConstructor extends ImpactClass<IntroScreen> {
+      new (callback: (msg: ig.SEQUENCE_MSG, labelReached?: String) => void): IntroScreen;
+    }
+    var IntroScreen: IntroScreenConstructor;
+  }
+}
