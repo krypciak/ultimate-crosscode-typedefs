b1e0904836f2a0433803226dac876a3a399ca77b
krypek <krypek@tuta.io>
2024-02-11 11:27:16 +0100

    Add ig.SequenceGui

--- modules/impact.feature.gui.base.basic-gui.d.ts
+++ modules/impact.feature.gui.base.basic-gui.d.ts
@@ -42,6 +42,41 @@ declare global {
     }
     var ColorGui: ColorGuiConstructor;
 
+    enum SEQUENCE_MSG {
+      LABEL_REACHED = 1,
+      ENDED = 2,
+    }
+
+    interface SequenceGui extends ig.GuiElementBase {
+      timer: number;
+      timeLineIndex: number;
+      gfx: Record<string, ig.Image>;
+      gui: Record<string, unknown>;
+      timeLine: unknown[];
+      reachedLabels: string[];
+      currentSkipLabel: unknown;
+      callback: (msg: ig.SEQUENCE_MSG, labelReached?: string) => void;
+
+      initTimeLine(
+        this: this,
+        gfx: Record<string, ig.Image>,
+        gui: Record<string, unknown>,
+        timeLine: unknown[],
+      ): void;
+      _setLabelReached(this: this, label: string): void;
+      start(this: this): void;
+      end(this: this): void;
+      notifyCallback(this: this, msg: ig.SEQUENCE_MSG, labelReached?: string): void;
+      skip(this: this): void;
+      jumpTo(this: this, label: string): void;
+      hasEnded(this: this): boolean;
+      isLabelReached(this: this, label: string): boolean;
+    }
+    interface SequenceGuiConstructor extends ImpactClass<SequenceGui> {
+      new (callback: (msg: ig.SEQUENCE_MSG, labelReached?: String) => void): SequenceGui;
+    }
+    var SequenceGui: SequenceGuiConstructor;
+
     interface SimpleGui extends ig.GuiElementBase {
       hide(this: this, skip?: boolean, initDelay?: number): void;
       show(this: this, skip?: boolean, initDelay?: number): void;
