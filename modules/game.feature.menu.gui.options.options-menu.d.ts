4f7f6fd64440de9e23e210ff87fb5155b6a8dc90
krypek <krypek@tuta.io>
2024-02-22 18:00:57 +0100

    Update sc.OptionsMenu

--- modules/game.feature.menu.gui.options.options-menu.d.ts
+++ modules/game.feature.menu.gui.options.options-menu.d.ts
@@ -1,17 +1,36 @@
 // requires impact.feature.gui.gui
 // requires game.feature.menu.gui.options.options-list
 
-
-export {}
+export {};
 
 declare global {
-    namespace sc {
-        interface OptionsMenu extends sc.BaseMenu {
-            exitMenu(this: this): void
-        }
-        interface OptionsMenuConstructor extends ImpactClass<OptionsMenu> {
-          new (): OptionsMenu
-        }
-        var OptionsMenu: OptionsMenuConstructor
+  namespace sc {
+    interface OptionsMenu extends sc.BaseMenu, sc.Model.Observer {
+      hotkeyHelp: sc.ButtonGui;
+      hotkeyDefault: sc.ButtonGui;
+      listBox: sc.OptionsTabBox;
+      helpGui: sc.HelpScreen;
+      blackBox: ig.ColorGui;
+      langGui: sc.OptionLangPopUp;
+
+      addObservers(this: this): void;
+      removeObservers(this: this): void;
+      showMenu(this: this): void;
+      hideMenu(this: this): void;
+      exitMenu(this: this): void;
+      onHotkeyDefaultCheck(this: this): boolean;
+      onDefaultButtonPressed(this: this): void;
+      onHotkeyHelpCheck(this: this): boolean;
+      onHelpButtonPressed(this: this): void;
+      createHelpGui(this: this): void;
+      onAddHotkeys(this: this, inputRegained?: boolean): void;
+      commitHotKeysToTopBar(this: this, inputRegained?: boolean): void;
+      onBackButtonPress(this: this): void;
+      onLangPopUpClose(this: this): void;
     }
+    interface OptionsMenuConstructor extends ImpactClass<OptionsMenu> {
+      new (): OptionsMenu;
+    }
+    var OptionsMenu: OptionsMenuConstructor;
+  }
 }
