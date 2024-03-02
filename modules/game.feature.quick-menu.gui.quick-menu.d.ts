a2c2aad8814dc8f45f6a093420b4df63c19cc215
krypek <krypek@tuta.io>
2024-03-02 14:49:31 +0100

    Add sc.QuickMenu

--- modules/game.feature.quick-menu.gui.quick-menu.d.ts
+++ modules/game.feature.quick-menu.gui.quick-menu.d.ts
@@ -5,3 +5,30 @@
 // requires game.feature.quick-menu.gui.quick-item-menu
 // requires game.feature.quick-menu.gui.quick-party
 // requires game.feature.menu.gui.menu-misc
+
+export {};
+
+declare global {
+  namespace sc {
+    interface QuickMenu extends ig.GuiElementBase, sc.Model.Observer {
+      ringmenu: sc.QuickRingMenu;
+      items: sc.QuickItemMenu;
+      analysis: sc.QuickMenuAnalysis;
+      party: sc.QuickPartyStrategyMenu;
+      location: sc.QuickLocationBox;
+      info: sc.InfoBar;
+      buffInfo: sc.BuffInfo;
+      buffStats: sc.QuickMenuBuffsGui;
+      backButton: null /* unused afaik */;
+
+      _checkBackButtonInput(this: this): boolean;
+      _enterMenu(this: this): void;
+      _exitMenu(this: this): void;
+      _setInfoBarAndLocation(this: this): void;
+    }
+    interface QuickMenuConstructor extends ImpactClass<QuickMenu> {
+      new (): QuickMenu;
+    }
+    var QuickMenu: QuickMenuConstructor;
+  }
+}
