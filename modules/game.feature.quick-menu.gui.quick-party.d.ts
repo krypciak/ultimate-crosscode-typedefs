2922307822f3c223336c258ce75afaead53839b3
krypek <krypek@tuta.io>
2024-03-02 14:49:27 +0100

    Add sc.QuickPartyStrategyMenu

--- modules/game.feature.quick-menu.gui.quick-party.d.ts
+++ modules/game.feature.quick-menu.gui.quick-party.d.ts
@@ -5,3 +5,51 @@
 // requires game.feature.menu.gui.list-boxes
 // requires game.feature.menu.gui.menu-misc
 // requires game.feature.quick-menu.gui.quick-item-menu
+
+export {};
+
+declare global {
+  namespace sc {
+    namespace QuickPartyStrategyMenu {
+      /* :) */
+      type GetButtonIndex1Argument = sc.PARTY_STRATEGY extends Record<infer _, infer V1>
+        ? V1 extends Record<infer K2, unknown>
+          ? K2
+          : never
+        : never;
+    }
+    interface QuickPartyStrategyMenu extends ig.BoxGui, sc.Model.Observer {
+      gfx: ig.Image;
+      ninepatch: ig.NinePatch;
+      base: ig.GuiHook;
+      anchor: ig.GuiHook;
+      arrow: sc.QuickItemArrow;
+      rows: sc.ButtonGui[][];
+      currentText: sc.TextGui[];
+      buttongroup: sc.RowButtonGroup;
+      _hidden: boolean;
+
+      addRow(this: this, langKey: string, row: number): void;
+      createButton(
+        this: this,
+        langKey: string,
+        id: number,
+        row: number,
+        type?: sc.ButtonGui.Type,
+      ): sc.ButtonGui;
+      resetRow(this: this, strategy: keyof sc.PartyModel.StrategyKeys, row: number): void;
+      resetButtons(this: this, except: sc.ButtonGui, buttons: sc.ButtonGui): void;
+      getButtonIndex(
+        this: this,
+        strategy: sc.QuickPartyStrategyMenu.GetButtonIndex1Argument,
+        row: number,
+      ): number;
+      onSelection(this: this, button: sc.ButtonGui): void;
+      onPress(this: this, button: sc.ButtonGui): void;
+    }
+    interface QuickPartyStrategyMenuConstructor extends ImpactClass<QuickPartyStrategyMenu> {
+      new (base: ig.GuiElementBase, anchor: ig.GuiElementBase): QuickPartyStrategyMenu;
+    }
+    var QuickPartyStrategyMenu: QuickPartyStrategyMenuConstructor;
+  }
+}
