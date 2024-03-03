12b84dfe74adde000261b4ccd2923378fb34e7fb
krypek <krypek@tuta.io>
2024-03-03 12:15:39 +0100

    Update sc.QuickItemMenu

--- modules/game.feature.quick-menu.gui.quick-item-menu.d.ts
+++ modules/game.feature.quick-menu.gui.quick-item-menu.d.ts
@@ -5,31 +5,48 @@
 // requires game.feature.menu.gui.list-boxes
 // requires game.feature.menu.gui.menu-misc
 
-export { };
+export {};
 
 declare global {
-    namespace sc {
-        interface QuickItemArrow extends ig.GuiElementBase {
-            flipX: boolean
-            flipY: boolean
-            bottomAnchor: boolean
+  namespace sc {
+    interface QuickItemArrow extends ig.GuiElementBase {
+      gfx: ig.Image;
+      flipX: boolean;
+      flipY: boolean;
+      bottomAnchor: boolean;
 
-            setPosition(this: this, x: number, y: number, flipX?: boolean, flipY?: boolean, bottomAnchor?: boolean): void
-        }
-        interface QuickItemArrowConstructor extends ImpactClass<QuickItemArrow> {
-            new(): QuickItemArrow
-        }
-        var QuickItemArrow: QuickItemArrowConstructor
-        interface QuickItemMenu extends ig.BoxGui {
-            list: sc.ButtonListBox;
-            buttongroup: sc.ButtonGroup;
-
-            updateList(this: this, skipSounds: boolean): void;
-            addFavoriteOverlay(this: this, button: sc.ItemBoxButton): void;
-        }
-        interface QuickItemMenuConstructor extends ImpactClass<QuickItemMenu> {
-
-        }
-        let QuickItemMenu: QuickItemMenuConstructor;
+      setPosition(
+        this: this,
+        x: number,
+        y: number,
+        flipX?: boolean,
+        flipY?: boolean,
+        bottomAnchor?: boolean,
+      ): void;
     }
+    interface QuickItemArrowConstructor extends ImpactClass<QuickItemArrow> {
+      new (): QuickItemArrow;
+    }
+    var QuickItemArrow: QuickItemArrowConstructor;
+
+    interface QuickItemMenu extends ig.BoxGui, sc.Model.Observer {
+      gfx: ig.Image;
+      ninepatch: ig.NinePatch;
+      base: ig.GuiHook;
+      anchor: ig.GuiHook;
+      list: sc.ButtonListBox;
+      arrow: sc.QuickItemArrow;
+      buttongroup: sc.ButtonGroup;
+      _hidden: boolean;
+
+      updateList(this: this, skipSounds?: boolean): void;
+      addFavoriteOverlay(this: this, button: sc.ItemBoxButton): void;
+      onSelection(this: this, button: sc.ItemBoxButton): void;
+      onPress(this: this, button: sc.ItemBoxButton): void;
+    }
+    interface QuickItemMenuConstructor extends ImpactClass<QuickItemMenu> {
+      new (baseGui: ig.GuiElementBase, anchorGui: ig.GuiElementBase): QuickItemMenu;
+    }
+    var QuickItemMenu: QuickItemMenuConstructor;
+  }
 }
