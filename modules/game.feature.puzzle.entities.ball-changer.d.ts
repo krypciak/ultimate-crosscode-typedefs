c79f4e5372469f5f3e45ab195a6b4156b6172b5d
krypek <krypek@tuta.io>
2024-03-04 19:36:17 +0100

    Add ig.ENTITY.BallChanger

--- modules/game.feature.puzzle.entities.ball-changer.d.ts
+++ modules/game.feature.puzzle.entities.ball-changer.d.ts
@@ -9,10 +9,10 @@ export {};
 declare global {
   namespace sc {
     enum BALL_CHANGER_TYPE {
-        CHANGE_DIR,
-        CHANGE_SPEED,
-        RESET_SPEED,
-        CHANGE_ELEMENT,
+      CHANGE_DIR,
+      CHANGE_SPEED,
+      RESET_SPEED,
+      CHANGE_ELEMENT,
     }
   }
   namespace ig {
@@ -28,10 +28,34 @@ declare global {
               dir?: keyof typeof ig.ActorEntity.FACE8; // for use when CHANGE_DIR
               factor?: number; // for use when CHANGE_SPEED
               element?: sc.ELEMENT; // for use when CHANGE_ELEMENT
-            }
-          }
+            };
+          };
         }
       }
+
+      interface BallChanger extends ig.AnimatedEntity {
+        isOn: boolean;
+        condition: ig.VarCondition;
+        changerType: sc.BALL_CHANGER_TYPE;
+        disableTimer: number;
+        disableBall: Nullable<ig.ENTITY.Ball>;
+        ballTime: number;
+        resetBounce: boolean;
+        _wm: ig.Config;
+        fx: { sheet: ig.EffectSheet };
+
+        varsChanged(this: this): void;
+        ballHit(this: this, entity: ig.Entity): boolean;
+      }
+      interface BallChangerConstructor extends ImpactClass<BallChanger> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.BallChanger.Settings,
+        ): BallChanger;
+      }
+      var BallChanger: BallChangerConstructor;
     }
   }
 }
