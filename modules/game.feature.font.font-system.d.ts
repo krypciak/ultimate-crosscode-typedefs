160bee799178a6d7640e17eb0faa60ce5ebc4d40
krypek <krypek@tuta.io>
2024-03-11 14:39:18 +0100

    Update sc.FontSystem

--- modules/game.feature.font.font-system.d.ts
+++ modules/game.feature.font.font-system.d.ts
@@ -19,12 +19,45 @@ declare global {
     }
 
     interface FontSystem extends ig.GameAddon {
+      gamepadIcons: boolean;
+      icons: Record<
+        | 'global'
+        | 'globalSmall'
+        | 'keyboard'
+        | 'gamepad'
+        | 'items'
+        | 'stats'
+        | 'stats_large'
+        | 'langs',
+        ig.Font
+      >;
+      colors: Record<
+        | 'red'
+        | 'green'
+        | 'purple'
+        | 'grey'
+        | 'orange_mid'
+        | 'orange_tiny'
+        | 'purple_mid'
+        | 'grey_mid'
+        | 'grey_tiny'
+        | 'red_mid'
+        | 'red_tiny'
+        | 'green_mid'
+        | 'green_tiny',
+        ig.Image
+      >;
       font: ig.MultiFont;
       smallFont: ig.MultiFont;
       tinyFont: ig.MultiFont;
-      gamepadIconStyle: sc.GAMEPAD_ICON_STYLE
+      switchIndex: number;
+      gamepadIconStyle: sc.GAMEPAD_ICON_STYLE;
 
       changeKeyCodeIcon(this: this, action: LiteralUnion<ig.Input.KnownAction>, key: ig.KEY): void;
+      changeGamepadIcon(this: this, action: LiteralUnion<ig.Input.KnownAction>, icon: string): void;
+      setGamepadIconStyle(this: this, style: sc.GAMEPAD_ICON_STYLE): void;
+      updateGamepadSwapMap(this: this): void;
+      hasIcon(this: this, icon: string): boolean;
     }
     interface FontSystemConstructor extends ImpactClass<FontSystem> {
       new (): FontSystem;
