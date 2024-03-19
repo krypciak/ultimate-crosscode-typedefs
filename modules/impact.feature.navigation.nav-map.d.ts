1289f693e89b28bc2f46d66ffa2899d1004ca493
krypek <krypek@tuta.io>
2024-03-19 22:14:22 +0100

    Add ig.MAP.Navigation

--- modules/impact.feature.navigation.nav-map.d.ts
+++ modules/impact.feature.navigation.nav-map.d.ts
@@ -1,3 +1,17 @@
 // requires impact.base.map
 // requires impact.base.image
 // requires game.config
+
+export {};
+
+declare global {
+  namespace ig.MAP {
+    interface Navigation extends ig.Map {
+      initialized: boolean;
+      _wm: ig.Config;
+    }
+    interface NavigationConstructor extends ImpactClass<Navigation> {
+      new (data: sc.MapModel.Map, zHeight: number): Navigation;
+    }
+  }
+}
