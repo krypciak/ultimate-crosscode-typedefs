498f6ddf3ec93bb86a1ba9d674b6038e34af9b5f
krypek <krypek@tuta.io>
2024-03-19 22:14:32 +0100

    Add ig.MAP.Collision

--- modules/impact.base.collision-map.d.ts
+++ modules/impact.base.collision-map.d.ts
@@ -1,2 +1,46 @@
 // requires impact.base.map
 // requires game.config
+
+export {};
+
+declare global {
+  namespace ig.MAP {
+    interface Collision extends ig.Map {
+      _wm: ig.Config;
+
+      prepare(this: this, data: sc.MapModel.Map, yOffset: number): void;
+      isTileGround(this: this, entityX: number, entityY: number): boolean;
+      isTileAreaBlocked(
+        this: this,
+        entityX: number,
+        entityY: number,
+        width: number,
+        height: number,
+      ): boolean;
+      isGridHole(this: this, x: number, y: number): boolean;
+      isOverHole(
+        this: this,
+        entityX: number,
+        entityY: number,
+        width: number,
+        height: number,
+        dirToHole?: Vec2,
+      ): boolean;
+      trace(
+        this: this,
+        res: ig.Physics.TraceResult,
+        x: number,
+        y: number,
+        vx: number,
+        vy: number,
+        objectWidth: number,
+        objectHeight: number,
+        ignoreHoles: boolean,
+        groundBlocks: boolean,
+      ): boolean;
+    }
+    interface CollisionConstructor extends ImpactClass<Collision> {
+      new (data: sc.MapModel.Map, zHeight: number): Collision;
+    }
+  }
+}
