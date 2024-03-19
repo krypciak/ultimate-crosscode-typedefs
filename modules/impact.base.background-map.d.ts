a904dd1ab392f93557e05d7f76a6fb347b5b6b3b
krypek <krypek@tuta.io>
2024-03-19 22:14:39 +0100

    Add ig.MAP.Background

--- modules/impact.base.background-map.d.ts
+++ modules/impact.base.background-map.d.ts
@@ -2,3 +2,26 @@
 // requires impact.base.image
 // requires impact.base.tile-info
 // requires game.config
+
+export {};
+
+declare global {
+  namespace ig.MAP {
+    interface Background extends ig.ChunkedMap {
+      tiles: ig.Image;
+      tilesetName: string;
+      tileInfo: ig.TileInfo;
+      hasAnimatedTiles: boolean;
+      screenRender: boolean;
+      lighter: boolean;
+      _wm: ig.Config;
+
+      clearCached(this: this): void;
+      checkAnimatedTiles(this: this): void;
+      setTileset(this: this, tileset: string): void;
+    }
+    interface BackgroundConstructor extends ImpactClass<Background> {
+      new (data: sc.MapModel.Map, zHeight: number): Background;
+    }
+  }
+}
