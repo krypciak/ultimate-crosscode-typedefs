c30bcc5f0934aaeb603c81f0b8ecf51d8bbfec0b
krypek <krypek@tuta.io>
2024-03-19 22:14:14 +0100

    Add ig.MAP.HeightMap

--- modules/impact.feature.height-map.height-map.d.ts
+++ modules/impact.feature.height-map.height-map.d.ts
@@ -2,3 +2,24 @@
 // requires impact.base.game
 // requires impact.base.image
 // requires game.config
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace MAP {
+      interface HeightMap extends ig.Map {
+        _wm: ig.Config;
+        noMerge: boolean;
+
+        draw(this: this): void;
+        drawTiled(this: this): void;
+      }
+      interface HeightMapConstructor extends ImpactClass<HeightMap> {
+        levelKey: 'heightMap';
+        new (data: sc.MapModel.Map, zHeight: number): HeightMap;
+      }
+      
+    }
+  }
+}
