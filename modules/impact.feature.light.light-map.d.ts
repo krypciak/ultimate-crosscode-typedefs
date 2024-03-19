1e4141f899430b5e76e3aae1bd3e359a1b0d34fd
krypek <krypek@tuta.io>
2024-03-19 22:14:18 +0100

    Add ig.MAP.Light

--- modules/impact.feature.light.light-map.d.ts
+++ modules/impact.feature.light.light-map.d.ts
@@ -3,3 +3,27 @@
 // requires impact.base.game
 // requires impact.base.image
 // requires game.config
+
+export {};
+
+declare global {
+  namespace ig.MAP {
+    interface Light extends ig.ChunkedMap {
+      lightmapGfx: ig.Image;
+      _wm: ig.Config;
+      lightSources: unknown;
+      glowSources: unknown;
+      noMerge: boolean;
+      lightCanvas: unknown;
+      shadowOrder: number;
+
+      drawShadows(this: this): void;
+      drawGlow(this: this): void;
+      draw(this: this): void;
+      drawTiled(this: this): void;
+    }
+    interface LightConstructor extends ImpactClass<Light> {
+      new (data: sc.MapModel.Map, zHeight: number): Light;
+    }
+  }
+}
