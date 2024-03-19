7d84a3a38c61d1565458aa9ced3845b2faf4913c
krypek <krypek@tuta.io>
2024-03-19 22:15:01 +0100

    Update ig.Renderer2d

--- modules/impact.base.renderer.d.ts
+++ modules/impact.base.renderer.d.ts
@@ -1,16 +1,36 @@
 // requires impact.base.image
 // requires impact.base.sprite
 
-export { }
+export {};
 
 declare global {
-    namespace ig {
-        interface Renderer2d extends ig.Class {
-            drawPostLayerSprites(this: this): void
-        }
-        interface Renderer2dConstructor extends ImpactClass<Renderer2d> {
-            new(): Renderer2d
-        }
-        var Renderer2d: Renderer2dConstructor
+  namespace ig {
+    namespace Renderer2d {
+      interface SpriteDrawSlot extends ig.Class {
+        cubeSprite: ig.CubeSprite;
+        ground: boolean;
+        yIndex: number;
+        zMin: number;
+        zMax: number;
+        spriteIdx: number;
+        drawShadow: boolean;
+
+        update(this: this, drawShadow: boolean): void;
+        draw(this: this, zMin: number, zMax: number): void;
+      }
+      interface SpriteDrawSlotConstructor extends ImpactClass<SpriteDrawSlot> {
+        new (cubeSprite: ig.CubeSprite, ground: boolean, spriteIdx: number): SpriteDrawSlot;
+      }
     }
+    interface Renderer2d extends ig.Class {
+      spriteSlots: ig.Renderer2d.SpriteDrawSlot[];
+      guiSpriteSlots: ig.Renderer2d.SpriteDrawSlot[];
+      drawPostLayerSprites(this: this): void;
+    }
+    interface Renderer2dConstructor extends ImpactClass<Renderer2d> {
+      Renderer2d: ig.Renderer2d.SpriteDrawSlotConstructor;
+      new (): Renderer2d;
+    }
+    var Renderer2d: Renderer2dConstructor;
+  }
 }
