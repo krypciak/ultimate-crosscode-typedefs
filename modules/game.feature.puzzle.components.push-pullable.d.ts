04e60a3309c95812c10e61bef1fe4eb8ab775f84
krypek <krypek@tuta.io>
2024-04-09 14:07:43 +0200

    Add sc.PullableEntity

--- modules/game.feature.puzzle.components.push-pullable.d.ts
+++ modules/game.feature.puzzle.components.push-pullable.d.ts
@@ -1,2 +1,17 @@
 // requires impact.base.entity
 // requires game.feature.interact.map-interact
+
+export {};
+
+declare global {
+  namespace sc {
+    type PullableEntity = ig.ENTITY.WavePushPullBlock | ig.ENTITY.PushPullBlock;
+    interface PushPullable extends ig.Class {
+      entity: PullableEntity;
+    }
+    interface PushPullableConstructor extends ImpactClass<PushPullable> {
+      new (entity: PullableEntity): PushPullable;
+    }
+    var PushPullable: PushPullableConstructor;
+  }
+}
