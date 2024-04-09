51687c79fb08f164c284759f1e7c394ded8853da
krypek <krypek@tuta.io>
2024-04-09 14:06:48 +0200

    Add sc.PlayerPetEntity

--- modules/game.feature.player.entities.player-pet.d.ts
+++ modules/game.feature.player.entities.player-pet.d.ts
@@ -1,1 +1,31 @@
 // requires game.feature.npc.entities.sc-actor
+
+export {};
+
+declare global {
+  namespace sc {
+    namespace PlayerPetEntity {
+      interface Settings {
+        petSkin: PetSkin;
+      }
+      interface PetSkin {}
+    }
+    interface PlayerPetEntity extends sc.ActorEntity {
+      npcEffects: ig.EffectSheet;
+      petSkin: sc.PlayerPetEntity.PetSkin;
+      configs: unknown;
+      pushTimer: number;
+      posOffset: Vec2;
+      state: number;
+      respawnPos: Vec3;
+      idleTimer: number;
+      idleSpecials: number;
+      tempHidden: boolean;
+      effects: { water: ig.EffectSheet };
+    }
+    interface PlayerPetEntityConstructor extends ImpactClass<PlayerPetEntity> {
+      new (x: number, y: number, z: number, settings: sc.PlayerPetEntity.Settings): PlayerPetEntity;
+    }
+    var PlayerPetEntity: PlayerPetEntityConstructor;
+  }
+}
