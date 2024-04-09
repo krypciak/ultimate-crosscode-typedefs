522a6c7de9f48d38f36c964648bb788de2b5e3b1
krypek <krypek@tuta.io>
2024-04-09 14:07:39 +0200

    Add sc.PlayerSkinLibrary

--- modules/game.feature.player.player-skin.d.ts
+++ modules/game.feature.player.player-skin.d.ts
@@ -1,3 +1,36 @@
 // requires impact.base.game
 // requires game.feature.player.player-model
 // requires game.feature.model.game-model
+
+export {};
+
+declare global {
+  namespace sc {
+    interface PlayerSkinBase extends ig.Class {
+      skinType: string;
+      name: string;
+
+      onLoadableComplete(this: this): void;
+      constructSkin(this: this, settings: unknown): void;
+    }
+    interface PlayerSkinBaseConstructor extends ImpactClass<PlayerSkinBase> {
+      new (type: string, skinName: string, settings: unknown): PlayerSkinBase;
+    }
+    var PlayerSkinBase: PlayerSkinBaseConstructor;
+
+    interface PlayerSkinLibrary
+      extends ig.GameAddon,
+        sc.Model,
+        sc.Model.Observer,
+        ig.ExtensionListener {
+      currentSkins: Record<string, sc.PlayerSkinBase>;
+
+      _createSkin(this: this, skinName: string): PlayerSkinBase;
+    }
+    interface PlayerSkinLibraryConstructor extends ImpactClass<PlayerSkinLibrary> {
+      new (): PlayerSkinLibrary;
+    }
+    var PlayerSkinLibrary: PlayerSkinLibraryConstructor;
+    var playerSkins: PlayerSkinLibrary;
+  }
+}
