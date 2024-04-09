294c757f07477e28029696cc8f849ce2b2742684
krypek <krypek@tuta.io>
2024-04-09 14:07:49 +0200

    Add sc.StartLoader

--- modules/game.loader.d.ts
+++ modules/game.loader.d.ts
@@ -1,2 +1,20 @@
 // requires impact.base.timer
 // requires impact.base.loader
+
+export {};
+
+declare global {
+  namespace sc {
+    interface StartLoader extends ig.Loader {
+      timer: ig.Timer;
+      endTimer: number;
+
+      draw(this: this): void;
+      onEnd(this: this): void;
+    }
+    interface StartLoaderConstructor extends ImpactClass<StartLoader> {
+      new (gameClass: sc.CrossCodeConstructor): StartLoader;
+    }
+    var StartLoader: StartLoaderConstructor;
+  }
+}
