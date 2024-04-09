b408af2d5b6284d1ca055ae31d6eedcb5a40c346
krypek <krypek@tuta.io>
2024-04-09 14:05:36 +0200

    Add sc.CombatCharge

--- modules/game.feature.combat.combat-charge.d.ts
+++ modules/game.feature.combat.combat-charge.d.ts
@@ -1,2 +1,27 @@
 // requires game.feature.combat.model.combat-params
 // requires game.feature.combat.entities.ball
+
+export {};
+
+declare global {
+  namespace sc {
+    interface CombatCharge extends ig.Class {
+      fx: ig.ENTITY.Effect;
+      darkness: ig.DarknessHandle;
+      soundHandle: ig.SoundHandle;
+      entity: ig.Entity;
+      stereo: boolean;
+      slowmo: boolean;
+      camera: boolean;
+
+      charge(this: this, element: sc.ELEMENT, chargeStage: number, showCombatArt?: boolean): void;
+      stop(this: this): void;
+      onActionEndDetach(this: this): void;
+      onEntityKillDetach(this: this): void;
+    }
+    interface CombatChargeConstructor extends ImpactClass<CombatCharge> {
+      new (entity: unknown, stereo: boolean, slowmo?: boolean, camera?: boolean): CombatCharge;
+    }
+    var CombatCharge: CombatChargeConstructor;
+  }
+}
