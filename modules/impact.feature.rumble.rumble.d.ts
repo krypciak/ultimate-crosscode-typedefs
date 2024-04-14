f545ec5cf9efb0f995e08f96d7722509ed19bb4a
krypek <krypek@tuta.io>
2024-04-14 22:01:48 +0200

    Add ig.Rumble

--- modules/impact.feature.rumble.rumble.d.ts
+++ modules/impact.feature.rumble.rumble.d.ts
@@ -1,2 +1,17 @@
 // requires impact.base.event
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace ig {
+    interface Rumble extends ig.GameAddon {
+      onPostUpdate(this: this): void;
+      onReset(this: this): void;
+    }
+    interface RumbleConstructor extends ImpactClass<Rumble> {
+      new (): Rumble;
+    }
+    var Rumble: RumbleConstructor;
+  }
+}
