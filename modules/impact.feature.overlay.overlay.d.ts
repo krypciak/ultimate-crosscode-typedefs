097642cc1e1cd2e7f56465e2b470f0811863074c
krypek <krypek@tuta.io>
2024-04-14 22:01:45 +0200

    Add ig.Overlay

--- modules/impact.feature.overlay.overlay.d.ts
+++ modules/impact.feature.overlay.overlay.d.ts
@@ -1,3 +1,18 @@
 // requires impact.base.event
 // requires impact.base.game
 // requires impact.feature.gui.gui
+
+export {};
+
+declare global {
+  namespace ig {
+    interface Overlay extends ig.GameAddon {
+      onDeferredUpdate(this: this): void;
+      onReset(this: this): void;
+    }
+    interface OverlayConstructor extends ImpactClass<Overlay> {
+      new (): Overlay;
+    }
+    var Overlay: OverlayConstructor;
+  }
+}
