cdc853448417041fcef164e8468db438db611c98
krypek <krypek@tuta.io>
2024-04-14 22:00:33 +0200

    Add ig.ENTITY.Effect#new

--- modules/impact.feature.effect.entities.effect.d.ts
+++ modules/impact.feature.effect.entities.effect.d.ts
@@ -5,12 +5,15 @@ export {};
 
 declare global {
   namespace ig.ENTITY {
+    namespace Effect {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface Effect extends ig.Entity {
       target: ig.Entity;
       spriteFilter: number[];
 
       setIgnoreSlowdown(this: this): void;
-      stop(this: this): void
+      stop(this: this): void;
       isDone(this: this): boolean;
       spawnParticle<E extends ig.Entity, S extends ig.Entity.Settings>(
         this: this,
@@ -18,9 +21,11 @@ declare global {
         offset?: Nullable<NullablePartial<Vec3>>,
         settings?: Nullable<S>,
         forceTargetPos?: Nullable<boolean>,
-        ): void;
-      }
-      interface EffectConstructor extends ImpactClass<Effect> {}
+      ): void;
+    }
+    interface EffectConstructor extends ImpactClass<Effect> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.Effect.Settings): Effect;
+    }
     var Effect: EffectConstructor;
   }
 }
