d29a3221e7cc0165475ba59308f173b0d2712047
krypek <krypek@tuta.io>
2024-04-14 21:57:48 +0200

    Add ig.ENTITY.WaveTeleport#new

--- modules/game.feature.puzzle.entities.wave-teleport.d.ts
+++ modules/game.feature.puzzle.entities.wave-teleport.d.ts
@@ -5,16 +5,23 @@
 export {};
 
 declare global {
-    namespace ig.ENTITY {
-        namespace WaveTeleport {
-            interface Teleportable {
-                doTeleport(this: this, portal: ig.ENTITY.WaveTeleport): void;
-            }
-            interface Settings extends ig.Entity.Settings {}
-        }
-        interface WaveTeleport extends ig.AnimatedEntity {}
-        interface WaveTeleportConstructor extends ImpactClass<WaveTeleport> {}
-
-        let WaveTeleport: WaveTeleportConstructor;
+  namespace ig.ENTITY {
+    namespace WaveTeleport {
+      interface Teleportable {
+        doTeleport(this: this, portal: ig.ENTITY.WaveTeleport): void;
+      }
+      interface Settings extends ig.Entity.Settings {}
     }
+    interface WaveTeleport extends ig.AnimatedEntity {}
+    interface WaveTeleportConstructor extends ImpactClass<WaveTeleport> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.WaveTeleport.Settings,
+      ): WaveTeleport;
+    }
+
+    let WaveTeleport: WaveTeleportConstructor;
+  }
 }
