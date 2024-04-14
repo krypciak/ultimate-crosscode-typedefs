caaf1a8b7971b80cabe93de9c4766118f8403a8d
krypek <krypek@tuta.io>
2024-04-14 22:00:38 +0200

    Add ig.EnvParticles

--- modules/impact.feature.env-particles.env-particles.d.ts
+++ modules/impact.feature.env-particles.env-particles.d.ts
@@ -1,2 +1,17 @@
 // requires impact.base.game
 // requires impact.base.loader
+
+export {};
+
+declare global {
+  namespace ig {
+    interface EnvParticles extends ig.GameAddon {
+      onDeferredUpdate(this: this): void;
+      onMidDraw(this: this): void;
+    }
+    interface EnvParticlesConstructor extends ImpactClass<EnvParticles> {
+      new (): EnvParticles;
+    }
+    var EnvParticles: EnvParticlesConstructor;
+  }
+}
