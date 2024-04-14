907a5f9a92cab95e8730064a2fae5acd0c8b4352
krypek <krypek@tuta.io>
2024-04-14 22:00:24 +0200

    Add a bunch of ig.ENTITY particle constructors

--- modules/impact.feature.effect.entities.effect-particle.d.ts
+++ modules/impact.feature.effect.entities.effect-particle.d.ts
@@ -5,20 +5,58 @@ export {};
 
 declare global {
   namespace ig.ENTITY {
+    namespace Particle {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface Particle extends ig.AnimatedEntity {}
-    interface ParticleConstructor extends ImpactClass<Particle> {}
+    interface ParticleConstructor extends ImpactClass<Particle> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.Particle.Settings): Particle;
+    }
     var Particle: ParticleConstructor;
 
+    namespace CopyParticle {
+      interface Settings extends ig.Entity.Settings {}
+    }
+    namespace CopyParticle {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface CopyParticle extends ig.Entity {}
-    interface CopyParticleConstructor extends ImpactClass<CopyParticle> {}
+    interface CopyParticleConstructor extends ImpactClass<CopyParticle> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.CopyParticle.Settings,
+      ): CopyParticle;
+    }
     var CopyParticle: CopyParticleConstructor;
 
+    namespace OffsetParticle {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface OffsetParticle extends ig.ENTITY.Particle {}
-    interface OffsetParticleConstructor extends ImpactClass<OffsetParticle> {}
+    interface OffsetParticleConstructor extends ImpactClass<OffsetParticle> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.OffsetParticle.Settings,
+      ): OffsetParticle;
+    }
     var OffsetParticle: OffsetParticleConstructor;
 
+    namespace RhombusParticle {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface RhombusParticle extends ig.ENTITY.Particle {}
-    interface RhombusParticleConstructor extends ImpactClass<RhombusParticle> {}
+    interface RhombusParticleConstructor extends ImpactClass<RhombusParticle> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.RhombusParticle.Settings,
+      ): RhombusParticle;
+    }
     var RhombusParticle: RhombusParticleConstructor;
   }
 }
