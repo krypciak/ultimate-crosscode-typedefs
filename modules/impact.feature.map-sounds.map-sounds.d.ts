e2c1ee1a01b5663b58cf122665785c5da51e846c
krypek <krypek@tuta.io>
2024-04-14 22:01:42 +0200

    Add ig.MapSounds

--- modules/impact.feature.map-sounds.map-sounds.d.ts
+++ modules/impact.feature.map-sounds.map-sounds.d.ts
@@ -1,1 +1,17 @@
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace ig {
+    interface MapSounds extends ig.GameAddon {
+      onReset(this: this): void;
+      onLevelLoaded(this: this): void;
+      onDeferredUpdate(this: this): void;
+    }
+    interface MapSoundsConstructor extends ImpactClass<MapSounds> {
+      new (): MapSounds;
+    }
+    var MapSounds: MapSoundsConstructor;
+  }
+}
