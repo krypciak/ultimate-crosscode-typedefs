123f8a382fff5b4ba7e6200d8ce3ebbffbd634b2
krypek <krypek@tuta.io>
2024-04-14 21:56:14 +0200

    Add sc.BetaControls

--- modules/game.feature.beta.beta-controls.d.ts
+++ modules/game.feature.beta.beta-controls.d.ts
@@ -1,1 +1,15 @@
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace sc {
+    interface BetaControls extends ig.GameAddon {
+      onPostUpdate(this: this): void;
+    }
+    interface BetaControlsConstructor extends ImpactClass<BetaControls> {
+      new (): BetaControls;
+    }
+    var BetaControls: BetaControlsConstructor;
+  }
+}
