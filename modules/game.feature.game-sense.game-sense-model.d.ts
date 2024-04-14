6d36f21eee711095854a7cb4f9acd54cfc49f69b
krypek <krypek@tuta.io>
2024-04-14 21:56:32 +0200

    Add sc.GameSense

--- modules/game.feature.game-sense.game-sense-model.d.ts
+++ modules/game.feature.game-sense.game-sense-model.d.ts
@@ -2,3 +2,17 @@
 // requires impact.base.game
 // requires game.feature.model.game-model
 // requires game.feature.model.options-model
+
+export {};
+
+declare global {
+  namespace sc {
+    interface GameSense extends ig.GameAddon {
+      onDeferredUpdate(this: this): void;
+    }
+    interface GameSenseConstructor extends ImpactClass<GameSense> {
+      new (): GameSense;
+    }
+    var GameSense: GameSenseConstructor;
+  }
+}
