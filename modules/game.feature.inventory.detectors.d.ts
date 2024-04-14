92ae8e9a26d9c6c3d6707aadd309903143b8a6f4
krypek <krypek@tuta.io>
2024-04-14 21:56:37 +0200

    Add sc.Detectors

--- modules/game.feature.inventory.detectors.d.ts
+++ modules/game.feature.inventory.detectors.d.ts
@@ -1,3 +1,18 @@
 // requires impact.base.game
 // requires game.feature.model.game-model
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace sc {
+    interface Detectors extends ig.GameAddon, sc.Model.Observer {
+      onLevelLoaded(this: this): void;
+      onDeferredUpdate(this: this): void;
+    }
+    interface DetectorsConstructor extends ImpactClass<Detectors> {
+      new (): Detectors;
+    }
+    var Detectors: DetectorsConstructor;
+  }
+}
