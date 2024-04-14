f204a940f6ac83b0b6dc803d42f8f331f864d5a4
krypek <krypek@tuta.io>
2024-04-14 21:59:40 +0200

    Add ig.ENTITY.TouchTrigger

--- modules/impact.feature.base.entities.touch-trigger.d.ts
+++ modules/impact.feature.base.entities.touch-trigger.d.ts
@@ -5,10 +5,10 @@ export {};
 declare global {
   namespace ig {
     enum TOUCH_TRIGGER_TYPE {
-        SET_FALSE,
-        SET_TRUE,
-        WHILE_TOUCH_ADD_1,
-        WHILE_TOUCH_TRUE,
+      SET_FALSE,
+      SET_TRUE,
+      WHILE_TOUCH_ADD_1,
+      WHILE_TOUCH_TRUE,
     }
     namespace ENTITY {
       namespace TouchTrigger {
@@ -22,6 +22,29 @@ declare global {
           startCondition?: string;
         }
       }
+
+      interface TouchTrigger extends ig.Entity {
+        name: string;
+        face: Vec2;
+        touchType: number;
+        touchedCnt: number;
+        startCondition: ig.VarCondition;
+        isOn: boolean;
+        _wm: ig.Config;
+
+        update(this: this): void;
+        setOn(this: this): void;
+        setOff(this: this): void;
+      }
+      interface TouchTriggerConstructor extends ImpactClass<TouchTrigger> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.TouchTrigger.Settings,
+        ): TouchTrigger;
+      }
+      var TouchTrigger: TouchTriggerConstructor;
     }
   }
 }
