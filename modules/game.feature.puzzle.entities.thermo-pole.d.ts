f77c60a4cac021405705a29392a60d686fad5005
krypek <krypek@tuta.io>
2024-04-14 21:57:35 +0200

    Add ig.ENTITY.ElementPole#new

--- modules/game.feature.puzzle.entities.thermo-pole.d.ts
+++ modules/game.feature.puzzle.entities.thermo-pole.d.ts
@@ -5,55 +5,52 @@
 export {};
 
 declare global {
-    namespace sc {
-        namespace ElementPoleGroups {
-            interface Group {
-                currentBall: ig.ENTITY.Ball | null;
-                hitCount: number;
-                poles: ig.ENTITY.ElementPole[];
-            }
-        }
+  namespace sc {
+    namespace ElementPoleGroups {
+      interface Group {
+        currentBall: ig.ENTITY.Ball | null;
+        hitCount: number;
+        poles: ig.ENTITY.ElementPole[];
+      }
+    }
 
-        interface ElementPoleGroups {
-            getGroup(group: string): ElementPoleGroups.Group;
-            onPoleHit(
-                pole: ig.ENTITY.ElementPole,
-                ball: ig.ENTITY.Ball,
-                alreadyHit: boolean,
-            ): boolean;
-            onCancelCheck(pole: ig.ENTITY.ElementPole): boolean;
-        }
-        let ElementPoleGroups: ElementPoleGroups;
+    interface ElementPoleGroups {
+      getGroup(group: string): ElementPoleGroups.Group;
+      onPoleHit(pole: ig.ENTITY.ElementPole, ball: ig.ENTITY.Ball, alreadyHit: boolean): boolean;
+      onCancelCheck(pole: ig.ENTITY.ElementPole): boolean;
+    }
+    let ElementPoleGroups: ElementPoleGroups;
 
-        enum TERMO_POLE_TYPE {
-            LONG,
-            LONG64,
-            SHORT,
-        }
+    enum TERMO_POLE_TYPE {
+      LONG,
+      LONG64,
+      SHORT,
+    }
   }
 
   namespace ig.ENTITY {
-    namespace ElementPole {
-    }
+    namespace ElementPole {}
   }
-    namespace ig.ENTITY {
-        namespace ElementPole {
-            interface Charge {
-                element: sc.ELEMENT;
-            }
-            interface Settings extends ig.Entity.Settings {
-                group: string;
-                spawnCondition?: string;
-                poleType?: keyof typeof sc.TERMO_POLE_TYPE;
-            }
-        }
-        interface ElementPole extends ig.AnimatedEntity {
-            group: string;
-            charge: ElementPole.Charge;
-
-            ballHit(this: this, ball: ig.ENTITY.Ball): boolean;
-        }
-        interface ElementPoleConstructor extends ImpactClass<ElementPole> {}
-        let ElementPole: ElementPoleConstructor;
+  namespace ig.ENTITY {
+    namespace ElementPole {
+      interface Charge {
+        element: sc.ELEMENT;
+      }
+      interface Settings extends ig.Entity.Settings {
+        group: string;
+        spawnCondition?: string;
+        poleType?: keyof typeof sc.TERMO_POLE_TYPE;
+      }
     }
+    interface ElementPole extends ig.AnimatedEntity {
+      group: string;
+      charge: ElementPole.Charge;
+
+      ballHit(this: this, ball: ig.ENTITY.Ball): boolean;
+    }
+    interface ElementPoleConstructor extends ImpactClass<ElementPole> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.ElementPole.Settings): ElementPole;
+    }
+    let ElementPole: ElementPoleConstructor;
+  }
 }
