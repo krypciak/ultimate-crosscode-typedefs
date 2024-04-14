cb1c04aaf27d9f5331fd86d5946e3a0e2f642daa
krypek <krypek@tuta.io>
2024-04-14 21:56:28 +0200

    Add ig.ENTITY.Projectile.Settings

--- modules/game.feature.combat.entities.projectile.d.ts
+++ modules/game.feature.combat.entities.projectile.d.ts
@@ -6,12 +6,17 @@ export {};
 
 declare global {
   namespace ig.ENTITY {
+    namespace Projectile {
+      interface Settings extends ig.Entity.Settings {}
+    }
     interface Projectile extends ig.AnimatedEntity {
       getHitCenter(this: this, entity: ig.Entity, dest?: Vec3): Vec3;
       getElement(this: this): sc.ELEMENT;
       getCombatantRoot(this: this): ig.ENTITY.Combatant;
     }
-    interface ProjectileConstructor extends ImpactClass<Projectile> {}
+    interface ProjectileConstructor extends ImpactClass<Projectile> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.Projectile.Settings): Projectile;
+    }
     var Projectile: ProjectileConstructor;
   }
 }
