53169569f3048b8ddc90ea51c79ffca1d1cdcb5e
krypek <krypek@tuta.io>
2024-04-14 22:01:52 +0200

    Add ig.ScreenBlur

--- modules/impact.feature.screen-blur.screen-blur.d.ts
+++ modules/impact.feature.screen-blur.screen-blur.d.ts
@@ -1,1 +1,18 @@
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace ig {
+    interface ScreenBlur extends ig.GameAddon {
+      onPostUpdate(this: this): void;
+      onPreDraw(this: this): void;
+      onPostDraw(this: this): void;
+      onReset(this: this): void;
+    }
+    interface ScreenBlurConstructor extends ImpactClass<ScreenBlur> {
+      new (): ScreenBlur;
+    }
+    var ScreenBlur: ScreenBlurConstructor;
+  }
+}
