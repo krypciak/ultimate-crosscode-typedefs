1450529bce72a5a68fe555e9d53e4001d91371b5
krypek <krypek@tuta.io>
2024-04-29 18:22:34 +0200

    Update sc.NewGameModeSelectDialog

--- modules/game.feature.menu.gui.new-game.new-game-dialogs.d.ts
+++ modules/game.feature.menu.gui.new-game.new-game-dialogs.d.ts
@@ -5,16 +5,41 @@
 // requires impact.feature.interact.gui.focus-gui
 // requires game.feature.menu.gui.menu-misc
 
-export {}
+export {};
 
 declare global {
-    namespace sc {
-        interface NewGameModeSelectDialog extends ig.GuiElementBase {
+  namespace sc {
+    interface NewGameModeSelectDialog extends ig.GuiElementBase {
+      gfx: ig.Image;
+      msgBox: sc.CenterBoxGui;
+      normal: sc.NewGameModeDialogButton;
+      plus: sc.NewGameModeDialogButton;
+      header: sc.TextGui;
+      content: ig.GuiElementBase;
+      info: sc.TextGui;
+      buttons: [];
+      buttonInteract: ig.ButtonInteractEntry;
+      buttongroup: sc.ButtonGroup;
+      callback: (button: sc.ButtonGui, dialog: sc.NewGameModeSelectDialog) => void;
 
-        }
-        interface NewGameModeSelectDialogConstructor extends ImpactClass<NewGameModeSelectDialog> {
-            new (): NewGameModeSelectDialog
-        }
-        var NewGameModeSelectDialog: NewGameModeSelectDialogConstructor
+      show(this: this): void;
+      hide(this: this): void;
+      onBackButtonCheck(this: this): boolean;
     }
+    interface NewGameModeSelectDialogConstructor extends ImpactClass<NewGameModeSelectDialog> {
+      new (callback: sc.NewGameModeSelectDialog['callback']): NewGameModeSelectDialog;
+    }
+    var NewGameModeSelectDialog: NewGameModeSelectDialogConstructor;
+
+    interface NewGameModeDialogButton extends sc.ButtonGui {
+      framePatch: ig.NinePatch;
+      gfx: ig.Image;
+      frame: ig.BoxGui;
+      image: ig.ImageGui;
+    }
+    interface NewGameModeDialogButtonConstructor extends ImpactClass<NewGameModeDialogButton> {
+      new (text: sc.TextLike, data?: sc.ButtonGui['data']): NewGameModeDialogButton;
+    }
+    var NewGameModeDialogButton: NewGameModeDialogButtonConstructor;
+  }
 }
