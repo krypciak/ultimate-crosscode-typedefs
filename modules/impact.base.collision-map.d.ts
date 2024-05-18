256828650f5d947f33cf726dfefabb92306d34bf
krypek <krypek@tuta.io>
2024-05-18 21:32:32 +0200

    Add ig.COLLISION

--- modules/impact.base.collision-map.d.ts
+++ modules/impact.base.collision-map.d.ts
@@ -4,46 +4,53 @@
 export {};
 
 declare global {
-  namespace ig.MAP {
-    interface Collision extends ig.Map {
-      _wm: ig.Config;
+  namespace ig {
+    namespace MAP {
+      interface Collision extends ig.Map {
+        _wm: ig.Config;
 
-      prepare(this: this, neighbourCollisionLayer?: ig.MAP.Collision, yDiff?: number): void;
-      isTileGround(this: this, entityX: number, entityY: number): boolean;
-      isTileAreaBlocked(
-        this: this,
-        entityX: number,
-        entityY: number,
-        width: number,
-        height: number,
-      ): boolean;
-      isGridHole(this: this, x: number, y: number): boolean;
-      isOverHole(
-        this: this,
-        entityX: number,
-        entityY: number,
-        width: number,
-        height: number,
-        dirToHole?: Vec2,
-      ): boolean;
-      trace(
-        this: this,
-        res: ig.Physics.TraceResult,
-        x: number,
-        y: number,
-        vx: number,
-        vy: number,
-        objectWidth: number,
-        objectHeight: number,
-        ignoreHoles: boolean,
-        groundBlocks: boolean,
-      ): boolean;
-    }
-    interface CollisionConstructor extends ImpactClass<Collision> {
-      new (data: sc.MapModel.MapLayer, zHeight: number): Collision;
+        prepare(this: this, neighbourCollisionLayer?: ig.MAP.Collision, yDiff?: number): void;
+        isTileGround(this: this, entityX: number, entityY: number): boolean;
+        isTileAreaBlocked(
+          this: this,
+          entityX: number,
+          entityY: number,
+          width: number,
+          height: number,
+        ): boolean;
+        isGridHole(this: this, x: number, y: number): boolean;
+        isOverHole(
+          this: this,
+          entityX: number,
+          entityY: number,
+          width: number,
+          height: number,
+          dirToHole?: Vec2,
+        ): boolean;
+        trace(
+          this: this,
+          res: ig.Physics.TraceResult,
+          x: number,
+          y: number,
+          vx: number,
+          vy: number,
+          objectWidth: number,
+          objectHeight: number,
+          ignoreHoles: boolean,
+          groundBlocks: boolean,
+        ): boolean;
+      }
+      interface CollisionConstructor extends ImpactClass<Collision> {
+        new (data: sc.MapModel.MapLayer, zHeight: number): Collision;
 
-      levelKey: 'collision';
-      staticNoCollision: ig.MAP.Collision;
+        levelKey: 'collision';
+        staticNoCollision: ig.MAP.Collision;
+      }
     }
+    var COLLISION: {
+      EPS: number;
+      SLIP_PIXELS: number;
+      HEIGHT_TOLERATE: number;
+    };
   }
 }
