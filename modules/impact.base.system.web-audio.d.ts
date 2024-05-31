668a53615ec0042676f0ad97d4610cfdb6dc2a08
krypek <krypek@tuta.io>
2024-05-31 09:42:19 +0200

    Update ig.WebAudioBufferGain

--- modules/impact.base.system.web-audio.d.ts
+++ modules/impact.base.system.web-audio.d.ts
@@ -1,37 +1,49 @@
 // requires impact.base.loader
 
-export {}
+export {};
 
 declare global {
-    namespace ig {
-        interface WebAudio extends ig.Class {
-            context: null | AudioContext
-            timeOffset: number
+  namespace ig {
+    interface WebAudio extends ig.Class {
+      context: null | AudioContext;
+      timeOffset: number;
 
-            getSampleRate(this: this): number
-            getCurrentTime(this: this): number
-            getCurrentTimeRaw(this: this): number
-            createBufferGain(this: this, buffer: AudioBufferSourceNode,
-                volume?: number, playbackRate?: number): ig.WebAudioBufferGain
-            createPanner(this: this): PannerNode
-        }
-        interface WebAudioConstructor extends ImpactClass<WebAudio> {
-            new (): ig.WebAudio
-        }
-        var WebAudio: WebAudioConstructor
-
-        interface WebAudioBufferGain extends ig.Class {
-            context: AudioContext
-            bufferNode: AudioBufferSourceNode
-            gainNode: GainNode
-
-            connect(this: this, node: AudioNode): void
-            setLoop(this: this, loop: boolean): void
-            play(this: this, a: number, b: number): void
-        }
-        interface WebAudioBufferGainConstructor extends ImpactClass<WebAudioBufferGain> {
-            new (context: AudioContext, buffer: AudioBufferSourceNode, volume?: number, playbackRate?: number): ig.WebAudioBufferGain
-        }
-        var WebAudioBufferGain: WebAudioBufferGainConstructor
+      getSampleRate(this: this): number;
+      getCurrentTime(this: this): number;
+      getCurrentTimeRaw(this: this): number;
+      createBufferGain(
+        this: this,
+        buffer: AudioBufferSourceNode,
+        volume?: number,
+        playbackRate?: number,
+      ): ig.WebAudioBufferGain;
+      createPanner(this: this): PannerNode;
     }
+    interface WebAudioConstructor extends ImpactClass<WebAudio> {
+      new (): ig.WebAudio;
+    }
+    var WebAudio: WebAudioConstructor;
+
+    interface WebAudioBufferGain extends ig.Class {
+      context: AudioContext;
+      bufferNode: AudioBufferSourceNode;
+      gainNode: GainNode;
+
+      connect(this: this, node: AudioNode): void;
+      disconnect(this: this, node: AudioNode): void;
+      setLoop(this: this, loop: boolean): void;
+      setVolume(this: this, volume: number): void;
+      play(this: this, when: number, offset: number): void;
+      stop(this: this, node: AudioNode): void;
+    }
+    interface WebAudioBufferGainConstructor extends ImpactClass<WebAudioBufferGain> {
+      new (
+        context: AudioContext,
+        buffer: AudioBufferSourceNode,
+        volume?: number,
+        playbackRate?: number,
+      ): ig.WebAudioBufferGain;
+    }
+    var WebAudioBufferGain: WebAudioBufferGainConstructor;
+  }
 }
