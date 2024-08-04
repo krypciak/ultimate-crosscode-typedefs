939912499f0768967e5a596315c6a29b3caa2adb
krypek <krypek@tuta.io>
2024-08-04 16:35:06 +0200

    Add sc.MaxNumberGui

--- modules/game.feature.gui.base.numbers.d.ts
+++ modules/game.feature.gui.base.numbers.d.ts
@@ -46,8 +46,8 @@ declare global {
     interface NumberGui extends ig.GuiElementBase {
       maxNumber: number;
       showPlus: boolean;
-      targetNumber: number; 
-      scramble: boolean
+      targetNumber: number;
+      scramble: boolean;
 
       setNumber(this: this, number: number, skipTransition?: Nullable<boolean>): void;
       setMaxNumber(this: this, number: number): void;
@@ -57,5 +57,19 @@ declare global {
       new (maxValue: number, settings?: sc.NumberGui.Settings): sc.NumberGui;
     }
     var NumberGui: NumberGuiConstructor;
+
+    interface MaxNumberGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+      currentNumberGui: sc.NumberGui;
+      maxNumberGui: sc.NumberGui;
+
+      getMaxNumber(this: this): number;
+      setMaxNumber(this: this, max: number, skipTransition?: Nullable<boolean>): void;
+      setNumber(this: this, number: number, skipTransition?: Nullable<boolean>): void;
+    }
+    interface MaxNumberGuiConstructor extends ImpactClass<MaxNumberGui> {
+      new (max: number, transitionTime?: number): MaxNumberGui;
+    }
+    var MaxNumberGui: MaxNumberGuiConstructor;
   }
 }
