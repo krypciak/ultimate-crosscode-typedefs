7e3359cf5f989fc212900f8366c20e18ea45c383
krypek <krypek@tuta.io>
2024-08-16 20:29:17 +0200

    Add a bunch of area related typedefs

--- modules/game.feature.menu.gui.map.map-misc.d.ts
+++ modules/game.feature.menu.gui.map.map-misc.d.ts
@@ -7,6 +7,13 @@ export {};
 
 declare global {
   namespace sc {
+    interface MapCurrentRoomWrapper extends ig.GuiElementBase {
+      gfx: ig.Image;
+    }
+    interface MapCurrentRoomWrapperConstructor extends ImpactClass<MapCurrentRoomWrapper> {
+      new (activeRoom: ig.GuiHook): MapCurrentRoomWrapper;
+    }
+    var MapCurrentRoomWrapper: MapCurrentRoomWrapperConstructor;
     interface LandmarkGui extends ig.FocusGui {
       map: sc.AreaLoadable.Map;
     }
@@ -32,13 +39,42 @@ declare global {
       name: sc.MapNameGui;
       hasText: boolean;
 
-      setText(this: this, text: sc.TextLike, wait?: Nullable<boolean>, skip?: Nullable<boolean>): void;
+      setText(
+        this: this,
+        text: sc.TextLike,
+        wait?: Nullable<boolean>,
+        skip?: Nullable<boolean>,
+      ): void;
     }
     interface WorldmapAreaNameConstructor extends ImpactClass<WorldmapAreaName> {
       new (): WorldmapAreaName;
     }
     var WorldmapAreaName: WorldmapAreaNameConstructor;
 
+    interface MapCursor extends ig.GuiElementBase, sc.Model.Observer {
+      gfx: ig.Image;
+      focusOffset: Vec2;
+      focus: boolean;
+      _focusTimer: number;
+      _focusTime: number;
+      _focusOffset: number;
+      _lastDevice: number;
+      _gamepadActive: boolean;
+      _worldmap: boolean;
+      _ignoreModel: boolean;
+
+      focusOnNode(this: this, x: number, y: number): void;
+      unfocus(this: this): void;
+      moveTo(this: this, x: number, y: number, smooth?: boolean, transitionTime?: number): void;
+      looseFocus(this: this): void;
+      addObservers(this: this): void;
+      removeObservers(this: this): void;
+      update(this: this): void;
+    }
+    interface MapCursorConstructor extends ImpactClass<MapCursor> {
+      new (worldmap: boolean): MapCursor;
+    }
+    var MapCursor: MapCursorConstructor;
     interface MapChestDisplay extends ig.GuiElementBase {
       current: sc.NumberGui;
       max: sc.NumberGui;
