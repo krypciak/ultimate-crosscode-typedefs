309ab14697c533aef63dc018b046121b4a36b74f
krypek <krypek@tuta.io>
2024-08-17 18:39:51 +0200

    Add ig.ENTITY.EnemySpawner

--- modules/game.feature.combat.entities.enemy-spawner.d.ts
+++ modules/game.feature.combat.entities.enemy-spawner.d.ts
@@ -15,7 +15,42 @@ declare global {
           enemyTypes?: ig.ENTITY.EnemySpawner.Entry[];
           onActiveClear?: boolean;
         }
+
+        interface EnemyTypesEntry {
+          info: sc.EnemyInfo.Settings;
+          type: sc.EnemyType;
+          count: number;
+          activeEnemies: [];
+        }
       }
+      interface EnemySpawner extends ig.Entity {
+        enemyTypes: ig.ENTITY.EnemySpawner.EnemyTypesEntry[];
+        maxEnemies: number;
+        respawnTimer: number;
+        initialSpawn: boolean;
+        effectInitialSpawn: boolean;
+        _wm: ig.Config;
+        onActiveClear?: boolean;
+
+        _storeState(this: this, clearState?: boolean): void;
+        _getVarPrefix(this: this): string;
+        isCleared(this: this): boolean;
+        respawnEnemies(this: this, showEffects?: boolean, killState?: number[]): void;
+        spawnEnemy(
+          this: this,
+          entry: ig.ENTITY.EnemySpawner.EnemyTypesEntry,
+          showEffects?: boolean
+        ): ig.ENTITY.Enemy;
+      }
+      interface EnemySpawnerConstructor extends ImpactClass<EnemySpawner> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.EnemySpawner.Settings,
+        ): EnemySpawner;
+      }
+      var EnemySpawner: EnemySpawnerConstructor;
     }
   }
 }
