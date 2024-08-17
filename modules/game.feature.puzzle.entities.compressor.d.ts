7e138086dd9ddee36d123e6c2c11510e691c8dd5
krypek <krypek@tuta.io>
2024-08-17 18:57:39 +0200

    Add ig.ENTITY.AntiCompressor

--- modules/game.feature.puzzle.entities.compressor.d.ts
+++ modules/game.feature.puzzle.entities.compressor.d.ts
@@ -5,84 +5,101 @@
 export {};
 
 declare global {
-    namespace ig {
-        namespace ENTITY {
-            namespace Compressor {
-                interface Settings extends ig.Entity.Settings {
-                    followCamera?: boolean;
-                    basllSpeed?: number;
-                    fastMode?: boolean;
-                }
-            }
-            interface Compressor extends ig.AnimatedEntity {
-                currentElement: sc.ELEMENT
-                compressorBall: sc.CompressedBaseEntity
-                ballSpeed: number
-                fastMode: boolean
-
-                createCompressorBall(this: this): void
-            }
-            interface CompressorConstructor extends ImpactClass<Compressor> {
-                new (x: number, y: number, z: number, settings: ig.ENTITY.Compressor.Settings): Compressor
-            }
-            var Compressor: CompressorConstructor
-
-            namespace AntiCompressor {
-                interface Settings extends ig.Entity.Settings {}
-            }
+  namespace ig {
+    namespace ENTITY {
+      namespace Compressor {
+        interface Settings extends ig.Entity.Settings {
+          followCamera?: boolean;
+          basllSpeed?: number;
+          fastMode?: boolean;
         }
+      }
+      interface Compressor extends ig.AnimatedEntity {
+        currentElement: sc.ELEMENT;
+        compressorBall: sc.CompressedBaseEntity;
+        ballSpeed: number;
+        fastMode: boolean;
+
+        createCompressorBall(this: this): void;
+      }
+      interface CompressorConstructor extends ImpactClass<Compressor> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Compressor.Settings): Compressor;
+      }
+      var Compressor: CompressorConstructor;
+
+      namespace AntiCompressor {
+        interface Settings extends ig.Entity.Settings {}
+      }
+      interface AntiCompressor extends ig.AnimatedEntity {
+        _wm: ig.Config;
+        effects: unknown;
+
+        onHideRequest(this: this): void;
+        onEffectEvent(this: this, effect: ig.ENTITY.Effect): void;
+        ballHit(this: this, entity: ig.Entity): boolean;
+      }
+      interface AntiCompressorConstructor extends ImpactClass<AntiCompressor> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.AntiCompressor.Settings,
+        ): AntiCompressor;
+      }
+      var AntiCompressor: AntiCompressorConstructor;
     }
-    namespace sc {
-        namespace CompressedBaseEntity {
-            interface Settings extends ig.Entity.Settings {}
-        }
-        interface CompressedBaseEntity extends ig.AnimatedEntity {
-            nudgeTimer: number;
-            killTimer: number;
-            globalCount: number;
-            speedFactor: number;
-            combatant: ig.ENTITY.Combatant;
-            attackInfo: sc.AttackInfo;
-            fastMode: boolean;
-
-            getElement(this: this): sc.ELEMENT;
-        }
-        interface CompressedBaseEntityConstructor extends ImpactClass<CompressedBaseEntity> {
-            new (
-                x: number,
-                y: number,
-                z: number,
-                settings: CompressedBaseEntity.Settings,
-            ): CompressedBaseEntity;
-        }
-        let CompressedBaseEntity: CompressedBaseEntityConstructor;
-
-        namespace CompressedWaveEntity {
-            interface Settings extends CompressedBaseEntity.Settings {}
-        }
-        interface CompressedWaveEntity extends CompressedBaseEntity {}
-        interface CompressedWaveEntityConstructor extends ImpactClass<CompressedWaveEntity> {
-            new (
-                x: number,
-                y: number,
-                z: number,
-                settings: CompressedWaveEntity.Settings,
-            ): CompressedWaveEntity;
-        }
-        let CompressedWaveEntity: CompressedWaveEntityConstructor;
-
-        namespace CompressedShockEntity {
-            interface Settings extends CompressedBaseEntity.Settings {}
-        }
-        interface CompressedShockEntity extends CompressedBaseEntity {}
-        interface CompressedShockEntityConstructor extends ImpactClass<CompressedShockEntity> {
-            new (
-                x: number,
-                y: number,
-                z: number,
-                settings: CompressedWaveEntity.Settings,
-            ): CompressedShockEntity;
-        }
-        let CompressedShockEntity: CompressedShockEntityConstructor;
+  }
+  namespace sc {
+    namespace CompressedBaseEntity {
+      interface Settings extends ig.Entity.Settings {}
     }
+    interface CompressedBaseEntity extends ig.AnimatedEntity {
+      nudgeTimer: number;
+      killTimer: number;
+      globalCount: number;
+      speedFactor: number;
+      combatant: ig.ENTITY.Combatant;
+      attackInfo: sc.AttackInfo;
+      fastMode: boolean;
+
+      getElement(this: this): sc.ELEMENT;
+    }
+    interface CompressedBaseEntityConstructor extends ImpactClass<CompressedBaseEntity> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: CompressedBaseEntity.Settings,
+      ): CompressedBaseEntity;
+    }
+    let CompressedBaseEntity: CompressedBaseEntityConstructor;
+
+    namespace CompressedWaveEntity {
+      interface Settings extends CompressedBaseEntity.Settings {}
+    }
+    interface CompressedWaveEntity extends CompressedBaseEntity {}
+    interface CompressedWaveEntityConstructor extends ImpactClass<CompressedWaveEntity> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: CompressedWaveEntity.Settings,
+      ): CompressedWaveEntity;
+    }
+    let CompressedWaveEntity: CompressedWaveEntityConstructor;
+
+    namespace CompressedShockEntity {
+      interface Settings extends CompressedBaseEntity.Settings {}
+    }
+    interface CompressedShockEntity extends CompressedBaseEntity {}
+    interface CompressedShockEntityConstructor extends ImpactClass<CompressedShockEntity> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: CompressedWaveEntity.Settings,
+      ): CompressedShockEntity;
+    }
+    let CompressedShockEntity: CompressedShockEntityConstructor;
+  }
 }
