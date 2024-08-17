c1d94f8a746b52d700e99f5f3633d0b21dce8adc
krypek <krypek@tuta.io>
2024-08-17 18:55:23 +0200

    Add ig.ENTITY.Magnet

--- modules/game.feature.puzzle.entities.magnet.d.ts
+++ modules/game.feature.puzzle.entities.magnet.d.ts
@@ -10,10 +10,40 @@ declare global {
     namespace ENTITY {
       namespace Magnet {
         interface Settings extends ig.Entity.Settings {
-          dir: keyof typeof ig.ActorEntity.FACE4
+          dir: keyof typeof ig.ActorEntity.FACE4;
           altDirs?: string[]; // array of conditions
         }
       }
+      interface Magnet extends ig.AnimatedEntity {
+        dir: number;
+        targetDir: number;
+        defaultDir: number;
+        face: Vec2;
+        altDirs: { condition: ig.VarCondition; dir: ig.ActorEntity.FACE4 }[];
+        pull: {
+          timer: number;
+          entities: ig.Entity[];
+          pulling: ig.Entity[];
+          moved: ig.Entity[];
+          done: boolean;
+        };
+        effects: { sheet: ig.EffectSheet; handle: ig.ENTITY.Effect };
+        _wm: ig.Config;
+
+        onHideRequest(this: this): void;
+        onEffectEvent(this: this, effect: ig.ENTITY.Effect): void;
+        startPull(this: this): void;
+        fetchPullEntities(this: this): void;
+        isPullActive(this: this): boolean;
+        pullEntities(this: this): void;
+        varsChanged(this: this): void;
+        updateDir(this: this, forceSet?: boolean): void;
+        ballHit(this: this, entity: ig.Entity): boolean;
+      }
+      interface MagnetConstructor extends ImpactClass<Magnet> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Magnet.Settings): Magnet;
+      }
+      var Magnet: MagnetConstructor;
     }
   }
 }
