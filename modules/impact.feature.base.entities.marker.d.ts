b0c7158c4a1d94d0d1434c50b6295e7d16e8f2f6
krypek <krypek@tuta.io>
2024-08-17 18:59:16 +0200

    Add ig.ENTITY.Marker

--- modules/impact.feature.base.entities.marker.d.ts
+++ modules/impact.feature.base.entities.marker.d.ts
@@ -10,6 +10,18 @@ declare global {
           dir: keyof typeof ig.ActorEntity.FACE8;
         }
       }
+      interface Marker extends ig.Entity {
+        name: string;
+        dir: unknown;
+        face: Vec2;
+        _wm: ig.Config;
+
+        applyMarkerPosition(this: this, entity: ig.Entity): void;
+      }
+      interface MarkerConstructor extends ImpactClass<Marker> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Marker.Settings): Marker;
+      }
+      var Marker: MarkerConstructor;
     }
   }
 }
