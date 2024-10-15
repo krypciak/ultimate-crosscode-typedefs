8f12ed4219f81fe090b248d98c1f0922f2bfcea1
krypek <krypek@tuta.io>
2024-10-15 14:50:08 +0200

    Add ig.DoorMat

--- modules/impact.feature.map-content.entities.door.d.ts
+++ modules/impact.feature.map-content.entities.door.d.ts
@@ -7,45 +7,65 @@
 export {};
 
 declare global {
-  namespace ig.ENTITY {
-    namespace Door {
-      interface Settings extends ig.Entity.Settings {
-        doorType?: keyof typeof ig.DOOR_TYPE;
-        transitionType?: 'REGULAR' | 'INTER_AREA';
-        narrow?: boolean;
+  namespace ig {
+    namespace ENTITY {
+      namespace Door {
+        interface Settings extends ig.Entity.Settings {
+          doorType?: keyof typeof ig.DOOR_TYPE;
+          transitionType?: 'REGULAR' | 'INTER_AREA';
+          narrow?: boolean;
+          map: string;
+          marker: string;
+          dir?: keyof typeof ig.ActorEntity.FACE4;
+          variation?: string;
+          condition?: string;
+          blockEvent?: unknown; // steps: e.blockEvent
+          blockEventCondition?: string;
+          hideCondition?: string;
+        }
+      }
+      interface Door extends ig.AnimatedEntity {
+        doorType: keyof typeof ig.DOOR_TYPE;
+        condition: ig.VarCondition;
         map: string;
         marker: string;
-        dir?: keyof typeof ig.ActorEntity.FACE4;
-        variation?: string;
-        condition?: string;
-        blockEvent?: unknown; // steps: e.blockEvent
-        blockEventCondition?: string;
-        hideCondition?: string;
+        dir: keyof typeof ig.ActorEntity.FACE4;
+        active: boolean;
+        openTimer: number;
+        hasDoorMat: boolean;
+        hasDoorGlow: boolean;
+        hideManager?: ig.EntityHideManager;
+        blockEvent: ig.Event;
+        blockEventCondition: ig.VarCondition;
+        openEffect: ig.EffectHandle;
+        sounds: { activate: ig.Sound; deactivate: ig.Sound };
+        fx: { sheet: ig.EffectSheet };
+        doorMat?: ig.DoorMat;
+
+        close(this: this): void;
+        open(this: this, entity: ig.Entity, data: Vec2 & { dot: number }): void;
+      }
+      interface DoorConstructor extends ImpactClass<Door> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Door.Settings): Door;
+      }
+      var Door: DoorConstructor;
+    }
+
+    namespace DoorMat {
+      interface Settings extends ig.Entity.Settings {
+        dir: keyof typeof ig.ActorEntity.FACE4;
+        active?: boolean;
       }
     }
-    interface Door extends ig.AnimatedEntity {
-      doorType: keyof typeof ig.DOOR_TYPE;
-      condition: ig.VarCondition;
-      map: string;
-      marker: string;
+    interface DoorMat extends ig.AnimatedEntity {
       dir: keyof typeof ig.ActorEntity.FACE4;
-      active: boolean;
-      openTimer: number;
-      hasDoorMat: boolean;
-      hasDoorGlow: boolean;
-      hideManager?: ig.EntityHideManager;
-      blockEvent: ig.Event;
-      blockEventCondition: ig.VarCondition;
-      openEffect: ig.EffectHandle;
-      sounds: { activate: ig.Sound; deactivate: ig.Sound };
-      fx: { sheet: ig.EffectSheet };
+      isActive: boolean;
 
-      close(this: this): void;
-      open(this: this, entity: ig.Entity, data: Vec2 & { dot: number }): void;
+      setActive(this: this, a: unknown): void;
     }
-    interface DoorConstructor extends ImpactClass<Door> {
-      new (x: number, y: number, z: number, settings: ig.ENTITY.Door.Settings): Door;
+    interface DoorMatConstructor extends ImpactClass<DoorMat> {
+      new (x: number, y: number, z: number, settings: ig.DoorMat.Settings): DoorMat;
     }
-    var Door: DoorConstructor;
+    var DoorMat: DoorMatConstructor;
   }
 }
