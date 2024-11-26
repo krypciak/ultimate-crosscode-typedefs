46c3f4f3d469d6ad3f2d55ed3e0672f0afca1d3e
krypek <krypek@tuta.io>
2024-11-26 18:57:17 +0100

    Update ig.WeightTimer

--- modules/impact.base.timer.d.ts
+++ modules/impact.base.timer.d.ts
@@ -1,47 +1,64 @@
-
-export { }
+export {};
 
 declare global {
-    namespace ig {
-        interface Timer extends ig.Class {
-            target: number
-            base: number
-            last: number
-            stopped: boolean
-            
-            set(this: this, seconds: number, keepRelativeDelta: boolean): void //todo
-            reverseRelativeDelta(this: this): void
-            stop(this: this): void
-            resume(this: this): void
-            reset(this: this): void
-            tick(this: this): number
-            weight(this: this): number
-            delta(this: this): number
-        }
-        interface TimerConstructor extends ImpactClass<Timer> {
-            _last: number
-            time: number
-            timeScale: number
-            maxStep: number
+  namespace ig {
+    interface Timer extends ig.Class {
+      target: number;
+      base: number;
+      last: number;
+      stopped: boolean;
 
-            step(): void
-
-            new(target: number): Timer
-        }
-        var Timer: TimerConstructor
-
-        enum TIMER_MODE { ONCE = 0, REPEAT = 1, BLINK = 2, SINUS = 3, SINUS_RND = 4 }
-        interface WeightTimer extends ig.Class {
-            duration: number
-            timer: number
-            actualTick: boolean
-            mode: ig.TIMER_MODE
-            repeatCount: number
-            _rndBool: boolean
-        }
-        interface WeightTimerConstructor extends ImpactClass<WeightTimer> {
-            new(actualTick: boolean, duration: number, mode: ig.TIMER_MODE): WeightTimer
-        }
-        var WeightTimer: WeightTimerConstructor
+      set(this: this, seconds: number, keepRelativeDelta: boolean): void; //todo
+      reverseRelativeDelta(this: this): void;
+      stop(this: this): void;
+      resume(this: this): void;
+      reset(this: this): void;
+      tick(this: this): number;
+      weight(this: this): number;
+      delta(this: this): number;
     }
+    interface TimerConstructor extends ImpactClass<Timer> {
+      _last: number;
+      time: number;
+      timeScale: number;
+      maxStep: number;
+
+      step(): void;
+
+      new (target: number): Timer;
+    }
+    var Timer: TimerConstructor;
+
+    enum TIMER_MODE {
+      ONCE = 0,
+      REPEAT = 1,
+      BLINK = 2,
+      SINUS = 3,
+      SINUS_RND = 4,
+    }
+    interface WeightTimer extends ig.Class {
+      duration: number;
+      timer: number;
+      actualTick: boolean;
+      mode: ig.TIMER_MODE;
+      repeatCount: number;
+      _rndBool: boolean;
+
+      set(this: this, duration: number, mode?: ig.TIMER_MODE): void;
+      getRemainingTime(this: this): number;
+      setRemainingTime(this: this, time: number): void;
+      tick(this: this): void;
+      done(this: this): boolean;
+      getTimePassed(this: this): number;
+      get(this: this): number;
+      getTotalWeight(this: this): number;
+      hasRepeated(this: this): boolean;
+      getRepeatCount(this: this): number;
+      onBlinkDecline(this: this): boolean;
+    }
+    interface WeightTimerConstructor extends ImpactClass<WeightTimer> {
+      new (actualTick: boolean, duration: number, mode: ig.TIMER_MODE): WeightTimer;
+    }
+    var WeightTimer: WeightTimerConstructor;
+  }
 }
