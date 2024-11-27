f845fad3ef0b1246256b48b26e81db7a5c0457c4
krypek <krypek@tuta.io>
2024-11-27 18:15:02 +0100

    Add ig.Clouds

--- modules/impact.feature.weather.clouds.d.ts
+++ modules/impact.feature.weather.clouds.d.ts
@@ -1,1 +1,37 @@
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace ig {
+    interface Clouds extends ig.Class {
+      gfx: ig.Image;
+      currentClouds: [];
+      density: number;
+      vel: Vec2;
+      cloudRange: Vec2;
+      timer: number;
+      maxTime: number;
+      alpha: number;
+      shadowOrder: number;
+
+      clearClouds(this: this, immediately?: boolean): void;
+      setClouds(this: this, density: number, vel: Vec2, alpha: number, immediately?: boolean): void;
+      spawnCloudLine(this: this, immediately?: boolean): void;
+      addCloud(this: this, x: number, y: number, timer?: number): void;
+      moveClouds(
+        this: this,
+        x: number,
+        y: number,
+        startIndex: number,
+        dontIncreaseTimer?: boolean,
+      ): void;
+      update(this: this): void;
+      drawShadows(this: this): void;
+    }
+    interface CloudsConstructor extends ImpactClass<Clouds> {
+      new (): Clouds;
+    }
+    var Clouds: CloudsConstructor;
+  }
+}
