ad793cd757046bf3241b97f706b726db998591ec
krypek <krypek@tuta.io>
2024-11-28 17:54:29 +0100

    Add sc.ItemGuiLayer

--- modules/game.feature.gui.widget.chest-items.d.ts
+++ modules/game.feature.gui.widget.chest-items.d.ts
@@ -1,3 +1,17 @@
 // requires impact.base.image
 // requires impact.feature.gui.gui
 // requires game.feature.inventory.inventory
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ItemGuiLayer extends ig.GuiElementBase {
+      addItem(this: this, entity: ig.Entity, item: sc.ItemID, count: number): void;
+    }
+    interface ItemGuiLayerConstructor extends ImpactClass<ItemGuiLayer> {
+      new (): ItemGuiLayer;
+    }
+    var ItemGuiLayer: ItemGuiLayerConstructor;
+  }
+}
