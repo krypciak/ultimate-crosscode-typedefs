fca3fc84b2c0f47ea29b5db64974d7f39f21dc7e
krypek <krypek@tuta.io>
2024-11-28 18:23:28 +0100

    Update sc.ItemDropEntity

--- modules/game.feature.combat.entities.item-drop.d.ts
+++ modules/game.feature.combat.entities.item-drop.d.ts
@@ -17,8 +17,51 @@ declare global {
     }
     var ITEM_DROP_TYPE: ITEM_DROP_TYPE;
 
-    interface ItemDropEntity extends ig.AnimatedEntity {}
+    namespace ItemDropEntity {
+      interface Settings {
+        dropType: sc.ITEM_DROP_TYPE;
+        vel?: Vec2;
+        target: ig.Entity;
+        item: sc.ItemID;
+        amount: number;
+      }
+    }
+    interface ItemDropEntity extends ig.AnimatedEntity {
+      gfx: ig.Image;
+      effects: ig.EffectSheet;
+      sounds: Record<
+        | 'start'
+        | 'catch'
+        | 'catchLow'
+        | 'catchNormal'
+        | 'catchRate'
+        | 'catchLegendary'
+        | 'catchUnique',
+        ig.Sound
+      >;
+      flying: boolean;
+      target: ig.Entity;
+      timer: number;
+      startPos: Vec2;
+      startZPos: number;
+      maxTime: number;
+      maxHeight: number;
+      item: number;
+      amount: number;
+      collected: boolean;
+      dropType: sc.ITEM_DROP_TYPE;
+      fromCombatant: boolean;
+
+      collectItem(this: this, count: number): void;
+      onSave(this: this): void;
+      update(this: this): void;
+      onTouchGround(this: this): void;
+      startRegularFly(this: this): void;
+      startFlying(this: this, maxTime: number, maxHeight: number): void;
+    }
     interface ItemDropEntityConstructor extends ImpactClass<ItemDropEntity> {
+      new (x: number, y: number, z: number, settings: sc.ItemDropEntity.Settings): ItemDropEntity;
+
       spawnDrops(
         entity: ig.Entity,
         align: ig.ENTITY_ALIGN,
