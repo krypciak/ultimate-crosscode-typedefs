b58c15959ee28319a4a0931a1b4971fecaa631e0
krypek <krypek@tuta.io>
2024-11-29 16:55:16 +0100

    Add sc.FerroEntity

--- modules/game.feature.puzzle.entities.ferro.d.ts
+++ modules/game.feature.puzzle.entities.ferro.d.ts
@@ -1,3 +1,195 @@
 // requires impact.base.entity
 // requires impact.base.actor-entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace FerroSpot {
+        interface Settings extends ig.Entity.Settings {
+          source: boolean;
+          variable: string;
+          activeCondition: string;
+        }
+      }
+      interface FerroSpot extends ig.AnimatedEntity {
+        state: number;
+        reached: boolean;
+        source: boolean;
+        ferro: sc.FerroEntity;
+        searched: boolean;
+        effects: { sheet: ig.EffectSheet; handle: Nullable<ig.ENTITY.Effect> };
+        _wm: ig.Config;
+        variable: string;
+
+        spawnFerro(this: this, spawnEffects?: boolean): void;
+        isActive(this: this): boolean;
+        showPermaFx(this: this): void;
+        clearPermaFx(this: this): void;
+        setOff(this: this): void;
+        setCarry(this: this, spawnEffects?: boolean): void;
+        setStandby(this: this, spawnEffects?: boolean): void;
+        setRespawn(this: this): void;
+        varsChanged(this: this): void;
+        checkNonSourceInactive(this: this): void;
+      }
+      interface FerroSpotConstructor extends ImpactClass<FerroSpot> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.FerroSpot.Settings): FerroSpot;
+      }
+      var FerroSpot: FerroSpotConstructor;
+    }
+  }
+  namespace sc {
+    namespace FerroEntity {
+      interface Settings extends ig.Entity.Settings {
+        spot?: ig.ENTITY.FerroSpot;
+      }
+    }
+    namespace FerroEntity {
+      interface State {
+        noInterrupt?: boolean;
+        noMerge?: boolean;
+        noBarrierStop?: boolean;
+        anims?: Record<string, string>;
+        elementReact?: sc.ELEMENT[];
+
+        start(entity: sc.FerroEntity, vel: Vec2, combatant: sc.BasicCombatant): void | boolean;
+        update(): void | boolean;
+        onQuickStop?(entity: sc.FerroEntity): void;
+        breakIce?(entity: sc.FerroEntity): void;
+        onTouchGround?(entity: sc.FerroEntity, num: number): void;
+        explode?(entity: sc.FerroEntity): void;
+        ballHit?(
+          entity: sc.FerroEntity,
+          vel: Vec2,
+          entity1: ig.Entity,
+          element: sc.ELEMENT,
+        ): boolean;
+        onMoveTrace?(entity: sc.FerroEntity, entity1: ig.Entity): void;
+        iceBreak?(entity: sc.FerroEntity): void;
+        onAttackHit?(entity: sc.FerroEntity): void;
+        ballHitFilter?(entity: sc.FerroEntity): boolean;
+        onOtherBallHit?(entity: sc.FerroEntity): void;
+        onCollideWith?(entity: sc.FerroEntity): void;
+        steam?(entity: sc.FerroEntity, vel: Vec2, combatant: sc.BasicCombatant): void;
+        shootFromWall?(entity: sc.FerroEntity, vel: Vec2, combatant?: sc.BasicCombatant): void;
+      }
+      interface States {
+        IDLE: sc.FerroEntity.State;
+        RESPAWN: sc.FerroEntity.State;
+        DELETE: sc.FerroEntity.State;
+        GATE_ABSORB: sc.FerroEntity.State;
+        BARRIER_BREAK: sc.FerroEntity.State;
+        NEUTRAL: sc.FerroEntity.State;
+        BOUNCE_BACK: sc.FerroEntity.State;
+        BOUNCE_BACK_BORDER: sc.FerroEntity.State;
+        BOUNCE_BACK_SMALL: sc.FerroEntity.State;
+        HEAT: sc.FerroEntity.State;
+        COLD: sc.FerroEntity.State;
+        SHOCK: sc.FerroEntity.State;
+        WAVE: sc.FerroEntity.State;
+        BOMB_FLY: sc.FerroEntity.State;
+        ICEDISK: sc.FerroEntity.State;
+        ICEDISK_SLIDE: sc.FerroEntity.State;
+        WAVE_COMPRESSOR: sc.FerroEntity.State;
+        SHOCK_COMPRESSOR: sc.FerroEntity.State;
+      }
+
+      interface Panel extends ig.Entity {
+        onBubbleBurst(this: this): void;
+      }
+    }
+    interface FerroEntity {
+      state: sc.FerroEntity.State;
+      absorbState: Nullable<sc.FerroEntity.State>;
+      timer: number;
+      ignoreTimer: number;
+      startZ: number;
+      teleportBall: Nullable<ig.ENTITY.Ball>;
+      panel: sc.FerroEntity.Panel;
+      mergeTimer: number;
+      mergeEntity: ig.Entity;
+      mergeStartPos: Vec3;
+      attackInfo: Nullable<sc.AttackInfo>;
+      savedDir: Vec2;
+      element: Nullable<sc.ELEMENT>;
+      combatant: Nullable<ig.ENTITY.Combatant>;
+      collisionList: ig.Entity[];
+      collReleaseTimer: number;
+      collReleaseTimeList: number[];
+      fastMode: boolean;
+      speedFactor: number;
+      phaseMode: boolean;
+      phaseTraveled: number;
+      wallKillTimer: number;
+      enterWall: { timer: number; dir: Vec2 };
+      slidingWall: Vec2;
+      blockCheck: number;
+      turnSoundTimer: number;
+      wallBounces: number;
+      startCollType: ig.COLLTYPE;
+      lastSpot: Nullable<ig.ENTITY.FerroSpot>;
+      effects: Record<
+        'sheet' | 'bomb' | 'bubble' | 'handle' | 'hitHandle' | 'perma' | 'trail',
+        Nullable<ig.EffectSheet>
+      >;
+      _wm: ig.Config;
+
+      remove(this: this, absorb?: boolean): void;
+      respawn(this: this, spot?: ig.ENTITY.FerroSpot): void;
+      isRespawning(this: this): boolean;
+      assignSpot(this: this, spot: ig.ENTITY.FerroSpot): void;
+      resetColl(this: this, dontResetAttackInfo?: boolean): void;
+      getAbsorbAnim(this: this, anim: string): string;
+      handleSurrounding(this: this): void;
+      isCloseInEntity(this: this, entity: ig.Entity): boolean;
+      updateStartZ(this: this): void;
+      setState(
+        this: this,
+        state: sc.FerroEntity.State,
+        vel: Vec2,
+        combatant: sc.BasicCombatant,
+      ): void | boolean;
+      setAbsorbState(
+        this: this,
+        absorbState: sc.FerroEntity.State,
+        vel: Nullable<Vec2>,
+        panel: sc.FerroEntity.Panel,
+      ): void;
+      setMergeEntity(this: this, mergeEntity: ig.Entity): void;
+      clearAbsorbState(this: this): void;
+      overlapActivateCheck(this: this): void;
+      killCloseRegenDestruct(this: this, radius: number, exception?: ig.Entity): void;
+      onPhysicsSquish(this: this): void;
+      onTouchGround(this: this, num: number): void;
+      handleMovementTrace(this: this, coll: ig.CollEntry): void;
+      setTeleportBall(this: this, teleportBall: ig.ENTITY.Ball): void;
+      clearTeleportBall(this: this): void;
+      onEntityKillDetach(this: this): void;
+      doTeleport(this: this): void;
+      onTeleportStart(this: this, waveTeleporter: ig.ENTITY.WaveTeleport): void;
+      getTeleportZOffset(this: this): number;
+      getElement(this: this): sc.ELEMENT;
+      getCombatant(this: this): ig.ENTITY.Player;
+      getCombatantRoot(this: this): ig.ENTITY.Player;
+      getAttackInfo(this: this): sc.AttackInfo;
+      isWaterBubble(this: this): boolean;
+      isIceDisk(this: this): boolean;
+      steam(this: this, vel: Vec2, combatant: sc.BasicCombatant): void;
+      isCompressor(this: this): boolean;
+      consume(this: this): void;
+      onCompressorMoveEnd(this: this, respawnOrSetIdle?: boolean): void;
+      _getAssistFactor(this: this): number;
+      isBallAdjust(this: this): boolean;
+      doBallAdjust(this: this, entity: ig.Entity, dest: Vec2, destSize: Vec3): boolean;
+      isBallDestroyer(this: this): boolean;
+      shootFromWall(this: this, vel: Vec2, combatant?: sc.BasicCombatant): void;
+    }
+    interface FerroEntityConstructor extends ImpactClass<FerroEntity> {
+      new (x: number, y: number, z: number, settings: sc.FerroEntity.Settings): FerroEntity;
+    }
+    var FerroEntity: FerroEntityConstructor;
+  }
+}
