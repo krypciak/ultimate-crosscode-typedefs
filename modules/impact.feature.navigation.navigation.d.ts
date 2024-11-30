cfd66ed1ae3f5b478a19de10160b9e8d0780a42a
krypek <krypek@tuta.io>
2024-11-30 22:34:23 +0100

    Add ig.NavPath

--- modules/impact.feature.navigation.navigation.d.ts
+++ modules/impact.feature.navigation.navigation.d.ts
@@ -4,18 +4,45 @@ export {};
 
 declare global {
   namespace ig {
+    interface NavDodgeType {
+      deltas: number[];
+      start: number;
+    }
+    interface NAV_DODGE_TYPE {
+      NEUTRAL: ig.NavDodgeType;
+      PASSIVE: ig.NavDodgeType;
+      GET_AWAY: ig.NavDodgeType;
+      AGGRESSIVE: ig.NavDodgeType;
+    }
+    var NAV_DODGE_TYPE: NAV_DODGE_TYPE;
+
+    namespace NavPath {
+      interface Options {
+        posOffset: number;
+      }
+      interface Path {
+        nodes: ig.NavPath.Node[];
+        distance: number;
+        multiLevel: number;
+        jumpCount: number;
+      }
+      interface Node {
+        node: ig.PathNodeConnect;
+        connection: ig.PathNodeConnect;
+      }
+    }
     interface NavPath extends ig.Class {
       mapVersion: number;
-      searcher: unknown;
+      searcher: sc.ActorEntity;
       startRelativeVel: number;
       targetEntity: ig.Entity;
       targetPos: Vec3;
       retargetPos: Vec3;
-      retargetNode: unknown;
+      retargetNode?: number;
       maxDistance: number;
       precise: boolean;
-      options: unknown;
-      path: unknown;
+      options?: ig.NavPath.Options;
+      path: Nullable<ig.NavPath.Path>;
       pathIdx: number;
       pathLength: number;
       nextNodeData: { startPos: Vec2; endPos: Vec2; jump: boolean; height: number };
@@ -33,9 +60,77 @@ declare global {
       failCount: number;
       wrongNodeTimer: number;
       lastSideWayDir: Vec2;
+
+      toEntity(
+        this: this,
+        targetEntity: ig.Entity,
+        maxDistance?: number,
+        options?: ig.NavPath.Options,
+        precise?: boolean,
+      ): void;
+      toPoint(this: this, pos: Vec2, maxDistance?: number, precise?: number): void;
+      redoPathDeferred(this: this): void;
+      sideways(
+        this: this,
+        actor: ig.ActorEntity,
+        distance: number,
+        variance: number,
+        throwing: number,
+        keepDir: number,
+        forceMinDistance: number,
+        forceMaxDistance: number,
+        precise?: boolean,
+      ): void;
+      dodge(
+        this: this,
+        actor: ig.ActorEntity,
+        minDist: number,
+        dodgeType?: ig.NAV_DODGE_TYPE,
+      ): void;
+      moveRange(
+        this: this,
+        actor: ig.ActorEntity,
+        b: number,
+        e: number,
+        f: number,
+        throwing?: boolean,
+        i?: boolean,
+      ): boolean;
+      runAway(this: this, actor: ig.ActorEntity, minDist: number, throwing?: boolean): void;
+      runToFace(
+        this: this,
+        actor: ig.ActorEntity,
+        faceRotate: number,
+        minDist: number,
+        maxDist: number,
+        throing?: boolean,
+      ): boolean;
+      _moveCircle(
+        this: this,
+        targetNode: ig.MAP.Navigation,
+        point1: Vec2,
+        point2: Vec2,
+        distance: number,
+        throwing: boolean,
+        minLength?: number,
+        maxLength?: number,
+        keepDirection?: boolean,
+        precise?: boolean,
+      ): boolean;
+      getDistance(this: this): number;
+      getJumpCount(this: this): number;
+      isDestReachable(this: this): boolean;
+      redoPath(this: this): void;
+      redoPath(this: this, pathNode: ig.PathNode, pos: Vec3): void;
+      interrupt(this: this): void;
+      isCurrentNodeInvalid(this: this): boolean;
+      moveEntity(this: this): boolean;
+      avoidEntities(this: this): void;
+      runPath(this: this): void;
+      selectNextTargetPos(this: this): void;
     }
     interface NavPathConstructor extends ImpactClass<NavPath> {
-      new (searcher: unknown): NavPath;
+      new (searcher: sc.ActorEntity): NavPath;
     }
     var NavPath: NavPathConstructor;
 
