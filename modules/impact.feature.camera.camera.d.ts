518a1cfa45e8eff83b6dbe058f34d835379acc9c
krypek <krypek@tuta.io>
2025-02-19 17:03:17 +0100

    Update ig.Camera

--- modules/impact.feature.camera.camera.d.ts
+++ modules/impact.feature.camera.camera.d.ts
@@ -5,47 +5,8 @@ export {};
 
 declare global {
   namespace ig {
-    namespace Camera {
-      interface EntityTarget extends ig.Class {
-        entity: ig.Entity;
-        _zSlow: boolean;
-        _currentZ: number;
-        _lockZ: boolean;
-
-        start(this: this): void;
-        getPos(this: this, outVec: Vec2): void;
-      }
-      interface EntityTargetConstructor extends ImpactClass<EntityTarget> {
-        new (entity: ig.Entity, lockZ?: boolean): EntityTarget;
-      }
-
-      interface TargetHandle extends ig.Class {
-        target: ig.Camera.EntityTarget;
-        offset: Vec2;
-        zoomOffset: Vec2;
-        keepZoomFocusAligned: boolean;
-        _offsetDuration: number;
-        _offsetTime: number;
-        _currentOffset: Vec2;
-        _currentZoomOffset: Vec2;
-        _lastOffset: Vec2;
-        _lastZoomOffset: Vec2;
-        zoomTimer: ig.WeightTimer;
-        oldZoom: number;
-        targetZoom: number;
-        keyspline: KeySpline;
-        lockZ: boolean;
-      }
-      interface TargetHandleConstructor extends ImpactClass<TargetHandle> {
-        new (target: ig.Camera.EntityTarget, x: number, y: number): TargetHandle;
-      }
-    }
-
     interface Camera extends ig.GameAddon {
-      targets: {
-        _currentOffset: Vec2;
-        _currentZoomOffset: Vec2;
-      }[];
+      targets: ig.Camera.TargetHandle[];
       namedTargets: Record<string, ig.Camera.TargetHandle>;
       _currentPos: Vec2;
       _currentZ: number;
@@ -63,14 +24,93 @@ declare global {
       levelLoadStartOrder: number;
 
       onPostUpdate(this: this): void;
+      pushTarget(
+        this: this,
+        target: ig.Camera.TargetHandle,
+        speed?: ig.Camera.SPEED_OPTIONS | keyof typeof ig.Camera.SPEED_OPTIONS,
+        transitionFunction?: KeySpline,
+        name?: string,
+      ): void;
+      _getDuration(
+        this: this,
+        speed: ig.Camera.SPEED_OPTIONS | keyof typeof ig.Camera.SPEED_OPTIONS,
+      ): number;
     }
-    interface CameraConstructor extends ImpactClass<ig.Camera> {
-      new (): ig.Camera;
+    interface CameraConstructor extends ImpactClass<Camera> {
+      new (): Camera;
 
+      SPEED_OPTIONS: ig.Camera.SPEED_OPTIONS;
+      PosTarget: ig.Camera.PosTargetConstructor;
       EntityTarget: ig.Camera.EntityTargetConstructor;
       TargetHandle: ig.Camera.TargetHandleConstructor;
     }
-    var Camera: ig.CameraConstructor;
-    var camera: ig.Camera;
+    var Camera: CameraConstructor;
+    var camera: Camera;
+
+    namespace Camera {
+      interface SPEED_OPTIONS {
+        NORMAL: number;
+        FAST: number;
+        FASTER: number;
+        FASTEST: number;
+        FASTESTEST: number;
+        SLOW: number;
+        SLOWER: number;
+        SLOWEST: number;
+        SLOWESTEST: number;
+        SLOWEST_DREAM: number;
+        IMMEDIATELY: number;
+      }
+
+      interface PosTarget extends ig.Class, ig.Camera.TargetHandle.Target {
+        pos: Vec2;
+
+        start(this: this): void;
+        getPos(this: this, dest: Vec2): void;
+      }
+      interface PosTargetConstructor extends ImpactClass<PosTarget> {
+        new (pos: Vec2): PosTarget;
+      }
+
+      interface EntityTarget extends ig.Class, ig.Camera.TargetHandle.Target {
+        entity: ig.Entity;
+        _zSlow: boolean;
+        _currentZ: number;
+        _lockZ: boolean;
+
+        start(this: this): void;
+        getPos(this: this, dest: Vec2): void;
+      }
+      interface EntityTargetConstructor extends ImpactClass<EntityTarget> {
+        new (entity: ig.Entity, lockZ?: boolean): EntityTarget;
+      }
+
+      namespace TargetHandle {
+        interface Target {
+          start(): void;
+          getPos(dest: Vec2): void;
+        }
+      }
+      interface TargetHandle extends ig.Class {
+        target: ig.Camera.TargetHandle.Target;
+        offset: Vec2;
+        zoomOffset: Vec2;
+        keepZoomFocusAligned: boolean;
+        _offsetDuration: number;
+        _offsetTime: number;
+        _currentOffset: Vec2;
+        _currentZoomOffset: Vec2;
+        _lastOffset: Vec2;
+        _lastZoomOffset: Vec2;
+        zoomTimer: ig.WeightTimer;
+        oldZoom: number;
+        targetZoom: number;
+        keyspline: KeySpline;
+        lockZ: boolean;
+      }
+      interface TargetHandleConstructor extends ImpactClass<TargetHandle> {
+        new (target: ig.Camera.TargetHandle.Target, x: number, y: number): TargetHandle;
+      }
+    }
   }
 }
