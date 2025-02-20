0f9f66fb9111d4f851a7faf403e7200538a6d21f
krypek <krypek@tuta.io>
2025-02-20 10:09:35 +0100

    Add sc.SpChangeGui

--- modules/game.feature.gui.hud.sp-change-hud.d.ts
+++ modules/game.feature.gui.hud.sp-change-hud.d.ts
@@ -1,3 +1,22 @@
 // requires impact.feature.gui.gui
 // requires game.feature.combat.model.combat-params
 // requires game.feature.model.options-model
+
+export {};
+
+declare global {
+  namespace sc {
+    interface SpChangeHudGui extends ig.GuiElementBase, sc.Model.Observer {
+      gfx: ig.Image;
+      currentSp: number;
+      consumedSp: number;
+      timer: number;
+
+      _updatePos(this: this): void;
+    }
+    interface SpChangeHudGuiConstructor extends ImpactClass<SpChangeHudGui> {
+      new (): SpChangeHudGui;
+    }
+    var SpChangeHudGui: SpChangeHudGuiConstructor;
+  }
+}
