06c21896c477b9c4d1ba10916cb3c7fdddd05151
krypek <krypek@tuta.io>
2025-02-20 11:44:19 +0100

    Add sc.QuickMenuAnalysis

--- modules/game.feature.quick-menu.gui.quick-screen.d.ts
+++ modules/game.feature.quick-menu.gui.quick-screen.d.ts
@@ -4,33 +4,42 @@
 // requires game.feature.menu.gui.menu-misc
 // requires game.feature.quick-menu.gui.quick-misc
 
-export { }
+export {};
 
 declare global {
-    namespace sc {
-        interface QuickMenuAnalysisCursor extends ig.GuiElementBase {
-            moveTo(this: this, x: number, y: number, setPos?: boolean, c?: unknown): void
-        }
-        interface QuickMenuAnalysisCursorConstructor extends ImpactClass<QuickMenuAnalysisCursor> {
-            new(): QuickMenuAnalysisCursor
-        }
-        var QuickMenuAnalysisCursor: QuickMenuAnalysisCursorConstructor
-        interface QuickMenuAnalysis extends ig.GuiElementBase {
-            buttonGroup: sc.MouseButtonGroup
-            entities: sc.QuickMenuTypesBase[]
-            _cursorPos: Vec2
-            cursor: sc.QuickMenuAnalysisCursor
-            iconContainer: ig.GuiElementBase
-            focusContainer: sc.QuickFocusScreen
-
-            show(this: this): void
-            hide(this: this): void
-            enter(this: this): void
-            exit(this: this): void
-        }
-        interface QuickMenuAnalysisConstructor extends ImpactClass<QuickMenuAnalysis> {
-            new(): QuickMenuAnalysis
-        }
-        var QuickMenuAnalysis: QuickMenuAnalysisConstructor
+  namespace sc {
+    interface QuickMenuAnalysisCursor extends ig.GuiElementBase {
+      moveTo(this: this, x: number, y: number, setPos?: boolean, c?: unknown): void;
     }
+    interface QuickMenuAnalysisCursorConstructor extends ImpactClass<QuickMenuAnalysisCursor> {
+      new (): QuickMenuAnalysisCursor;
+    }
+    var QuickMenuAnalysisCursor: QuickMenuAnalysisCursorConstructor;
+
+    interface QuickMenuAnalysis extends ig.GuiElementBase, sc.Model.Observer {
+      gfx: ig.Image;
+      buttonGroup: sc.MouseButtonGroup;
+      cursor: sc.QuickMenuAnalysisCursor;
+      entities: sc.QuickMenuTypesBase[];
+      iconContainer: ig.GuiElementBase;
+      focusContainer: sc.QuickFocusScreen;
+      corners: ig.ImageGui[];
+      background: ig.GuiElementBase;
+      _cursorPos: Vec2;
+      entered: boolean;
+
+      show(this: this): void;
+      hide(this: this): void;
+      enter(this: this): void;
+      exit(this: this): void;
+      focusCenter(this: this): void;
+      initCursor(this: this, pos: Vec2): void;
+      limitCursorPos(this: this): void;
+      createCorner(this: this, flipX?: boolean, flipY?: boolean): void;
+    }
+    interface QuickMenuAnalysisConstructor extends ImpactClass<QuickMenuAnalysis> {
+      new (): QuickMenuAnalysis;
+    }
+    var QuickMenuAnalysis: QuickMenuAnalysisConstructor;
+  }
 }
