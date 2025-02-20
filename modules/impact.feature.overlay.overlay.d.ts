61c23606819351edf8bc0e0fb4fb7efd814a7df9
krypek <krypek@tuta.io>
2025-02-20 10:05:57 +0100

    Update ig.Overlay

--- modules/impact.feature.overlay.overlay.d.ts
+++ modules/impact.feature.overlay.overlay.d.ts
@@ -6,6 +6,15 @@ export {};
 
 declare global {
   namespace ig {
+    interface OverlayGui extends ig.GuiElementBase {
+      color: ig.Overlay.RGBAColorData;
+      lighter: boolean;
+    }
+    interface OverlayGuiConstructor extends ImpactClass<OverlayGui> {
+      new (): OverlayGui;
+    }
+    var OverlayGui: OverlayGuiConstructor;
+
     interface AlphaTransitionHandler extends ig.Class {
       timer: ig.WeightTimer;
       startAlpha: number;
@@ -32,9 +41,48 @@ declare global {
     }
     var OverlayCornerGui: OverlayCornerGuiConstructor;
 
+    enum OVERLAY_CORNER {
+      WHITE,
+      RED,
+      BLACK,
+    }
+    namespace Overlay {
+      interface RGBAColorData extends ig.RGBColorData {
+        a: number;
+      }
+    }
     interface Overlay extends ig.GameAddon {
+      colorOld: ig.Overlay.RGBAColorData;
+      colorNew: ig.Overlay.RGBAColorData;
+      lighter: boolean;
+      timer: number;
+      max: number;
+      gfx: Record<'WHITE' | 'RED' | 'BLACK', ig.Image>;
+      colorGui: ig.OverlayGui;
+      cornerGui: Record<ig.OVERLAY_CORNER, ig.OverlayCornerGui>;
+      deferredUpdateOrder: number;
+
       onDeferredUpdate(this: this): void;
       onReset(this: this): void;
+      setColor(
+        this: this,
+        r: number,
+        g: number,
+        b: number,
+        a: number,
+        max: number,
+        lighter?: boolean,
+        zIndex?: number,
+      ): void;
+      setAlpha(this: this, alpha: number, max: number): void;
+      setCorner(
+        this: this,
+        corner: ig.OVERLAY_CORNER | keyof typeof ig.OVERLAY_CORNER,
+        alpha: number,
+        time: number,
+        blinkAlpha?: number,
+      ): void;
+      _getCurrentColor(this: this): ig.Overlay.RGBAColorData;
     }
     interface OverlayConstructor extends ImpactClass<Overlay> {
       new (): Overlay;
