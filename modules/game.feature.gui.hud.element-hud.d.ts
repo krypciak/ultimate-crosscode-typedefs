b0476d960ebfbfbee7f0de719df020563cf5f500
krypek <krypek@tuta.io>
2025-02-20 10:36:09 +0100

    Add sc.ElementHudGui

--- modules/game.feature.gui.hud.element-hud.d.ts
+++ modules/game.feature.gui.hud.element-hud.d.ts
@@ -1,3 +1,46 @@
 // requires impact.feature.gui.gui
 // requires game.feature.combat.model.combat-params
 // requires game.feature.model.options-model
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ElementHudGui extends ig.GuiElementBase, sc.Model.Observer {
+      timer: number;
+      icons: sc.ElementHudIconGui[];
+
+      showElement(this: this, element: sc.ELEMENT): void;
+      _updatePos(this: this): void;
+    }
+    interface ElementHudGuiConstructor extends ImpactClass<ElementHudGui> {
+      new (): ElementHudGui;
+    }
+    var ElementHudGui: ElementHudGuiConstructor;
+
+    namespace ElementHudIconGui {
+      interface IconDir {
+        alignX: ig.GUI_ALIGN;
+        alignY: ig.GUI_ALIGN;
+        tile: number;
+        rotate: number;
+        pShowX: number;
+        pShowY: number;
+        pHideX: number;
+        pHideY: number;
+      }
+    }
+    interface ElementHudIconGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+      iconDir: sc.ElementHudIconGui.IconDir;
+      currentElement: number;
+      bigSize: boolean;
+
+      show(this: this, currentElement: sc.ELEMENT, bigSize: boolean): void;
+    }
+    interface ElementHudIconGuiConstructor extends ImpactClass<ElementHudIconGui> {
+      new (iconDir: sc.ElementHudIconGui.IconDir): ElementHudIconGui;
+    }
+    var ElementHudIconGui: ElementHudIconGuiConstructor;
+  }
+}
