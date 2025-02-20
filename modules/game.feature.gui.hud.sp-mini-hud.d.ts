5eb23efb3426e39449258e881eef9911d7aab649
krypek <krypek@tuta.io>
2025-02-20 10:52:34 +0100

    Add sc.StatusHudGui

--- modules/game.feature.gui.hud.sp-mini-hud.d.ts
+++ modules/game.feature.gui.hud.sp-mini-hud.d.ts
@@ -1,2 +1,18 @@
 // requires impact.feature.gui.gui
 // requires game.feature.gui.base.numbers
+
+export {};
+
+declare global {
+  namespace sc {
+    interface SpMiniHudGui extends ig.GuiElementBase, sc.Model.Observer {
+      gfx: ig.Image;
+      params: sc.CombatParams;
+      targetSp: number;
+    }
+    interface SpMiniHudGuiConstructor extends ImpactClass<SpMiniHudGui> {
+      new (params: sc.CombatParams): SpMiniHudGui;
+    }
+    var SpMiniHudGui: SpMiniHudGuiConstructor;
+  }
+}
--- modules/game.feature.gui.hud.status-hud.d.ts
+++ modules/game.feature.gui.hud.status-hud.d.ts
@@ -10,21 +10,78 @@
 export {};
 
 declare global {
-    namespace sc {
-        interface StatusUpperGui extends ig.GuiElementBase {}
-        interface StatusUpperGuiConstructor extends ImpactClass<StatusUpperGui> {
-            new (): StatusUpperGui;
-        }
-        let StatusUpperGui: StatusUpperGuiConstructor
+  namespace sc {
+    interface StatusHudGui extends ig.GuiElementBase, sc.Model.Observer, ig.Vars.Accessor {
+      battleBgGui: null;
+      battleSymbolGui: null;
+      upperGui: sc.StatusUpperGui;
+      lowerGui: sc.StatusLowerGui;
+      elementBgGui: sc.StatusElementBgGui;
+      elementModeGui: sc.StatusElementModeGui;
+      paramGui: sc.ParamHudGui;
+      partyGui: sc.PartyHudGui;
+      keyHud: sc.KeyHudGui;
+      elementSwitchTimer: number;
+      menuMode: boolean;
 
-        interface ElementalLoadOverlayGui extends ig.GuiElementBase {
-            gfx: ig.Image;
-            alphaHandler: ig.AlphaTransitionHandler;
-            currentWarnMode: number;
-        }
-        interface ElementalLoadOverlayGuiConstructor extends ImpactClass<ElementalLoadOverlayGui> {
-            new (): ElementalLoadOverlayGui;
-        }
-        var ElementalLoadOverlayGui: ElementalLoadOverlayGuiConstructor
+      _updateVisibility(this: this): void;
+      _minimizeDisplay(this: this): void;
+      _minimizeDisplayFast(this: this): void;
+      _enterQuickMenuMode(this: this): void;
+      _enterMenuMode(this: this): void;
+      _changeMenuModeVisibility(this: this, menu: sc.MENU_SUBMENU): void;
+      elementSwitchDisplay(this: this): void;
+      getFreeScreenMinY(this: this, x: number): number;
     }
+    interface StatusHudGuiConstructor extends ImpactClass<StatusHudGui> {
+      new (): StatusHudGui;
+    }
+    var StatusHudGui: StatusHudGuiConstructor;
+
+    interface StatusUpperGui extends ig.GuiElementBase {}
+    interface StatusUpperGuiConstructor extends ImpactClass<StatusUpperGui> {
+      new (): StatusUpperGui;
+    }
+    let StatusUpperGui: StatusUpperGuiConstructor;
+
+    interface StatusLowerGui extends ig.GuiElementBase, sc.Model.Observer {
+      buffGui: sc.BuffHudGui;
+      itemTimerGui: sc.ItemTimerHudGui;
+
+      moveSubGui(this: this, moveMore: boolean): void;
+    }
+    interface StatusLowerGuiConstructor extends ImpactClass<StatusLowerGui> {
+      new (): StatusLowerGui;
+    }
+    var StatusLowerGui: StatusLowerGuiConstructor;
+
+    interface ElementalLoadOverlayGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+      alphaHandler: ig.AlphaTransitionHandler;
+      currentWarnMode: number;
+    }
+    interface ElementalLoadOverlayGuiConstructor extends ImpactClass<ElementalLoadOverlayGui> {
+      new (): ElementalLoadOverlayGui;
+    }
+    var ElementalLoadOverlayGui: ElementalLoadOverlayGuiConstructor;
+
+    interface StatusElementModeGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+      timer: number;
+      selectBg: boolean;
+    }
+    interface StatusElementModeGuiConstructor extends ImpactClass<StatusElementModeGui> {
+      new (): StatusElementModeGui;
+    }
+    var StatusElementModeGui: StatusElementModeGuiConstructor;
+
+    interface StatusElementBgGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+    }
+    interface StatusElementBgGuiConstructor extends ImpactClass<StatusElementBgGui> {
+      new (): StatusElementBgGui;
+    }
+    var StatusElementBgGui: StatusElementBgGuiConstructor;
+  }
 }
+
