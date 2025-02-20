60a001630c2948e376b20e68ab7a07593ddefd97
krypek <krypek@tuta.io>
2025-02-20 12:39:54 +0100

    Update sc.ShopMenu

--- modules/game.feature.menu.gui.shop.shop-menu.d.ts
+++ modules/game.feature.menu.gui.shop.shop-menu.d.ts
@@ -14,11 +14,43 @@ export {};
 
 declare global {
   namespace sc {
-    interface ShopMenu extends sc.BaseMenu {
+    interface ShopMenu extends sc.BaseMenu, sc.Model.Observer {
+      hotkeyHelp: sc.ButtonGui;
+      hotkeySort: sc.ButtonGui;
+      hotkeySwitch: sc.ButtonGui;
+      shopName: sc.ShopStartTitle;
+      start: sc.ShopStartMenu;
+      shopList: sc.ShopListMenu;
+      pageView: sc.ShopPageCounter;
+      stats: sc.ShopEquipStats;
       cart: sc.ShopCart;
+      quantity: sc.ShopQuantitySelect;
+      confirmDialog: sc.ShopConfirmDialog;
+      sortMenu: sc.SortMenu;
+      helpGui: sc.HelpScreen;
 
+      openQuantitySelect(this: this, button: sc.ButtonGui): void;
+      updateSortMenuButton(this: this, sortName: string): void;
+      resolveStateChange(
+        this: this,
+        prevShopState: Nullable<sc.MENU_SHOP_STATE>,
+        skipTransition?: boolean,
+      ): void;
+      onConfirm(this: this): void;
       buyItems(this: this): boolean;
       sellItems(this: this): boolean;
+      onQuantitySubmit(this: this, button:sc.ShopItemButton, amount: number): void;
+      onQuantityBack(this: this, button: sc.ShopItemButton): void;
+      onHotkeyHelpCheck(this: this): boolean;
+      onHelpButtonPressed(this: this): void;
+      onHotkeySortCheck(this: this): boolean;
+      onHotkeySwitchCheck(this: this): boolean;
+      onSortButtonPress(this: this): void;
+      onExecuteSort(this: this, button: sc.ButtonGui): void;
+      createHelpGui(this: this): void;
+      onAddHotkeys(this: this, longTransition?: boolean): void;
+      commitHotKeysToTopBar(this: this, longTransition?: boolean): void;
+      onBackButtonPress(this: this): void;
     }
     interface ShopMenuConstructor extends ImpactClass<ShopMenu> {
       new (): ShopMenu;
--- modules/game.feature.menu.gui.shop.shop-start.d.ts
+++ modules/game.feature.menu.gui.shop.shop-start.d.ts
@@ -2,3 +2,31 @@
 // requires impact.feature.gui.base.basic-gui
 // requires game.feature.menu.gui.menu-misc
 // requires game.feature.gui.base.button
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ShopStartMenu extends ig.BoxGui {
+      ninepatch: ig.NinePatch;
+      buy: sc.ButtonGui;
+      sell: sc.ButtonGui;
+      buttongroup: sc.ButtonGroup;
+
+      onButtonPress(this: this, button: sc.ButtonGui): void;
+    }
+    interface ShopStartMenuConstructor extends ImpactClass<ShopStartMenu> {
+      new (): ShopStartMenu;
+    }
+    var ShopStartMenu: ShopStartMenuConstructor;
+
+    interface ShopStartTitle extends ig.BoxGui {
+      ninepatch: ig.NinePatch;
+      text: sc.TextGui;
+    }
+    interface ShopStartTitleConstructor extends ImpactClass<ShopStartTitle> {
+      new (): ShopStartTitle;
+    }
+    var ShopStartTitle: ShopStartTitleConstructor;
+  }
+}
