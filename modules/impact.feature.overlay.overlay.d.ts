9ebc4589e290262f9191bdfacbcbef5d7a04f96a
krypek <krypek@tuta.io>
2025-02-20 09:58:48 +0100

    Add ig.OverlayCornerGui

--- modules/impact.feature.overlay.overlay.d.ts
+++ modules/impact.feature.overlay.overlay.d.ts
@@ -21,6 +21,17 @@ declare global {
     }
     var AlphaTransitionHandler: AlphaTransitionHandlerConstructor;
 
+    interface OverlayCornerGui extends ig.GuiElementBase {
+      gfx: ig.Image;
+      alphaHandler: ig.AlphaTransitionHandler;
+
+      setAlpha(this: this, alpha: number, time: number, blinkAlpha?: number): void;
+    }
+    interface OverlayCornerGuiConstructor extends ImpactClass<OverlayCornerGui> {
+      new (gfx: ig.Image): OverlayCornerGui;
+    }
+    var OverlayCornerGui: OverlayCornerGuiConstructor;
+
     interface Overlay extends ig.GameAddon {
       onDeferredUpdate(this: this): void;
       onReset(this: this): void;
