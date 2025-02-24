b123768ff4ce8e0be06a6e8f7082b12b45d12822
krypek <krypek@tuta.io>
2025-02-24 10:12:20 +0100

    Add sc.PushPullable

--- modules/game.feature.puzzle.components.push-pullable.d.ts
+++ modules/game.feature.puzzle.components.push-pullable.d.ts
@@ -8,6 +8,41 @@ declare global {
     type PullableEntity = ig.ENTITY.WavePushPullBlock | ig.ENTITY.PushPullBlock;
     interface PushPullable extends ig.Class {
       entity: PullableEntity;
+      active: boolean;
+      gripDir: keyof typeof ig.ActorEntity.FACE4;
+      deferredRelease: boolean;
+      interactEntry: sc.MapInteractEntry;
+      interactIcons: Record<'vertical' | 'horizontal', sc.MapInteractIcon>;
+      coordDelta: Vec2;
+      targetPos: Vec2;
+      dragState: number;
+      dragSpeed: number;
+      speedTimer: number;
+      dustTimer: number;
+      rumbleHandle: ig.Rumble.RumbleHandle;
+      gripCancelTimer: number;
+      soundHandle: Nullable<ig.SoundHandle>;
+      navBlocker: ig.NavBlocker;
+      magnetJustEnded: number;
+
+      setActive(this: this, active: boolean): void;
+      isActive(this: this): boolean;
+      onInteraction(this: this): void;
+      onInteractionEnd(this: this): void;
+      isInteractionBlocked(this: this): boolean;
+      resetPos(this: this, pos: Vec3, noSpawnEffects?: boolean): void;
+      onUpdate(this: this): void;
+      checkLockIn(this: this): void;
+      onKill(this: this): void;
+      onDeferredUpdate(this: this): void;
+      updateStateFromIdle(this: this): void;
+      stopSound(this: this): void;
+      cancelGrip(this: this): void;
+      onInteractObjectDrop(this: this): void;
+      getGripPosAndFace(this: this, dest: Vec3): Vec3;
+      getMovePlayerToPushableInteractibleEntityBoxThingeeAndSetGripDir(this: this): ig.Action;
+      moveBox(this: this, vx: number, vy: number): void;
+      onMagnetEnd(this: this): void;
     }
     interface PushPullableConstructor extends ImpactClass<PushPullable> {
       new (entity: PullableEntity): PushPullable;
