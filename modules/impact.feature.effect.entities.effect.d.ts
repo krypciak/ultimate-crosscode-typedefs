72f08316adf2301cbb4d6c5e723baa7c47c08d12
krypek <krypek@tuta.io>
2025-03-21 18:28:28 +0100

    Add ig.EffectTools

--- modules/impact.feature.effect.entities.effect.d.ts
+++ modules/impact.feature.effect.entities.effect.d.ts
@@ -4,28 +4,37 @@
 export {};
 
 declare global {
-  namespace ig.ENTITY {
-    namespace Effect {
-      interface Settings extends ig.Entity.Settings {}
-    }
-    interface Effect extends ig.Entity {
-      target: ig.Entity;
-      spriteFilter: number[];
+  namespace ig {
+    namespace ENTITY {
+      namespace Effect {
+        interface Settings extends ig.Entity.Settings {}
+      }
+      interface Effect extends ig.Entity {
+        target: ig.Entity;
+        spriteFilter: number[];
 
-      setIgnoreSlowdown(this: this): void;
-      stop(this: this): void;
-      isDone(this: this): boolean;
-      spawnParticle<E extends ig.Entity, S extends ig.Entity.Settings>(
-        this: this,
-        entity: string | (new (x: number, y: number, z: number, settings: S) => E),
-        offset?: Nullable<NullablePartial<Vec3>>,
-        settings?: Nullable<S>,
-        forceTargetPos?: Nullable<boolean>,
-      ): void;
+        setIgnoreSlowdown(this: this): void;
+        stop(this: this): void;
+        isDone(this: this): boolean;
+        spawnParticle<E extends ig.Entity, S extends ig.Entity.Settings>(
+          this: this,
+          entity: string | (new (x: number, y: number, z: number, settings: S) => E),
+          offset?: Nullable<NullablePartial<Vec3>>,
+          settings?: Nullable<S>,
+          forceTargetPos?: Nullable<boolean>,
+        ): void;
+      }
+      interface EffectConstructor extends ImpactClass<Effect> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Effect.Settings): Effect;
+      }
+      var Effect: EffectConstructor;
     }
-    interface EffectConstructor extends ImpactClass<Effect> {
-      new (x: number, y: number, z: number, settings: ig.ENTITY.Effect.Settings): Effect;
+    namespace EffectTools {
+      function clearEffects(entity: ig.Entity, withTheSameGroup?: ig.ENTITY.Effect): void;
+      function getFirstEffect(
+        entity: ig.Entity,
+        withTheSameGroup?: ig.ENTITY.Effect,
+      ): Nullable<ig.ENTITY.Effect>;
     }
-    var Effect: EffectConstructor;
   }
 }
