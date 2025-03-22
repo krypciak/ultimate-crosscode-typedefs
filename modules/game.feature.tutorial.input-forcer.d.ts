88f5551b21882c3691ac6ce0535049b2964b9fec
krypek <krypek@tuta.io>
2025-03-22 13:03:15 +0100

    Update sc.INPUT_FORCER_ENTRIES

--- modules/game.feature.tutorial.input-forcer.d.ts
+++ modules/game.feature.tutorial.input-forcer.d.ts
@@ -1,49 +1,59 @@
 // requires impact.base.game
 // requires impact.feature.gui.gui
 
-export {}
+export {};
 
 declare global {
-    namespace sc {
-        interface InputForcerGui extends ig.GuiElementBase { }
+  namespace sc {
+    interface InputForcerGui extends ig.GuiElementBase {}
 
-        namespace InputForcer {
-            interface Entry {
-                cancelAction: boolean
-                check: () => boolean
-                keep: boolean
-            }
-        }
-        interface InputForcer extends ig.GameAddon {
-            activeEntry: sc.InputForcer.Entry
-            texts: { title: ig.LangLabel.Data | string, textKeyboard: ig.LangLabel.Data | string, textGamepad: ig.LangLabel.Data | string }
-            gui: sc.InputForcerGui
-            blocked: boolean
-            darknessHandle: boolean
-            lightHandle: boolean
-            inputSubmitted: boolean
-            sounds: { start: ig.Sound }
-
-            setEntry(this: this, action: keyof typeof sc.INPUT_FORCER_ENTRIES, title: ig.LangLabel.Data | string,
-                textKeyboard: ig.LangLabel.Data | string, textGamepad: ig.LangLabel.Data | string): void
-            isBlocking(this: this): boolean
-            clearEntry(this: this): void
-            isSubmitted(this: this): boolean
-            onPreUpdate(this: this): void
-            _startBlock(this: this, cancelAction: boolean): void
-            _endBlock(this: this): void
-        }
-        interface InputForcerConstructor extends ImpactClass<InputForcer> {
-            new (): InputForcer
-        }
-        var InputForcer: InputForcerConstructor
-        var inputForcer: InputForcer
-
-        var INPUT_FORCER_ENTRIES: {
-            DODGE_RIGHT: sc.InputForcer.Entry
-            ATTACK_LEFT: sc.InputForcer.Entry
-            CHARGE_HOLD: sc.InputForcer.Entry
-            CHARGE_RELEASE: sc.InputForcer.Entry
-        }
+    namespace InputForcer {
+      interface Entry {
+        cancelAction: boolean;
+        check: () => boolean;
+        keep: boolean;
+      }
     }
+    interface InputForcer extends ig.GameAddon {
+      activeEntry: sc.InputForcer.Entry;
+      texts: {
+        title: ig.LangLabel.Data | string;
+        textKeyboard: ig.LangLabel.Data | string;
+        textGamepad: ig.LangLabel.Data | string;
+      };
+      gui: sc.InputForcerGui;
+      blocked: boolean;
+      darknessHandle: boolean;
+      lightHandle: boolean;
+      inputSubmitted: boolean;
+      sounds: { start: ig.Sound };
+
+      setEntry(
+        this: this,
+        action: keyof typeof sc.INPUT_FORCER_ENTRIES,
+        title: ig.LangLabel.Data | string,
+        textKeyboard: ig.LangLabel.Data | string,
+        textGamepad: ig.LangLabel.Data | string,
+      ): void;
+      isBlocking(this: this): boolean;
+      clearEntry(this: this): void;
+      isSubmitted(this: this): boolean;
+      onPreUpdate(this: this): void;
+      _startBlock(this: this, cancelAction: boolean): void;
+      _endBlock(this: this): void;
+    }
+    interface InputForcerConstructor extends ImpactClass<InputForcer> {
+      new (): InputForcer;
+    }
+    var InputForcer: InputForcerConstructor;
+    var inputForcer: InputForcer;
+
+    interface INPUT_FORCER_ENTRIES {
+      DODGE_RIGHT: sc.InputForcer.Entry;
+      ATTACK_LEFT: sc.InputForcer.Entry;
+      CHARGE_HOLD: sc.InputForcer.Entry;
+      CHARGE_RELEASE: sc.InputForcer.Entry;
+    }
+    var INPUT_FORCER_ENTRIES: INPUT_FORCER_ENTRIES;
+  }
 }
