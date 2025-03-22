ec3d68626d41b9bdd4236072c0d5f61b2eff2b22
krypek <krypek@tuta.io>
2025-03-22 10:08:08 +0100

    Add ig.Html5GamepadHandler

--- modules/impact.feature.gamepad.html5-gamepad.d.ts
+++ modules/impact.feature.gamepad.html5-gamepad.d.ts
@@ -1,1 +1,15 @@
 // requires impact.feature.gamepad.gamepad
+
+export {};
+
+declare global {
+  namespace ig {
+    interface Html5GamepadHandler extends ig.Class {
+      update(this: this, gamepads: Record<string, ig.Gamepad>): void;
+    }
+    interface Html5GamepadHandlerConstructor extends ImpactClass<Html5GamepadHandler> {
+      new (): Html5GamepadHandler;
+    }
+    var Html5GamepadHandler: Html5GamepadHandlerConstructor;
+  }
+}
