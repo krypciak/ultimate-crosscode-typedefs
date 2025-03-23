903e492a58faa6dee956b221bc733a293b286c0a
krypek <krypek@tuta.io>
2025-03-23 14:47:59 +0100

    Update sc.ItemConsumer

--- modules/game.feature.player.item-consumption.d.ts
+++ modules/game.feature.player.item-consumption.d.ts
@@ -1,15 +1,37 @@
 // requires game.feature.inventory.inventory
 
-export {}
+export {};
 
 declare global {
-    namespace sc {
-        interface ItemConsumption extends ig.Class {
-            runHealChange(this: this, settings: sc.StatChangeSettings): void;
-        }
-        interface ItemConsumptionConstructor extends ImpactClass<ItemConsumption> {
-            new (): ItemConsumption;
-        }
-        var ItemConsumption: ItemConsumptionConstructor;
+  namespace sc {
+    interface ItemConsumption extends ig.Class {
+      sounds: Record<'eat', ig.Sound>;
+
+      runItemUseAction(
+        this: this,
+        player: ig.ENTITY.Player,
+        model: sc.PlayerModel,
+        itemId: sc.ItemID,
+      ): void;
+      activateItemEffect(
+        this: this,
+        player: ig.ENTITY.Player,
+        model: sc.PlayerModel,
+        itemId: sc.ItemID,
+      ): void;
+      runHealChange(this: this, settings: sc.StatChangeSettings): void;
+      runStatChange(
+        this: this,
+        stats: sc.StatChange.StatName[],
+        duration: number,
+        itemId: sc.ItemID,
+      ): void;
+      getAction(this: this, itemId: sc.ItemID): ig.Action;
+      createAction(this: this, events: ig.EventStepBase.Settings[]): ig.Action;
     }
+    interface ItemConsumptionConstructor extends ImpactClass<ItemConsumption> {
+      new (): ItemConsumption;
+    }
+    var ItemConsumption: ItemConsumptionConstructor;
+  }
 }
