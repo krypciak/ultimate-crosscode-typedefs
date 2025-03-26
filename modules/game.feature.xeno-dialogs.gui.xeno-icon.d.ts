53091cbbdf742fc9b567a9b5d5db565137e930f3
krypek <krypek@tuta.io>
2025-03-26 11:49:12 +0100

    Update sc.XenoDialogIcon

--- modules/game.feature.xeno-dialogs.gui.xeno-icon.d.ts
+++ modules/game.feature.xeno-dialogs.gui.xeno-icon.d.ts
@@ -4,21 +4,31 @@
 // requires game.feature.interact.map-interact
 // requires game.feature.msg.gui.msg-skip-hud
 
-export { }
+export {};
 
 declare global {
-    namespace sc {
-        interface XenoDialogIcon extends ig.BoxGui {
-            ninepatch: ig.NinePatch
-            textGui: sc.TextGui
-            skipGui: sc.MsgSkipGui
-            xenoDialog: ig.ENTITY.XenoDialog
-            skipEntry: sc.SkipInteractEntry
-        }
-        interface XenoDialogIconConstructor extends ImpactClass<XenoDialogIcon> {
-            new(): XenoDialogIcon
-        }
-        var XenoDialogIcon: XenoDialogIconConstructor
+  namespace sc {
+    interface XenoDialogIcon extends ig.BoxGui {
+      ninepatch: ig.NinePatch;
+      textGui: sc.TextGui;
+      skipGui: sc.MsgSkipGui;
+      xenoDialog: ig.ENTITY.XenoDialog;
+      skipEntry: sc.SkipInteractEntry;
 
+      onTextFinish(this: this): void;
+      isTextFinished(this: this): boolean;
+      updateSkipIcon(this: this): void;
+      setText(this: this, text: sc.TextLike, dialog: ig.ENTITY.XenoDialog): void;
+      show(this: this): void;
+      hide(this: this): void;
+      onSkipInteract(this: this, msg: sc.SKIP_INTERACT_MSG): void;
+      setIconState(this: this): void;
+      isActive(this: this, state: sc.INPUT_FORCER_ENTRIES): boolean;
+      remove(this: this): void;
     }
+    interface XenoDialogIconConstructor extends ImpactClass<XenoDialogIcon> {
+      new (): XenoDialogIcon;
+    }
+    var XenoDialogIcon: XenoDialogIconConstructor;
+  }
 }
