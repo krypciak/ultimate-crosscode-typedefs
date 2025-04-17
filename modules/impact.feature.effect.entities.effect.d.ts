fdb23623df74642fcd32f12abc6f02ded5b88582
krypek <krypek@tuta.io>
2025-04-17 20:33:51 +0200

    Update ig.ENTITY.Effect.Settings

--- modules/impact.feature.effect.entities.effect.d.ts
+++ modules/impact.feature.effect.entities.effect.d.ts
@@ -5,13 +5,45 @@ export {};
 
 declare global {
   namespace ig {
+    enum EFFECT_STATE {
+      RUNNING = 0,
+      POST_LOOP = 1,
+      ENDED = 2,
+    }
     namespace ENTITY {
       namespace Effect {
-        interface Settings extends ig.Entity.Settings {}
+        interface Settings extends ig.Entity.Settings, ig.EffectSheet.SpawnSettings {
+          effect?: Nullable<ig.Effect>;
+          target?: Nullable<ig.Entity>;
+        }
       }
       interface Effect extends ig.Entity {
-        target: ig.Entity;
+        offset: Vec3;
+        rotOffset?: Nullable<Vec2>;
+        angle: number;
+        flipX: boolean;
+        duration: number;
+        state: ig.EFFECT_STATE;
+        effect?: Nullable<ig.Effect>;
+        target?: Nullable<ig.Entity>;
         spriteFilter: number[];
+        timelineIndex: number;
+        timer: number;
+        looped: boolean;
+        particles: unknown[];
+        runners: (ig.EffectParticleRunner | ig.EffectTimeRunner)[];
+        align: ig.ENTITY_ALIGN;
+        target2: {
+          point?: Nullable<Vec3>;
+          entity?: Nullable<ig.Entity>;
+          align?: Nullable<ig.ENTITY_ALIGN>;
+          offset: Vec3;
+        };
+        attachGroup: string;
+        callback: Nullable<ig.EffectSheet.EventCallback>;
+        rotateFace: number;
+        flipLeftFace: boolean;
+        noMultiGroup: boolean;
 
         setIgnoreSlowdown(this: this): void;
         stop(this: this): void;
