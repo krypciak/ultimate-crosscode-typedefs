ddd581eb6b475338b8572ac4f77f91f65fce4eb2
krypek <krypek@tuta.io>
2025-04-17 20:33:51 +0200

    Update ig.ENTITY.Effect.Settings

--- modules/impact.feature.effect.effect-sheet.d.ts
+++ modules/impact.feature.effect.effect-sheet.d.ts
@@ -48,16 +48,22 @@ declare global {
       }
 
       interface SpawnSettings {
-        target?: ig.Entity;
-        align?: keyof typeof ig.ENTITY_ALIGN | ig.ENTITY_ALIGN;
-        target2?: ig.Entity;
-        target2Align?: ig.ENTITY_ALIGN;
-        spriteFilter?: number[];
-        callback?: EventCallback;
-        duration?: number;
-        group?: string;
-        angle?: Vec2;
-        rotateFace?: number;
+        target2?: Nullable<ig.Entity>;
+        target2Point?: Nullable<Vec3>;
+        target2Align?: Nullable<ig.ENTITY_ALIGN>;
+        target2Offset?: Nullable<Vec3>;
+        noMultiGroup?: Nullable<boolean>;
+        spriteFilter?: Nullable<number[]>;
+        offset?: Nullable<Vec3>;
+        rotOffset?: Nullable<Vec2>;
+        align?: Nullable<keyof typeof ig.ENTITY_ALIGN | ig.ENTITY_ALIGN>;
+        angle?: Nullable<number>;
+        flipX?: Nullable<boolean>;
+        rotateFace?: Nullable<number>;
+        flipLeftFace?: Nullable<boolean>;
+        duration?: Nullable<number>;
+        group?: Nullable<string>;
+        callback?: Nullable<EventCallback>;
       }
     }
     interface EffectSheet extends ig.JsonLoadable {
@@ -82,6 +88,54 @@ declare global {
       cache: Record<string, ig.EffectSheet>;
     }
     var EffectSheet: EffectSheetConstructor;
+
+    interface Effect extends ig.Class {
+      id: string;
+      steps: ig.EffectStepBase[];
+      loopStartIdx: number;
+      loopStartTime: number;
+      loopEndIdx: number;
+      loopEndTime: number;
+      maxTime: number;
+
+      clearCached(this: this): void;
+      update(this: this, effect: ig.ENTITY.Effect): void;
+      isEnding(this: this, effect: ig.ENTITY.Effect): boolean;
+      isDone(this: this, effect: ig.ENTITY.Effect): boolean;
+      getRemainingTime(this: this, effect: ig.ENTITY.Effect): number;
+    }
+    interface EffectConstructor extends ImpactClass<Effect> {
+      new (sheet: ig.EffectSheet, effectName: string, data: unknown[]): Effect;
+    }
+    var Effect: EffectConstructor;
+
+    namespace EffectStepBase {
+      interface Settings {
+        anim: string;
+        followUpAnim?: Nullable<string>;
+        postAnim?: Nullable<string>;
+        pAlpha?: Nullable<number>;
+        pScale?: Nullable<number>;
+        pRotate?: Nullable<number>;
+        angleVary?: Nullable<number>;
+        randFlip?: Nullable<boolean>;
+        moveWithTarget?: Nullable<number>;
+        pLight?: keyof typeof ig.LIGHT_SIZE | null;
+        particleDuration?: Nullable<number>;
+        particleDurVariance?: Nullable<number>;
+        cancelable?: Nullable<boolean>;
+      }
+    }
+    interface EffectStepBase extends ig.StepBase {
+      _nextStep: Nullable<ig.EffectStepBase>;
+      branches: Nullable<Record<string, ig.EffectStepBase>>;
+
+      start(this: this, entity: ig.ENTITY.Effect): void;
+      run(this: this, entity: ig.ENTITY.Effect): boolean;
+      getNext(this: this, entity: ig.ENTITY.Effect): ig.EffectStepBase;
+    }
+    interface EffectStepBaseConstructor extends ImpactClass<EffectStepBase> {}
+    var EffectStepBase: EffectStepBaseConstructor;
 
     namespace EffectHandle {
       interface Settings {
@@ -114,32 +168,29 @@ declare global {
     }
     let EffectHandle: EffectHandleConstructor;
 
-    namespace EffectStepBase {
-      interface Settings {
-        anim: string;
-        followUpAnim?: Nullable<string>;
-        postAnim?: Nullable<string>;
-        pAlpha?: Nullable<number>;
-        pScale?: Nullable<number>;
-        pRotate?: Nullable<number>;
-        angleVary?: Nullable<number>;
-        randFlip?: Nullable<boolean>;
-        moveWithTarget?: Nullable<number>;
-        pLight?: keyof typeof ig.LIGHT_SIZE | null;
-        particleDuration?: Nullable<number>;
-        particleDurVariance?: Nullable<number>;
-        cancelable?: Nullable<boolean>;
-      }
-    }
-    interface EffectStepBase extends ig.StepBase {
-      _nextStep: Nullable<ig.EffectStepBase>;
-      branches: Nullable<Record<string, ig.EffectStepBase>>;
+    interface EffectRunnerBase extends ig.Class {
+      step: ig.EffectStepBase;
+      data: unknown;
+      duration: number;
+      _timer: number;
 
-      start(this: this, entity: ig.ENTITY.Effect): void;
-      run(this: this, entity: ig.ENTITY.Effect): boolean;
-      getNext(this: this, entity: ig.ENTITY.Effect): ig.EffectStepBase;
+      update(this: this, effect: ig.ENTITY.Effect): boolean;
+      cancel(this: this, effect: ig.ENTITY.Effect): boolean;
     }
-    interface EffectStepBaseConstructor extends ImpactClass<EffectStepBase> {}
-    var EffectStepBase: EffectStepBaseConstructor;
+
+    interface EffectTimeRunner extends EffectRunnerBase {}
+    interface EffectTimeRunnerConstructor extends ImpactClass<EffectTimeRunner> {
+      new (step: ig.EffectStepBase, data: unknown): EffectTimeRunner;
+    }
+    var EffectTimeRunner: EffectTimeRunnerConstructor;
+
+    interface EffectParticleRunner extends EffectRunnerBase {
+      totalParticles: number;
+      currentParticle: number;
+    }
+    interface EffectParticleRunnerConstructor extends ImpactClass<EffectParticleRunner> {
+      new (step: ig.EffectStepBase, data: unknown): EffectParticleRunner;
+    }
+    var EffectParticleRunner: EffectParticleRunnerConstructor;
   }
 }
