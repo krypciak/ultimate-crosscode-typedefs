b21ff59e2d8481d0f41bb48e8a97e9a3c04d2f01
krypek <krypek@tuta.io>
2025-05-04 19:09:50 +0200

    Update ig.ENTITY.Switch

--- modules/game.feature.puzzle.entities.switch.d.ts
+++ modules/game.feature.puzzle.entities.switch.d.ts
@@ -1,21 +1,25 @@
 // requires impact.base.entity
 
-export { }
+export {};
 
 declare global {
-    namespace ig {
-        namespace ENTITY {
-            namespace Switch {
-                interface Settings {}
-            }
-            interface Switch extends ig.AnimatedEntity {
-                variable: string
-                isOn: boolean
-            }
-            interface SwitchConstructor extends ImpactClass<Switch> {
-                new(x: number, y: number, z: number, settings: ig.ENTITY.Switch.Settings): Switch
-            }
-            var Switch: SwitchConstructor
-        }
+  namespace ig {
+    namespace ENTITY {
+      namespace Switch {
+        interface Settings {}
+      }
+      interface Switch extends ig.AnimatedEntity {
+        variable: string;
+        isOn: boolean;
+        sounds: Record<'hit' | 'bing', ig.Sound>;
+
+        ballHit(this: this, ball: ig.Entity): boolean;
+        varsChanged(this: this): void;
+      }
+      interface SwitchConstructor extends ImpactClass<Switch> {
+        new (x: number, y: number, z: number, settings: ig.ENTITY.Switch.Settings): Switch;
+      }
+      var Switch: SwitchConstructor;
     }
+  }
 }
