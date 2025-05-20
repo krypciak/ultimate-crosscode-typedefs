371bf847d311fa0ebe04357740c0d90d975883f5
krypek <krypek@tuta.io>
2025-05-20 17:40:33 +0200

    Add sc.ArenaCache

--- modules/game.feature.arena.arena-loadable.d.ts
+++ modules/game.feature.arena.arena-loadable.d.ts
@@ -1,2 +1,16 @@
 // requires impact.base.game
 // requires impact.base.loader
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ArenaCache extends ig.Cacheable {
+      onCacheCleared(this: this): void;
+    }
+    interface ArenaCacheConstructor extends ImpactClass<ArenaCache> {
+      new (): ArenaCache;
+    }
+    var ArenaCache: ArenaCacheConstructor;
+  }
+}
