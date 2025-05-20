d6a3748e4d199553b7c4950ffde1fda71f5c3211
krypek <krypek@tuta.io>
2025-05-20 11:08:36 +0200

    Add ig.EVENT_STEP.SOLVE_QUEST_CONDITION

--- modules/game.feature.quest.quest-steps.d.ts
+++ modules/game.feature.quest.quest-steps.d.ts
@@ -3,3 +3,16 @@
 // requires impact.base.event
 // requires game.feature.quest.quest-model
 // requires game.feature.menu.gui.quests.quest-misc
+
+export {};
+
+declare global {
+  namespace ig.EVENT_STEP {
+    namespace SOLVE_QUEST_CONDITION {
+      interface QuestLabelSelect {
+        quest: sc.QuestModel.QuestId;
+        label: string;
+      }
+    }
+  }
+}
