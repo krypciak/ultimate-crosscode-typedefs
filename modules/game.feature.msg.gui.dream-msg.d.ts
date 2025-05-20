b29d1ac69814bf76e3d67d6890e941fad8bb59f1
krypek <krypek@tuta.io>
2025-05-20 18:35:08 +0200

    Add sc.DREAM_TEXT_POS_TYPE

--- modules/game.feature.msg.gui.dream-msg.d.ts
+++ modules/game.feature.msg.gui.dream-msg.d.ts
@@ -1,3 +1,14 @@
 // requires impact.base.image
 // requires impact.feature.interact.gui.focus-gui
 // requires game.feature.interact.button-group
+
+export {};
+
+declare global {
+  namespace sc {
+    enum DREAM_TEXT_POS_TYPE {
+      TOP = 1,
+      BOTTOM = 2,
+    }
+  }
+}
