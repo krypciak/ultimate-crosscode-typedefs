0fcf398e0c9c81e125d20dd845f5f57a60c8ccc2
krypek <krypek@tuta.io>
2025-05-20 17:15:42 +0200

    Add ig.EVENT_STEP.WAIT_UNTIL_CREDIT_TRIGGER.CreditsTriggerSelect

--- modules/game.feature.credits.credits-steps.d.ts
+++ modules/game.feature.credits.credits-steps.d.ts
@@ -1,3 +1,13 @@
 // requires impact.base.action
 // requires impact.base.event
 // requires game.feature.credits.credit-loadable
+
+export {};
+
+declare global {
+  namespace ig.EVENT_STEP.WAIT_UNTIL_CREDIT_TRIGGER {
+    interface CreditsTriggerSelect {
+      trigger: string;
+    }
+  }
+}
