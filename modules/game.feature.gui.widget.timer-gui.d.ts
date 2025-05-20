a083f4f6da56a712c14d35ead7cdf1b74c11b9ae
krypek <krypek@tuta.io>
2025-05-20 18:25:44 +0200

    Add autogenerated steps game.feature.gui.widget.timer-gui

--- modules/game.feature.gui.widget.timer-gui.d.ts
+++ modules/game.feature.gui.widget.timer-gui.d.ts
@@ -6,14 +6,30 @@
 export {};
 
 declare global {
-    namespace ig {
-        namespace GUI {
-            interface TimerGui extends sc.SlickBoxRawGui {}
-            interface TimerGuiConstructor extends ImpactClass<TimerGuiConstructor> {}
-        }
-        interface GUI {
-            TimerGui: GUI.TimerGuiConstructor;
-        }
+  namespace ig {
+    namespace GUI {
+      interface TimerGui extends sc.SlickBoxRawGui {}
+      interface TimerGuiConstructor extends ImpactClass<TimerGuiConstructor> {}
+    }
+    interface GUI {
+      TimerGui: GUI.TimerGuiConstructor;
     }
 
+    namespace EVENT_STEP {
+      namespace STOP_TIMER {
+        interface Settings {
+          hide: boolean;
+        }
+      }
+      interface STOP_TIMER extends ig.EventStepBase {
+        hide: boolean;
+        _wm: ig.Config;
+      }
+      interface STOP_TIMER_CONSTRUCTOR extends ImpactClass<STOP_TIMER> {
+        new (settings: ig.EVENT_STEP.STOP_TIMER.Settings): STOP_TIMER;
+      }
+      var STOP_TIMER: STOP_TIMER_CONSTRUCTOR;
+    }
+  }
 }
+
