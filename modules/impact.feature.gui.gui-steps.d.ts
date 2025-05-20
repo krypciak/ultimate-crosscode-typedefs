40612dddb3df7558208a25450aac79df5949b0cf
krypek <krypek@tuta.io>
2025-05-20 17:17:10 +0200

    ADd ig.EVENT_STEP.ADD_GUI.GUI

--- modules/impact.feature.gui.gui-steps.d.ts
+++ modules/impact.feature.gui.gui-steps.d.ts
@@ -1,3 +1,28 @@
 // requires impact.base.action
 // requires impact.base.event
 // requires impact.feature.gui.gui
+
+export {};
+
+declare global {
+  namespace ig.EVENT_STEP.ADD_GUI {
+    type AllGuiInfo = {
+      [K in keyof typeof ig.GUI]: {
+        type: K;
+      } & ConstructorParameters<(typeof ig.GUI)[K]>[0];
+    };
+    type GuiInfo = AllGuiInfo[keyof typeof ig.GUI];
+
+    interface GUI {
+      name?: string;
+      guiInfo: GuiInfo;
+    }
+  }
+
+  namespace ig.EVENT_STEP.CHANGE_GUI_STATE {
+    interface GUIState {
+      type: keyof typeof ig.GUI;
+      state: string;
+    }
+  }
+}
