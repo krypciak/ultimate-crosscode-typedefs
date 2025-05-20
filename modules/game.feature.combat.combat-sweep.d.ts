5813af42f91b5cc35b889d6c8e3a3606f39b1943
krypek <krypek@tuta.io>
2025-05-20 18:21:59 +0200

    Add sc.COMBAT_SWEEPS

--- modules/game.feature.combat.combat-sweep.d.ts
+++ modules/game.feature.combat.combat-sweep.d.ts
@@ -1,1 +1,36 @@
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace sc {
+    namespace COMBAT_SWEEPS {
+      interface Force {
+        radius: number;
+        zHeight: number;
+        centralAngle: number;
+        duration: number;
+        attack: sc.AttackInfo.AttackSettings;
+        checkCollision?: boolean;
+        alwaysFull?: boolean;
+      }
+      interface Base {
+        sheet: ig.EffectSheet;
+        keys: string[];
+        fxRotOffset?: Vec2;
+        fxDuration?: number;
+        force: Force;
+      }
+    }
+    interface COMBAT_SWEEPS {
+      SPHEROMANCER: sc.COMBAT_SWEEPS.Base;
+      SPHEROMANCER_FINISHER: sc.COMBAT_SWEEPS.Base;
+      TRIBLADER: sc.COMBAT_SWEEPS.Base;
+      TRIBLADER_FINISHER: sc.COMBAT_SWEEPS.Base;
+      QUADROGUARD: sc.COMBAT_SWEEPS.Base;
+      QUADROGUARD_FINISHER: sc.COMBAT_SWEEPS.Base;
+    }
+
+    var COMBAT_SWEEPS: COMBAT_SWEEPS;
+  }
+}
