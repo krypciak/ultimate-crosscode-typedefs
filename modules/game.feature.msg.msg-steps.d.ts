c64e6326c60af6b95b83f6d5d1eddf0996ab3408
krypek <krypek@tuta.io>
2025-05-20 11:29:42 +0200

    Add ig.EVENT_STEP.SHOW_MODAL_CHOICE

--- modules/game.feature.msg.msg-steps.d.ts
+++ modules/game.feature.msg.msg-steps.d.ts
@@ -5,3 +5,13 @@
 // requires game.feature.gui.widget.demo-stats
 // requires game.feature.gui.widget.demo-highscore
 // requires game.feature.msg.gui.dream-msg
+
+export {};
+
+declare global {
+  namespace ig.EVENT_STEP.SHOW_MODAL_CHOICE {
+    interface ModalChoiceOption {
+      label: ig.LangLabel.Data;
+    }
+  }
+}
