c091d889b3f6c67da80791b1c79b0dab2abf55d5
krypek <krypek@tuta.io>
2025-05-20 17:17:00 +0200

    Add ig.EVENT_STEP.CALL_EVENT_FROM_SHEET.EventSheetCall

--- modules/impact.feature.event-sheet.event-sheet-steps.d.ts
+++ modules/impact.feature.event-sheet.event-sheet-steps.d.ts
@@ -1,2 +1,21 @@
 // requires impact.base.action
 // requires impact.base.event
+
+export {};
+
+declare global {
+  namespace ig.EVENT_STEP.CALL_EVENT_FROM_SHEET {
+    interface Input {
+      cornerPos: Vec2;
+      targetPos: Vec2;
+      wait: number;
+      postCondition: string;
+      variable: string;
+    }
+    interface EventSheetCall {
+      path: string;
+      name: string;
+      input: Input;
+    }
+  }
+}
