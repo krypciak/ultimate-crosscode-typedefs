26ddbdf6d9298db01954275bc274bf7863c89121
krypek <krypek@tuta.io>
2025-05-20 17:45:45 +0200

    Add AUTO_CTRL_ACTION

--- modules/game.feature.auto-control.auto-control.d.ts
+++ modules/game.feature.auto-control.auto-control.d.ts
@@ -4,10 +4,45 @@ export {};
 
 declare global {
   namespace sc {
+    var AUTO_CTRL_AXIS: ['left', 'right'];
+    var AUTO_INPUT_DEVICE: {
+      MOUSE: ig.INPUT_DEVICES.KEYBOARD_AND_MOUSE;
+      GAMEPAD: ig.INPUT_DEVICES.GAMEPAD;
+    };
+    var AUTO_CTRL_ACTION: [
+      'menu',
+      'quickmenu',
+      'menuConfirm',
+      'menuBack',
+      'menuDown',
+      'menuUp',
+      'menuLeft',
+      'menuRight',
+      'rightPressed',
+      'menuHotkeyHelp',
+      'menuHotkeyHelp2',
+      'menuHotkeyHelp3',
+      'menuCircleLeft',
+      'menuCircleRight',
+      'questCircleLeft',
+      'questCircleRight',
+      'heatMode',
+      'coldMode',
+      'shockMode',
+      'waveMode',
+      'moveDirX',
+      'moveDirY',
+      'aiming',
+      'thrown',
+      'charge',
+      'melee',
+      'dashing',
+      'guarding',
+    ];
     interface AutoControl extends ig.GameAddon {
       get(this: this, type: string): boolean;
-      isActive(this: this): boolean
-      setActive(this: this, value: boolean): void
+      isActive(this: this): boolean;
+      setActive(this: this, value: boolean): void;
     }
     interface AutoControlConstructor extends ImpactClass<AutoControl> {
       new (): AutoControl;
