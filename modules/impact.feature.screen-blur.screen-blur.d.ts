6f5acab37a38fd7d69965ed3d0e4760232b23038
krypek <krypek@tuta.io>
2025-05-21 19:16:32 +0200

    Add ig.BLUR_ZOOM_CONFIG

--- modules/impact.feature.screen-blur.screen-blur.d.ts
+++ modules/impact.feature.screen-blur.screen-blur.d.ts
@@ -4,6 +4,24 @@ export {};
 
 declare global {
   namespace ig {
+    namespace BLUR_ZOOM_CONFIG {
+      interface Base {
+        zoomAdd: number;
+        duration: number;
+        repeat: number;
+        alphaStart: number;
+      }
+    }
+    interface BLUR_ZOOM_CONFIG {
+      LIGHTEST: ig.BLUR_ZOOM_CONFIG.Base;
+      LIGHTER: ig.BLUR_ZOOM_CONFIG.Base;
+      LIGHT: ig.BLUR_ZOOM_CONFIG.Base;
+      MEDIUM: ig.BLUR_ZOOM_CONFIG.Base;
+      SLOW_INTENSE: ig.BLUR_ZOOM_CONFIG.Base;
+      STATIC_LIGHT: ig.BLUR_ZOOM_CONFIG.Base;
+    }
+    var BLUR_ZOOM_CONFIG: BLUR_ZOOM_CONFIG;
+
     interface ScreenBlur extends ig.GameAddon {
       onPostUpdate(this: this): void;
       onPreDraw(this: this): void;
