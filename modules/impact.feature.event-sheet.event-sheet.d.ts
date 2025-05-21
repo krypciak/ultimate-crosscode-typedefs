fb895a63999ad7541c5f7d1bbadf0c40668d1180
krypek <krypek@tuta.io>
2025-05-21 19:03:49 +0200

    Add ig.EventSheet

--- modules/impact.feature.event-sheet.event-sheet.d.ts
+++ modules/impact.feature.event-sheet.event-sheet.d.ts
@@ -1,1 +1,17 @@
 // requires impact.base.event
+
+export {};
+
+declare global {
+  namespace ig {
+    interface EventSheet extends ig.JsonLoadable {
+      events: Record<string, ig.Event>;
+
+      getEvent(this: this, name: string): Nullable<ig.Event>;
+    }
+    interface EventSheetConstructor extends ImpactClass<EventSheet> {
+      new (): EventSheet;
+    }
+    var EventSheet: EventSheetConstructor;
+  }
+}
