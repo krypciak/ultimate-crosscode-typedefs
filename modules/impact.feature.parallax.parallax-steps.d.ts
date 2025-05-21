9750d78feb1e2c65873c26a4d2e8183b60d3935e
krypek <krypek@tuta.io>
2025-05-21 19:14:30 +0200

    Add autogenerated steps impact.feature.parallax.parallax-steps

--- modules/impact.feature.parallax.parallax-steps.d.ts
+++ modules/impact.feature.parallax.parallax-steps.d.ts
@@ -1,3 +1,27 @@
 // requires impact.feature.parallax.parallax
 // requires impact.base.action
 // requires impact.base.event
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace EVENT_STEP {
+      namespace SHOW_PARALLAX {
+        interface Settings {
+          parallax: string;
+        }
+      }
+      interface SHOW_PARALLAX extends ig.EventStepBase {
+        parallaxGui: unknown;
+        _wm: ig.Config;
+
+        clearCached(this: this): void;
+      }
+      interface SHOW_PARALLAX_CONSTRUCTOR extends ImpactClass<SHOW_PARALLAX> {
+        new (settings: ig.EVENT_STEP.SHOW_PARALLAX.Settings): SHOW_PARALLAX;
+      }
+      var SHOW_PARALLAX: SHOW_PARALLAX_CONSTRUCTOR;
+    }
+  }
+}
