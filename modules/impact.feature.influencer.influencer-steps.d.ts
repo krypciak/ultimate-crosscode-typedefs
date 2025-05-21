34b2f208e57f7af33c9363c9ec1191d92ed33510
krypek <krypek@tuta.io>
2025-05-21 19:10:00 +0200

    Add autogenerated steps impact.feature.influencer.influencer-steps

--- modules/impact.feature.influencer.influencer-steps.d.ts
+++ modules/impact.feature.influencer.influencer-steps.d.ts
@@ -2,3 +2,39 @@
 // requires impact.base.event
 // requires impact.base.entity
 // requires impact.feature.influencer.influencer
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ACTION_STEP {
+      namespace ADD_TEMP_INFLUENCE {
+        interface Settings {
+          target: unknown;
+          timeScale?: number;
+          logicTimeScale?: number;
+          moveXYScale?: number;
+        }
+      }
+      interface ADD_TEMP_INFLUENCE extends ig.ActionStepBase {
+        _wm: ig.Config;
+        influenceEntry: ig.InfluenceEntry;
+      }
+      interface ADD_TEMP_INFLUENCE_CONSTRUCTOR extends ImpactClass<ADD_TEMP_INFLUENCE> {
+        new (settings: ig.ACTION_STEP.ADD_TEMP_INFLUENCE.Settings): ADD_TEMP_INFLUENCE;
+      }
+      var ADD_TEMP_INFLUENCE: ADD_TEMP_INFLUENCE_CONSTRUCTOR;
+
+      namespace CLEAR_TEMP_INFLUENCE {
+        interface Settings {}
+      }
+      interface CLEAR_TEMP_INFLUENCE extends ig.ActionStepBase {
+        _wm: ig.Config;
+      }
+      interface CLEAR_TEMP_INFLUENCE_CONSTRUCTOR extends ImpactClass<CLEAR_TEMP_INFLUENCE> {
+        new (settings: ig.ACTION_STEP.CLEAR_TEMP_INFLUENCE.Settings): CLEAR_TEMP_INFLUENCE;
+      }
+      var CLEAR_TEMP_INFLUENCE: CLEAR_TEMP_INFLUENCE_CONSTRUCTOR;
+    }
+  }
+}
