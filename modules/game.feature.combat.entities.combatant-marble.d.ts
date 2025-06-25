8b9149ee1baf6033c9efecdcaad3aad5ce136de0
krypek <krypek@tuta.io>
2025-06-25 13:23:18 +0200

    Add ig.ENTITY.CombatantMarble

--- modules/game.feature.combat.entities.combatant-marble.d.ts
+++ modules/game.feature.combat.entities.combatant-marble.d.ts
@@ -1,2 +1,34 @@
 // requires impact.feature.effect.effect-sheet
 // requires impact.base.entity
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace CombatantMarble {
+      interface Settings extends ig.AnimatedEntity.Settings {
+        target: ig.ENTITY.EnemyCounter;
+      }
+    }
+    interface CombatantMarble extends ig.AnimatedEntity {
+      tileSheet: ig.TileSheet;
+      effects: ig.EffectSheet;
+      startPos: Vec2;
+      startZPos: number;
+      target: ig.ENTITY.EnemyCounter;
+      timer: number;
+      maxZHeight: number;
+      maxTime: number;
+      lineHandle: ig.ENTITY.Effect;
+    }
+    interface CombatantMarbleConstructor extends ImpactClass<CombatantMarble> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.CombatantMarble.Settings,
+      ): CombatantMarble;
+    }
+    var CombatantMarble: CombatantMarbleConstructor;
+  }
+}
