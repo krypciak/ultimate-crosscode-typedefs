65db7206d687ccdcf362691fdf703f305bc79a61
krypek <krypek@tuta.io>
2025-08-03 19:23:21 +0200

    Add ig.ENTITY.FloorSwitch

--- modules/game.feature.puzzle.entities.floor-switch.d.ts
+++ modules/game.feature.puzzle.entities.floor-switch.d.ts
@@ -5,11 +5,17 @@ export {};
 
 declare global {
   namespace sc {
-    enum FLOOR_SWITCH_TYPE {
-      PERMANENT,
-      WHILE_ON_TOP,
-      UNDOABLE,
+    interface FloorSwitchType {
+      prenament?: boolean;
+      delay?: number;
+      undoable?: boolean;
     }
+    interface FLOOR_SWITCH_TYPE {
+      PERMANENT: sc.FloorSwitchType;
+      WHILE_ON_TOP: sc.FloorSwitchType;
+      UNDOABLE: sc.FloorSwitchType;
+    }
+    var FLOOR_SWITCH_TYPE: FLOOR_SWITCH_TYPE;
   }
   namespace ig.ENTITY {
     namespace FloorSwitch {
@@ -19,5 +25,30 @@ declare global {
         lockCondition?: string | null;
       }
     }
+    interface FloorSwitch extends ig.AnimatedEntity, ig.EffectSheet.EventCallback {
+      hitCondition: null;
+      ballDestroyer: boolean;
+      switchType: sc.FloorSwitchType;
+      variable: string;
+      isOn: boolean;
+      lockCondition: ig.VarCondition;
+      setOnDelay: number;
+      effects: Record<'sheet', ig.EffectSheet>;
+      _wm: ig.Config;
+
+      onHideRequest(this: this): void;
+      _isStillPressed(this: this): boolean;
+      activate(this: this, noDelay?: boolean): void;
+      deactivate(this: this): void;
+      varsChanged(this: this): void;
+      collideWith(this: this, entity: ig.Entity): void;
+      onGroundAdd(this: this, entity: ig.Entity): void;
+      checkEntityArraySupport(this: this, entities: ig.Entity[]): boolean;
+      isEntitySupported(this: this, entity: ig.Entity): boolean;
+    }
+    interface FloorSwitchConstructor extends ImpactClass<FloorSwitch> {
+      new (x: number, y: number, z: number, settings: ig.ENTITY.FloorSwitch.Settings): FloorSwitch;
+    }
+    var FloorSwitch: FloorSwitchConstructor;
   }
 }
