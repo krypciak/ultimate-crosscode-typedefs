f48459a83cd9ae53d070feeb6788aef6e9d0c001
krypek <krypek@tuta.io>
2025-08-26 17:58:45 +0200

    Update sc.CircuitOverviewMenu

--- modules/game.feature.skills.skilltree.d.ts
+++ modules/game.feature.skills.skilltree.d.ts
@@ -1,24 +1,28 @@
 // requires impact.base.game
 // requires game.feature.skills.skills
 
+import { SkillTreeFileData } from '../file-types/skilltree';
+
 export {};
 
 declare global {
-    namespace sc {
-        var SKILLS_DISTANCE_MULTIPLIER: number;
-        enum SKILLS_DIRECTION {
-            STRAIGHT = 0,
-            CW_45 = 1,
-            CW_90 = 2,
-            CW_135 = 3,
-            CCW_45 = 4,
-            CCW_90 = 5,
-            CCW_135 = 6
-        }
-
-        interface SkillTree extends ig.SingleLoadable {
-            version: number;
-            skills: sc.BaseSkill[]
-        }
+  namespace sc {
+    var SKILLS_DISTANCE_MULTIPLIER: number;
+    enum SKILLS_DIRECTION {
+      STRAIGHT = 0,
+      CW_45 = 1,
+      CW_90 = 2,
+      CW_135 = 3,
+      CCW_45 = 4,
+      CCW_90 = 5,
+      CCW_135 = 6,
     }
+
+    interface SkillTree extends ig.SingleLoadable {
+      version: number;
+      _trees: SkillTreeFileData;
+      skills: sc.BaseSkill[];
+    }
+  }
 }
+
