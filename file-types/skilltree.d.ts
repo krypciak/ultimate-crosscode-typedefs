975b656513b69b8b1d5ac8d416e5014688fa88af
krypek <krypek@tuta.io>
2025-08-27 17:21:33 +0200

    Update sc.SkillTree

--- file-types/skilltree.d.ts
+++ file-types/skilltree.d.ts
@@ -1,32 +1,31 @@
-export type SkillTreeFileData = Record<string, SkillNode[]>
+export type SkillTreeFileData = Record<keyof typeof sc.ELEMENT, SkillNode[]>;
 
-export type SkillNode = SkillNodeSkill | SkillNodeOr
+export type SkillNode = SkillNodeSkill | SkillNodeOr;
 
 export interface SkillNodeBase {
-    distance: number;
-    direction: SkillDirection;
-    children: SkillNode[]
-    id: number;
+  distance: number;
+  direction: keyof typeof sc.SKILLS_DIRECTION;
+  children: SkillNode[];
+  id: number;
 }
 
 export interface SkillNodeSkill extends SkillNodeBase {
-    level: number;
-    skill: Skill;
+  level: number;
+  skill: Skill;
 }
 
 export interface SkillNodeOr extends SkillNodeBase {
-    orBranch: OrBranch;
+  orBranch: OrBranch;
 }
 
 export interface Skill {
-    type: string
+  type: string;
 }
 
 export interface OrBranch {
-    direction:  SkillDirection;
-    levels: number[]
-    left: Skill[]
-    right: Skill[]
+  direction: keyof typeof sc.SKILLS_DIRECTION;
+  levels: number[];
+  left: Skill[];
+  right: Skill[];
 }
 
-export type SkillDirection = "STRAIGHT" | "CW_45" | "CW_90" | "CW_135" | "CCW_45" | "CCW_90" | "CCW_135";
