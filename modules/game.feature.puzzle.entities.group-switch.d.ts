ac303b5602cc4bf719bdf8c04257da14de751299
krypek <krypek@tuta.io>
2025-08-30 22:34:55 +0200

    Add sc.GROUP_SWITCH_TYPE

--- modules/game.feature.puzzle.entities.group-switch.d.ts
+++ modules/game.feature.puzzle.entities.group-switch.d.ts
@@ -1,1 +1,33 @@
 // requires impact.base.entity
+
+export {};
+
+declare global {
+  namespace sc {
+    enum GROUP_SWITCH_UPDATE_TYPE {
+      LOCAL = 0,
+      GLOBAL = 1,
+    }
+  }
+  namespace ig.ENTITY {
+    interface GroupSwitch extends ig.AnimatedEntity {}
+    interface GroupSwitchConstructor extends ImpactClass<GroupSwitch> {
+      new (x: number, y: number, z: number, settings: unknown): GroupSwitch;
+    }
+    var GroupSwitch: GroupSwitchConstructor;
+  }
+  namespace sc {
+    interface GROUP_SWITCH_TYPE_BASE {
+      size: Vec3;
+      activeZHeight: number;
+      useStyleSheet: boolean;
+      anims: any;
+
+      hitCondition(switchEntity: ig.ENTITY.GroupSwitch, ball: ig.ENTITY.Ball): void;
+    }
+    interface GROUP_SWITCH_TYPE {
+      default: GROUP_SWITCH_TYPE_BASE;
+    }
+    var GROUP_SWITCH_TYPE: GROUP_SWITCH_TYPE;
+  }
+}
