e2736ee3813335e1f2471b68d3f9cd51bfd50772
krypek <krypek@tuta.io>
2025-08-30 22:20:00 +0200

    Add sc.ElementShieldBallEntity

--- modules/game.feature.puzzle.entities.element-shield.d.ts
+++ modules/game.feature.puzzle.entities.element-shield.d.ts
@@ -1,3 +1,15 @@
 // requires impact.base.entity
 // requires impact.base.actor-entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ElementShieldBallEntity extends ig.AnimatedEntity, ig.BallLike {}
+    interface ElementShieldBallEntityConstructor extends ImpactClass<ElementShieldBallEntity> {
+      new (x: number, y: number, z: number, settings: unknown): ElementShieldBallEntity;
+    }
+    var ElementShieldBallEntity: ElementShieldBallEntityConstructor;
+  }
+}
