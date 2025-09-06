62dc9650d086077580b050f57ec92bbf5b95f67a
krypek <krypek@tuta.io>
2025-09-06 16:56:11 +0200

    Update sc.BounceSwitchGroups

--- modules/game.feature.puzzle.entities.bounce-switch.d.ts
+++ modules/game.feature.puzzle.entities.bounce-switch.d.ts
@@ -8,22 +8,38 @@ declare global {
   namespace sc {
     namespace BounceSwitchGroups {
       interface Group {
-        endSwitch: null;
+        endSwitch: Nullable<ig.ENTITY.BounceSwitch>;
         variable: string;
-        blocks: unknown[];
+        blocks: ig.ENTITY.BounceBlock[];
         blockHitCount: number;
         finalHit: boolean;
         currentBall: Nullable<ig.ENTITY.Ball>;
-        cameraHandle: null;
-        overrideHandle: null;
+        cameraHandle: Nullable<ig.Camera.TargetHandle>;
+        overrideHandle: Nullable<ig.Camera.TargetHandle>;
       }
     }
     interface BounceSwitchGroups extends ig.GameAddon {
       groups: Record<string, sc.BounceSwitchGroups.Group>;
+      effects: ig.EffectSheet;
 
-      evaluateGroup(this: this, name: string): void;
+      registerSwitch(this: this, bounceSwitch: ig.ENTITY.BounceSwitch, variable: string): void;
+      registerBlock(this: this, bounceBlock: ig.ENTITY.BounceBlock): void;
+      evaluateGroup(this: this, groupName: string): void;
+      resetGroup(this: this, groupName: string): void;
+      resolveGroup(this: this, name: string): void;
+      setCameraBall(this: this, groupName: string, ball: ig.ENTITY.Ball): void;
+      setCameraPos(this: this, groupName: string, pos: Vec2): void;
+      resetCamera(this: this, groupName: string): void;
       isGroupBallConflict(this: this, groupName: string, entity: ig.Entity): boolean;
+      isGroupResolved(this: this, groupName: string): boolean;
+      isBallOfAnyGroup(this: this, groupNames: string[]): boolean;
+      getEndSwitch(this: this, groupName: string): Nullable<ig.ENTITY.BounceSwitch>;
+      getHitCount(this: this, groupName: string): number;
+      onBlockHit(this: this, bounceBlock: ig.ENTITY.BounceBlock, ball: ig.ENTITY.Ball): number;
+      onSwitchHit(this: this, bounceSwitch: ig.ENTITY.BounceSwitch, ball: ig.ENTITY.Ball): boolean;
       onDeferredUpdate(this: this): void;
+      onReset(this: this): void;
+      onLevelLoadStart(this: this): void;
       getGroup(this: this, groupName: string): sc.BounceSwitchGroups.Group;
     }
     interface BounceSwitchGroupsConstructor extends ImpactClass<BounceSwitchGroups> {
