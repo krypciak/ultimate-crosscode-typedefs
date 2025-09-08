a0969ccc273a907e200fae3f3af8ac2aced969ab
krypek <krypek@tuta.io>
2025-09-08 16:27:11 +0200

    Update ig.CubeSprite

--- modules/impact.base.sprite.d.ts
+++ modules/impact.base.sprite.d.ts
@@ -23,13 +23,14 @@ declare global {
       lighterOverlay: { color: Nullable<string>; alpha: number };
       aboveZ: number;
       mergeTop: boolean;
-      renderData: Record<string, unknown>;
+      renderData: Record<string, Nullable<ig.Renderer2d.SpriteDrawSlot>>;
       gui: boolean;
       renderMode: Nullable<GlobalCompositeOperation>;
       alwaysRender: boolean;
       noOverlapSolving: boolean;
 
-      clear(this: this, guiSprites?: boolean): void
+      clear(this: this, guiSprites?: boolean): void;
+      kill(this: this): void;
       setPos(this: this, x: number, y: number, z: number): void;
       setGfxOffset(this: this, x: number, y: number): void;
       setGfxCut(this: this, top: number, bottom: number, left: number, right: number): void;
@@ -42,15 +43,46 @@ declare global {
         type?: ig.COLL_SHADOW_TYPE,
         scaleY?: number,
       ): void;
+      setSize(this: this, width: number, height: number, zHeight: number, wallY?: number): void;
       setImageSrc(
         this: this,
         image: this['image'],
         offsetX?: Nullable<number>,
         offsetY?: Nullable<number>,
       ): void;
+      setFlip(this: this, flipX?: boolean, flipY?: boolean): void;
+      setPivot(this: this, x: number, y: number): void;
+      setTransform(this: this, x: number, y: number, rotate: number): void;
+      setAlpha(this: this, alpha: number): void;
       setOverlayColor(this: this, color: string, alpha: number): void;
       setLighterOverlayColor(this: this, color: string, alpha: number): void;
       setInvisible(this: this): void;
+      setSizeFromEntity(
+        this: this,
+        entity: ig.Entity,
+        width: number,
+        height: number,
+        shapeType: ig.ANIM_SHAPE_TYPE,
+        wallY?: number,
+      ): void;
+      setImageSrcFromEntity(
+        this: this,
+        entity: ig.Entity,
+        tileWidth: number,
+        d: number,
+        image: this['image'],
+        offsetX?: Nullable<number>,
+        offsetY?: Nullable<number>,
+      ): void;
+      setShadowFromEntity(this: this, entity: ig.Entity): void;
+      setPosFromEntity(
+        this: this,
+        entity: ig.Entity,
+        offset?: Vec3,
+        shapeType?: ig.ANIM_SHAPE_TYPE | keyof typeof ig.ANIM_SHAPE_TYPE,
+        wallY?: number,
+      ): void;
+      centerPivot(this: this, entity: ig.Entity): void;
       setEntityDefault(
         this: this,
         entity: ig.Entity,
@@ -63,6 +95,7 @@ declare global {
         srcX: number,
         srcY: number,
       ): void;
+      assign(this: this, sprite: ig.CubeSprite): void;
     }
     interface CubeSpriteConstructor extends ImpactClass<CubeSprite> {
       new (gui: boolean): CubeSprite;
