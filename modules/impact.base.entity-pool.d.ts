0e43ff89c861abb8c2bff70e270c04a28ee8a54c
krypek <krypek@tuta.io>
2025-11-04 20:15:31 +0100

    Add ig.EntityPool

--- modules/impact.base.entity-pool.d.ts
+++ modules/impact.base.entity-pool.d.ts
@@ -1,1 +1,32 @@
 // requires impact.base.game
+
+export {};
+
+declare global {
+  namespace ig {
+    interface EntityPool {
+      pools: Record<number, ig.Entity[]>;
+      mixin: {
+        staticInstantiate(
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.Entity.Settings,
+        ): Nullable<ig.Entity>;
+        erase(): void;
+      };
+      enableFor(clazz: ig.ClassConstructor): void;
+      getFromPool(
+        classId: number,
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.Entity.Settings,
+      ): Nullable<ig.Entity>;
+      putInPool(clazz: ig.Class): void;
+      drainPool(classId: number): void;
+      drainAllPools(): void;
+    }
+    var EntityPool: EntityPool;
+  }
+}
