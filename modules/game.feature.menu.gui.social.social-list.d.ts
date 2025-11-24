ec0de3e720e5587258377d02de2ddc347ed69aca
krypek <krypek@tuta.io>
2025-11-24 15:23:04 +0100

    Update sc.SocialList

--- modules/game.feature.menu.gui.social.social-list.d.ts
+++ modules/game.feature.menu.gui.social.social-list.d.ts
@@ -10,7 +10,30 @@ export {};
 
 declare global {
   namespace sc {
-    interface SocialList extends sc.ListTabbedPane {}
+    interface SocialList extends sc.ListTabbedPane, sc.Model.Observer {
+      submitSound: ig.Sound;
+      favSound: ig.Sound;
+      errorSound: ig.Sound;
+
+      addObservers(this: this): void;
+      removeObservers(this: this): void;
+      updatePartyMembers(this: this): void;
+      getCurrentSortText(this: this): string;
+      getMemberList(
+        this: this,
+        type: sc.PARTY_MEMBER_TYPE,
+        sortType?: sc.PARTY_SORT_TYPE,
+      ): string[];
+      sortList(this: this, names: string[], sortType: sc.PARTY_SORT_TYPE): void;
+      onCreateListEntries(
+        this: this,
+        list: sc.ButtonListBox,
+        buttonGroup: sc.ButtonGroup,
+        type: sc.PARTY_MEMBER_TYPE,
+        sort: sc.PARTY_SORT_TYPE,
+      ): void;
+      onListEntrySelected(this: this, button: sc.SocialEntryButton): void;
+    }
     interface SocialListConstructor extends ImpactClass<SocialList> {
       new (): SocialList;
     }
