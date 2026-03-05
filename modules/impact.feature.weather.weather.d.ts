13d943254929ec0753b00deba731655ef0c3f791
krypek <krypek@tuta.io>
2026-03-05 22:12:40 +0100

    Update ig.Weather

--- modules/impact.feature.weather.weather.d.ts
+++ modules/impact.feature.weather.weather.d.ts
@@ -12,11 +12,16 @@ declare global {
         type: string;
         quantity: number;
       }
+      interface SubOverlay {
+        brightColor: string;
+        darkColor: string;
+      }
     }
     interface WeatherType {
       lightMapDarkness?: number;
       particles?: WeatherType.Particle[];
       glowColor?: string;
+      subOverlay?: ig.WeatherType.SubOverlay;
     }
 
     var WEATHER_TYPES: Record<string, ig.WeatherType>;
@@ -33,11 +38,44 @@ declare global {
     }
     var WeatherInstance: WeatherInstanceConstructor;
 
+    namespace Weather {
+      interface ExtraParticle {
+        spawner: ig.EnvParticleSpawner;
+        count: number;
+      }
+      interface LightLevel {
+        last: number;
+        target: number;
+        timer: number;
+      }
+    }
     interface Weather extends ig.GameAddon {
+      levelWeather: ig.WeatherInstance;
+      currentWeather: ig.WeatherInstance;
+      clouds: ig.Clouds;
+      fog: ig.Fog;
+      rain: ig.Rain;
+      darknessHandle: ig.DarknessHandle;
+      outside: boolean;
+      lightMapDarkness: ig.Weather.LightLevel;
+      subOverlay: Nullable<ig.WeatherType.SubOverlay>;
+      lightMapBrightness: ig.Weather.LightLevel;
+      extraParticles: ig.Weather.ExtraParticle[];
+
+      setWeather(this: this, currentWeather: ig.WeatherInstance, immediately: boolean): void;
+      addExtraParticles(this: this, spawner: ig.EnvParticleSpawner, count: number): void;
+      removeExtraParticles(
+        this: this,
+        spawner: ig.EnvParticleSpawner,
+        readdSpawner?: boolean,
+      ): void;
+      onLevelLoadStart(this: this, data: sc.MapModel.Map): void;
       onLevelLoaded(this: this): void;
       onDeferredUpdate(this: this): void;
       onMidDraw(this: this): void;
       onReset(this: this): void;
+      updateWeather(this: this, immediately: boolean): void;
+      restoreParticles(this: this, immediately: boolean): void;
     }
     interface WeatherConstructor extends ImpactClass<Weather> {
       new (): Weather;
