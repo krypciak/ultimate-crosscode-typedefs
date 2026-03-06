23ab992315aeb9c9b4e4d352a582925f6b935496
krypek <krypek@tuta.io>
2026-03-06 18:30:19 +0100

    Add ig.ENTITY.TeleportStairs

--- modules/impact.feature.map-content.entities.stair-door.d.ts
+++ modules/impact.feature.map-content.entities.stair-door.d.ts
@@ -2,3 +2,52 @@
 // requires impact.base.actor-entity
 // requires impact.feature.map-content.entities.hidden-block
 // requires impact.feature.base.entities.object-layer-view
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace TeleportStairs {
+        interface Settings extends ig.Entity.Settings {
+          transitionType?: 'REGULAR' | 'INTER_AREA';
+          map: string;
+          marker: string;
+          layer: unknown;
+          npcRunnerProb?: number;
+          blockEvent?: ig.EventStepBase.Settings;
+          blockEventCondition?: string;
+        }
+      }
+      interface TeleportStairs
+        extends ig.Entity,
+          ig.ENTITY.Marker.MarkerLike,
+          sc.NpcRunnerSpawner.RunnerDestinationEntity,
+          sc.NPCRunnerEntity.EnterableEntity {
+        gfx: ig.Image;
+        map: string;
+        marker: string;
+        stairType: unknown;
+        blockEvent: ig.Event;
+        blockEventCondition: ig.VarCondition;
+        spawnDistance: number;
+        transitionType: number;
+        npcRunnerProb: number;
+        centerWalkThrough: boolean;
+        objMaps: unknown;
+
+        getEnterActionData(this: this, entity: ig.Entity): ig.EventStepBase.Settings[];
+        getStartPoint(this: this, entity: ig.Entity): Vec2;
+      }
+      interface TeleportStairsConstructor extends ImpactClass<TeleportStairs> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.TeleportStairs.Settings,
+        ): TeleportStairs;
+      }
+      var TeleportStairs: TeleportStairsConstructor;
+    }
+  }
+}
