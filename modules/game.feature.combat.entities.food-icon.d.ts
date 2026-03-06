70bf35e5266e3ab94e022bc82b07f487e918caf0
krypek <krypek@tuta.io>
2026-03-06 12:47:03 +0100

    Update sc.FoodIconEntity

--- modules/game.feature.combat.entities.food-icon.d.ts
+++ modules/game.feature.combat.entities.food-icon.d.ts
@@ -11,19 +11,25 @@ declare global {
       DONE = 2,
     }
 
-    var FOOD_SPRITE: Record<string, number>;
+    type FOOD_SPRITE = number
+    var FOOD_SPRITE: Record<string, FOOD_SPRITE>;
 
     namespace FoodIconEntity {
       interface Settings extends ig.Entity.Settings {
-        icon: number;
+        icon: sc.FOOD_SPRITE;
         combatant: ig.ENTITY.Combatant;
       }
     }
-    interface FoodIconEntity extends ig.Entity {
+    interface FoodIconEntity extends ig.Entity, ig.ActorEntity.ActionAttachedListener {
+      icon: sc.FOOD_SPRITE;
+      combatant: ig.ENTITY.Combatant;
+      offset: Vec2;
       state: sc.FOOD_ICON_STATE;
-      sprites: ig.CubeSprite[];
-      icon: number;
+      foodSheet: ig.TileSheet;
+      bubbleGfx: ig.Image;
+      timer: number;
 
+      deferredUpdate(this: this): void;
       setState(this: this, state: sc.FOOD_ICON_STATE, offset?: Nullable<Vec2>): void;
     }
     interface FoodIconEntityConstructor extends ImpactClass<FoodIconEntity> {
