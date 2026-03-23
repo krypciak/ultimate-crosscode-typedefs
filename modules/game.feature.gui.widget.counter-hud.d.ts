30ba86f6dc3fb33b98c4e0417b242a189d015121
krypek <krypek@tuta.io>
2026-03-23 17:13:16 +0100

    Update ig.GUI.ScoreHud

--- modules/game.feature.gui.widget.counter-hud.d.ts
+++ modules/game.feature.gui.widget.counter-hud.d.ts
@@ -5,18 +5,38 @@
 export {};
 
 declare global {
-    namespace ig {
-        namespace GUI {
-            interface CounterHud extends sc.RightHudBoxGui {
-            }
-            interface CounterHudConstructor extends ImpactClass<CounterHud> {}
-            interface ScoreHud extends sc.RightHudBoxGui {
-            }
-            interface ScoreHudConstructor extends ImpactClass<ScoreHud> {}
+  namespace ig {
+    namespace GUI {
+      interface CounterHud extends sc.RightHudBoxGui {}
+      interface CounterHudConstructor extends ImpactClass<CounterHud> {}
+
+      namespace ScoreHud {
+        interface Settings {
+          taskTitle: string;
+          maxCount: number;
+          variable: string;
+          cutsceneOkay?: boolean;
+          maxValue?: number;
+          time?: number;
+          signed?: boolean;
+          useDots?: boolean;
         }
-        interface GUI {
-            CounterHud: GUI.CounterHudConstructor;
-            ScoreHud: GUI.ScoreHudConstructor;
-        }
+      }
+      interface ScoreHud extends sc.RightHudBoxGui {
+        maxCount: number;
+        currentCount: number;
+        variable: string;
+        maxNumberGui: sc.MaxNumberGui;
+        zIndex: number;
+        _wm: ig.Config;
+      }
+      interface ScoreHudConstructor extends ImpactClass<ScoreHud> {
+        new (settings: ig.GUI.ScoreHud.Settings): ScoreHud;
+      }
     }
+    interface GUI {
+      CounterHud: GUI.CounterHudConstructor;
+      ScoreHud: GUI.ScoreHudConstructor;
+    }
+  }
 }
