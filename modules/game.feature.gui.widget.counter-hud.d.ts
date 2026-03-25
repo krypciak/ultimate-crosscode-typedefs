5a95ccedf1afbe88f32ad6cb898714130c0ad29d
krypek <krypek@tuta.io>
2026-03-25 17:13:03 +0100

    Update ig.GUI.CounterHud

--- modules/game.feature.gui.widget.counter-hud.d.ts
+++ modules/game.feature.gui.widget.counter-hud.d.ts
@@ -7,12 +7,34 @@ export {};
 declare global {
   namespace ig {
     namespace GUI {
-      interface CounterHud extends sc.RightHudBoxGui {}
-      interface CounterHudConstructor extends ImpactClass<CounterHud> {}
+      namespace CounterHud {
+        interface Settings {
+          taskTitle: ig.LangLabel.Data;
+          maxCount: number;
+          variable: string;
+        }
+      }
+      interface CounterHud extends sc.RightHudBoxGui, sc.Model.Observer {
+        maxCount: number;
+        currentCount: number;
+        variable: string;
+        zIndex: number;
+        _wm: ig.Config;
+        maxNumberGui: sc.MaxNumberGui;
+
+        varsChanged(this: this): void;
+        updateNumber(this: this): void;
+        onSpawn(this: this): void;
+      }
+      interface CounterHudConstructor extends ImpactClass<CounterHud> {
+        new (settings: ig.GUI.CounterHud.Settings): CounterHud;
+
+        spawnHandler(rightHudBox: sc.RightHudBoxGui): void;
+      }
 
       namespace ScoreHud {
         interface Settings {
-          taskTitle: string;
+          taskTitle: ig.LangLabel.Data;
           maxCount: number;
           variable: string;
           cutsceneOkay?: boolean;
@@ -22,16 +44,24 @@ declare global {
           useDots?: boolean;
         }
       }
-      interface ScoreHud extends sc.RightHudBoxGui {
+      interface ScoreHud extends sc.RightHudBoxGui, sc.Model.Observer {
         maxCount: number;
         currentCount: number;
         variable: string;
         maxNumberGui: sc.MaxNumberGui;
         zIndex: number;
         _wm: ig.Config;
+
+        numberGui: sc.NumberGui;
+        _cutscene: boolean;
+
+        varsChanged(this: this): void;
+        onSpawn(this: this): void;
       }
       interface ScoreHudConstructor extends ImpactClass<ScoreHud> {
         new (settings: ig.GUI.ScoreHud.Settings): ScoreHud;
+
+        spawnHandler(rightHudBox: sc.RightHudBoxGui): void;
       }
     }
     interface GUI {
