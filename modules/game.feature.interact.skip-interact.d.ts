0348b5f4ed90bbfd6e1500b905fcf68cfff03b59
krypek <krypek@tuta.io>
2026-04-03 18:52:13 +0200

    Add sc.SkipInteract

--- modules/game.feature.interact.skip-interact.d.ts
+++ modules/game.feature.interact.skip-interact.d.ts
@@ -1,24 +1,38 @@
 // requires impact.feature.interact.interact
 
-export { }
+export {};
 
 declare global {
-    namespace sc {
-        enum SKIP_INTERACT_MSG {
-            ENABLED = 1,
-            DISABLED = 2,
-            SKIPPED = 3,
-        }
-        interface SkipInteractEntry extends ig.Class {
-            handler: sc.XenoDialogIcon | sc.SideMessageHudGui
-            order: number
-
-            isActive(this: this): boolean
-        }
-        interface SkipInteractEntryConstructor extends ImpactClass<SkipInteractEntry> {
-            new(): SkipInteractEntry
-        }
-        var SkipInteractEntry: SkipInteractEntryConstructor
-
+  namespace sc {
+    enum SKIP_INTERACT_MSG {
+      ENABLED = 1,
+      DISABLED = 2,
+      SKIPPED = 3,
     }
+    interface SkipInteract extends ig.GameAddon {
+      entries: sc.MapInteractEntry[];
+
+      addEntry(this: this, entry: sc.MapInteractEntry): void;
+      removeEntry(this: this, entry: sc.MapInteractEntry): void;
+      getActiveEntry(this: this): sc.MapInteractEntry | undefined;
+      triggerSkip(this: this): void;
+    }
+    interface SkipInteractConstructor extends ImpactClass<SkipInteract> {
+      new (): SkipInteract;
+    }
+    var SkipInteract: SkipInteractConstructor;
+    var skipInteract: SkipInteract
+
+    namespace SkipInteractEntry {}
+    interface SkipInteractEntry extends ig.Class {
+      handler: sc.XenoDialogIcon | sc.SideMessageHudGui;
+      order: number;
+
+      isActive(this: this): boolean;
+    }
+    interface SkipInteractEntryConstructor extends ImpactClass<SkipInteractEntry> {
+      new (handler: sc.SkipInteractEntry['handler'], order: number): SkipInteractEntry;
+    }
+    var SkipInteractEntry: SkipInteractEntryConstructor;
+  }
 }
