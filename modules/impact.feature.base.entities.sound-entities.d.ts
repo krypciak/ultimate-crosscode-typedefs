a235c8f6f35b10413734df2209779a9f10ef8673
krypek <krypek@tuta.io>
2026-04-14 19:59:32 +0200

    Add ig.ENTITY.SoundSource

--- modules/impact.feature.base.entities.sound-entities.d.ts
+++ modules/impact.feature.base.entities.sound-entities.d.ts
@@ -1,2 +1,38 @@
 // requires impact.base.entity
 // requires impact.base.actor-entity
+
+export {};
+
+declare global {
+  namespace ig {
+    namespace ENTITY {
+      namespace SoundSource {
+        interface Settings extends ig.Entity.Settings {
+          sound: string;
+          volume?: number;
+          radius?: number;
+          rangeType?: ig.SOUND_RANGE_TYPE | keyof ig.SOUND_RANGE_TYPE;
+          speed?: number;
+          faceDuration?: number;
+        }
+      }
+      interface SoundSource extends ig.Entity {
+        sound: ig.Sound;
+        soundHandle: unknown;
+        radius: number;
+        rangeType: ig.SOUND_RANGE_TYPE | keyof ig.SOUND_RANGE_TYPE;
+        _wm: ig.Config;
+
+        stop(this: this): void;
+      }
+      interface SoundSourceConstructor extends ImpactClass<SoundSource> {
+        new (
+          x: number,
+          y: number,
+          z: number,
+          settings: ig.ENTITY.SoundSource.Settings,
+        ): SoundSource;
+      }
+    }
+  }
+}
