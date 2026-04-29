e2ad700c8f592df05eef4ae36407a12a06a80be2
krypek <krypek@tuta.io>
2026-04-29 18:18:27 +0200

    Add sc.LoadingScreenGui

--- modules/game.feature.gui.screen.loading-screen.d.ts
+++ modules/game.feature.gui.screen.loading-screen.d.ts
@@ -1,2 +1,18 @@
 // requires impact.feature.gui.gui
 // requires impact.base.image
+
+export {};
+
+declare global {
+  namespace sc {
+    interface LoadingScreenGui extends ig.GuiElementBase, sc.Model.Observer {
+      gfx: ig.Image;
+      timer: number;
+      textBlock: ig.TextBlock;
+    }
+    interface LoadingScreenGuiConstructor extends ImpactClass<LoadingScreenGui> {
+      new (): LoadingScreenGui;
+    }
+    var LoadingScreenGui: LoadingScreenGuiConstructor;
+  }
+}
