c877fe10d6562b2fa4d88a2d51274cd2c26b6a80
krypek <krypek@tuta.io>
2026-07-24 20:18:29 +0200

    Add ig.ENTITY.WaterBlock

--- modules/game.feature.puzzle.entities.water-block.d.ts
+++ modules/game.feature.puzzle.entities.water-block.d.ts
@@ -1,2 +1,50 @@
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
+
+export {}
+
+declare global {
+    namespace ig.ENTITY {
+        namespace WaterBlock {
+            interface Settings extends ig.AnimatedEntity.Settings {
+                blockType?: keyof BlockType
+                changeDuration?: number
+            }
+            interface BlockType {
+                shape: ig.COLLSHAPE
+                offY: number
+                flipX: boolean
+                wallY: number
+            }
+            interface BLOCK_TYPES {
+                SQUARE: BlockType
+                CORNER_NE: BlockType
+                CORNER_SE: BlockType
+                CORNER_SW: BlockType
+                CORNER_NW: BlockType
+            }
+        }
+        interface WaterBlock extends ig.AnimatedEntity, ig.EffectSheet.EventCallback {
+            blockType: number
+            state: number
+            changeDuration: number
+            timer: number
+            compressorSlow: number
+            effects: { sheet: ig.EffectSheet; handle: Nullable<ig.EffectHandle>; hitHandle: Nullable<ig.EffectHandle> }
+            _wm: ig.Config
+
+            steam(this: this, vec: Vec2, combatant?: ig.ENTITY.Combatant): void
+            isCompressor(this: this): boolean
+            isFrozen(this: this): boolean
+            turnIce(this: this): void
+            melt(this: this): void
+            reform(this: this): void
+            bounce(this: this): void
+            onGroundAdd(this: this, entity: ig.Entity): void
+        }
+        interface WaterBlockConstructor extends ImpactClass<WaterBlock> {
+            new (x: number, y: number, z: number, settings: ig.ENTITY.WaterBlock.Settings): WaterBlock
+        }
+        var WaterBlock: WaterBlockConstructor
+    }
+}
