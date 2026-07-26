abc60ed68599e377af93b963a4616868674a82c2
krypek <krypek@tuta.io>
2026-07-26 17:23:49 +0200

    Add ig.ENTITY.SlidingBlock

--- modules/game.feature.puzzle.entities.sliding-block.d.ts
+++ modules/game.feature.puzzle.entities.sliding-block.d.ts
@@ -1,3 +1,30 @@
 // requires impact.base.actor-entity
 // requires impact.base.entity
 // requires impact.feature.effect.effect-sheet
+
+export {};
+
+declare global {
+  namespace ig.ENTITY {
+    namespace SlidingBlock {
+      interface Settings extends ig.AnimatedEntity.Settings {}
+    }
+    interface SlidingBlock extends ig.AnimatedEntity {
+      _wm: ig.Config;
+      moving: boolean;
+      moveDir: Vec2;
+      bombSnap: boolean;
+      squishRespawn: boolean;
+      effects: { sheet: ig.EffectSheet };
+    }
+    interface SlidingBlockConstructor extends ImpactClass<SlidingBlock> {
+      new (
+        x: number,
+        y: number,
+        z: number,
+        settings: ig.ENTITY.SlidingBlock.Settings,
+      ): SlidingBlock;
+    }
+    var SlidingBlock: SlidingBlockConstructor;
+  }
+}
