bf7811e71de6a09db92d37fcc64a745f1374f6df
krypek <krypek@tuta.io>
2026-09-10 17:20:23 +0200

    Update ig.ENTITY.WaveTeleport

--- modules/game.feature.puzzle.entities.wave-teleport.d.ts
+++ modules/game.feature.puzzle.entities.wave-teleport.d.ts
@@ -12,7 +12,34 @@ declare global {
       }
       interface Settings extends ig.Entity.Settings {}
     }
-    interface WaveTeleport extends ig.AnimatedEntity {}
+    interface WaveTeleport
+      extends
+        ig.AnimatedEntity,
+        ig.EffectSheet.EventCallback,
+        ig.ActorEntity.ActionAttachedListener {
+      effects: {
+        sheet: ig.EffectSheet;
+        handle: Nullable<ig.EffectHandle>;
+        hideHandle: Nullable<ig.EffectHandle>;
+      };
+      teleportTimer: number;
+      teleportTargets: ig.Entity[];
+      _wm: ig.Config;
+      permaRemove: boolean;
+      delayedHide: boolean;
+
+      onHideRequest(this: this): void;
+      startTeleport(this: this): void;
+      doTeleport(this: this): void;
+      hasBlockOnTop(this: this): boolean;
+      ballHit(this: this, ballLike: ig.BallLike, blockDir?: Vec2): boolean;
+      isBallDestroyer(
+        this: this,
+        collPos: Vec3,
+        collRes: ig.Physics.TraceResult,
+        isThrowCharged?: boolean,
+      ): boolean;
+    }
     interface WaveTeleportConstructor extends ImpactClass<WaveTeleport> {
       new (
         x: number,
